7679fffb7c94f795a566e90772d926035647a2c1
Levent Erkok <levent.erkok@galois.com>
2011-01-26 21:08:48 -0800

    SAW script: cleanup recursion detector

--- SAWScriptMain.hs
+++ SAWScriptMain.hs
@@ -3,7 +3,9 @@ module Main where
 import Control.Monad(zipWithM_)
 -- import Data.Version(showVersion)
 import Data.Graph
+import Data.List(sortBy)
 import Data.Maybe(listToMaybe)
+import Data.Ord(comparing)
 import qualified Data.Map as M
 import System.Environment (getArgs)
 
@@ -17,7 +19,7 @@ main = do jvs <- getArgs
           case jvs of
             [f] -> do (pgm@(JVPgm _ parseMap), deps) <- parseJVPgm f
                       case checkCycles deps of
-                        Just c  -> complainCycle c
+                        Just c  -> complainCycle deps c
                         Nothing -> let cnt   = M.size parseMap
                                        specs = show cnt ++ " SAW sript" ++ if cnt > 1 then "s" else ""
                                    in do putStrLn $ "Loaded " ++ specs ++ " successfully."
@@ -25,13 +27,14 @@ main = do jvs <- getArgs
             _  -> putStrLn $ "Usage: sawScript <methodSpecFile>"
 
 checkCycles :: M.Map FilePath [(FilePath, Pos)] -> Maybe [FilePath]
-checkCycles m = listToMaybe [d | CyclicSCC d <- stronglyConnComp g]
+checkCycles m = listToMaybe $ sortBy (comparing length) [ns | CyclicSCC ns <- stronglyConnComp g]
   where g = [(f, f, map fst fps) | (f, fps) <- M.assocs m]
 
-complainCycle :: [FilePath] -> IO ()
-complainCycle [c] = putStrLn $ "ERROR: SAW script " ++ show c ++ " recursively imports itself."
-complainCycle cs  = do putStrLn $ "ERROR: Following SAW scripts specs recursively depend on each other:"
-                       mapM_ (putStrLn .  ("        " ++) . show) cs
+complainCycle :: M.Map FilePath [(FilePath, Pos)] -> [FilePath] -> IO ()
+complainCycle deps c = do putStrLn $ "ERROR: Mutually recursive SAW script" ++ (if length deps' > 1 then "s" else "") ++ " detected:"
+                          mapM_ disp deps'
+  where deps' = concat [[ip | ip@(i, _) <- is, i `elem` c] | (f, is) <- M.assocs deps, f `elem` c]
+        disp (f, p) = putStrLn $ "  Script: " ++ show f ++ " imported at " ++ show p
 
 process :: JVPgm -> IO ()
 process (JVPgm _ m) = do putStrLn "*** Starting script dump."
