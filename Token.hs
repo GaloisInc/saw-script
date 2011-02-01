a5a68cf3454bb2bb86018fca250775c135615a02
Levent Erkok <levent.erkok@galois.com>
2011-01-31 20:25:27 -0800

    derive Functor, instead of implementing it ourselves. GHC is cool!

--- Token.hs
+++ Token.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveFunctor #-}
 module SAWScript.Token where
 
 data Token p = TVar      { tokPos :: p, tokStr :: String                     }
@@ -10,16 +11,4 @@ data Token p = TVar      { tokPos :: p, tokStr :: String                     }
              | TCmntS    { tokPos :: p, tokStr :: String                     }
              | TCmntE    { tokPos :: p, tokStr :: String                     }
              | TEOF      { tokPos :: p, tokStr :: String                     }
-             deriving Show
-
-instance Functor Token where
-  fmap g (TVar       p n)   = TVar      (g p) n
-  fmap g (TLit       p s)   = TLit      (g p) s
-  fmap g (TUnknown   p c)   = TUnknown  (g p) c
-  fmap g (TPunct     p s)   = TPunct    (g p) s
-  fmap g (TReserved  p s)   = TReserved (g p) s
-  fmap g (TOp        p s)   = TOp       (g p) s
-  fmap g (TNum       p s i) = TNum      (g p) s i
-  fmap g (TCmntS     p s)   = TCmntS    (g p) s
-  fmap g (TCmntE     p s)   = TCmntE    (g p) s
-  fmap g (TEOF       p s)   = TEOF      (g p) s
+             deriving (Show, Functor)
