0abcc1a99a460c8ef3edfe5afd8daaabb5bd0998
Joe Hendrix <jhendrix@galois.com>
2012-11-26 14:57:02 -0800

    Add compatibility with older versions of TemplateHaskell.

--- src/Verifier/SAW/Prelude.hs
+++ src/Verifier/SAW/Prelude.hs
@@ -1,4 +1,5 @@
 -- Provides access to the preludeLanguage.Haskell.TH
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Verifier.SAW.Prelude
   ( Module
@@ -6,6 +7,11 @@ module Verifier.SAW.Prelude
   ) where
 
 import qualified Data.ByteString.Lazy as BL
+#if __GLASGOW_HASKELL__ >= 706
+#else
+import qualified Data.ByteString.Lazy.UTF8 as UTF8
+#endif
+
 import Data.ByteString.Unsafe (unsafePackAddressLen)
 import Language.Haskell.TH
 import System.IO.Unsafe (unsafePerformIO)
@@ -22,7 +28,11 @@ preludeModule = unsafePerformIO $ do
                       (_,[]) -> return (AppE (AppE packExpr lenExpr) primExpr)
                        where packExpr = VarE $ mkName "unsafePackAddressLen"
                              lenExpr  = LitE $ IntegerL $ toInteger $ BL.length b
+#if __GLASGOW_HASKELL__ >= 706
                              primExpr = LitE $ StringPrimL $ BL.unpack b
+#else
+                             primExpr = LitE $ StringPrimL $ UTF8.toString b
+#endif
                       (_,errors) -> fail $ "Failed to parse prelude:\n" ++ show errors)
   let (decls,[]) = runParser "SAW.core" (BL.fromChunks [b]) parseSAW
   return (unsafeMkModule decls)
