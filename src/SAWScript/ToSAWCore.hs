ca9f316bf0547f83e3a7eeb5752742f3c0460879
Aaron Tomb <atomb@galois.com>
2013-03-22 08:39:46 -0700

    Add missing file.

---
+++ src/SAWScript/ToSAWCore.hs
@@ -0,0 +1,7 @@
+module SAWScript.ToSAWCore where
+
+import qualified SAWScript.AST as SS
+import qualified Verifier.SAW.TypedAST as SC
+
+toSAWCore :: SS.Module SS.LType -> SC.Module
+toSAWCore = undefined
