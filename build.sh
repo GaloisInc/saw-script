6ad11e319dc9fdff9fa3bc27773a12d4785ad9f9
Aaron Tomb <atomb@galois.com>
2013-06-05 16:29:08 -0700

    Update SAWScript build script.
    Now assumes that abcBridge, jvm-parser, and llvm-pretty are cloned into
    the build subdirectory.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-cabal-dev install --force-reinstalls ../../abcBridge
-cabal-dev install --force-reinstalls ../../jvm-parser
-cabal-dev install --force-reinstalls ../../llvm-pretty
+cabal-dev install --force-reinstalls ./build/abcBridge
+cabal-dev install --force-reinstalls ./build/jvm-parser
+cabal-dev install --force-reinstalls ./build/llvm-pretty
 cabal-dev install --force-reinstalls ../Verinf
 cabal-dev install --force-reinstalls ../SAWCore
 cabal-dev install --force-reinstalls ../Java --constraint="statistics==0.10.3.1"
