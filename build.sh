53b6df02f95d7247c7d48caecec514ddeb6dc6d3
Aaron Tomb <atomb@galois.com>
2013-06-05 14:55:12 -0700

    Add preliminary build script for SAWScript.

---
+++ build.sh
@@ -0,0 +1,8 @@
+cabal-dev install --force-reinstalls ../../abcBridge
+cabal-dev install --force-reinstalls ../../jvm-parser
+cabal-dev install --force-reinstalls ../../llvm-pretty
+cabal-dev install --force-reinstalls ../Verinf
+cabal-dev install --force-reinstalls ../SAWCore
+cabal-dev install --force-reinstalls ../Java --constraint="statistics==0.10.3.1"
+cabal-dev install --force-reinstalls ../LLVM
+cabal-dev install --force-reinstalls
