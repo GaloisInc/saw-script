c1ecc0391b2fd1fd614c76cd640f0431aed342b3
Aaron Tomb <atomb@galois.com>
2013-06-10 11:28:46 -0700

    Minimal SAWCore source file execution.

--- src/SAWScript/Execution.hs
+++ src/SAWScript/Execution.hs
@@ -2,7 +2,6 @@
 module SAWScript.Execution where
 
 import Verifier.SAW.Evaluator
-import Verifier.SAW.ParserUtils ( readModuleFromFile )
 import Verifier.SAW.Prelude
 import Verifier.SAW.SharedTerm
 import Verifier.SAW.TypedAST
@@ -14,10 +13,9 @@ execSAWCore m =
     Nothing -> putStrLn "Module doesn't include a main function"
     Just mainDef -> do
       putStrLn $ "The main function has type " ++ show (defType (mainDef))
-      putStrLn $ "Would execute SAWCore module " ++ show (moduleName m)
-      sawScriptModule <- readModuleFromFile [preludeModule] "examples/prelude.sawcore"
-      (sc :: SharedContext s) <- mkSharedContext sawScriptModule
-      let global = evalGlobal sawScriptModule (allPrims global)
+      putStrLn $ "Going to execute SAWCore module " ++ show (moduleName m)
+      (sc :: SharedContext s) <- mkSharedContext m
+      let global = evalGlobal m (allPrims global)
       let t = Term (FTermF (GlobalDef mainId))
       runSC (fromValue (evalTerm global [] t :: Value s)) sc
   where mainId = (mkIdent (moduleName m) "main")
