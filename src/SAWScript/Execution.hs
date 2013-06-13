ec4b138475ec29ff48cb853f0b04d50df6814c7a
Aaron Tomb <atomb@galois.com>
2013-06-13 10:39:50 -0700

    Implement more builtin functions.
    Type-checks, but not well tested.

--- src/SAWScript/Execution.hs
+++ src/SAWScript/Execution.hs
@@ -7,16 +7,17 @@ import Verifier.SAW.SharedTerm
 import Verifier.SAW.TypedAST
 
 import SAWScript.Builtins
+import SAWScript.Options
 
-execSAWCore :: Module -> IO ()
-execSAWCore m =
+execSAWCore :: Options -> Module -> IO ()
+execSAWCore opts m =
   case findDef m mainId of
     Nothing -> putStrLn "Module doesn't include a main function"
     Just mainDef -> do
       putStrLn $ "The main function has type " ++ show (defType (mainDef))
       putStrLn $ "Going to execute SAWCore module " ++ show (moduleName m)
       (sc :: SharedContext s) <- mkSharedContext m
-      let global = evalGlobal m (allPrims global)
+      let global = evalGlobal m ((allPrims opts) global)
       let t = Term (FTermF (GlobalDef mainId))
       runSC (fromValue (evalTerm global [] t :: Value s)) sc
   where mainId = (mkIdent (moduleName m) "main")
