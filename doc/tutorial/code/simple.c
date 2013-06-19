32ea7f2f842db2ebae6d862a827c8d5aaec5771f
Aaron Tomb <atomb@galois.com>
2013-06-19 16:31:18 -0700

    Update simple SAWScript example.

--- doc/tutorial/code/simple.c
+++ doc/tutorial/code/simple.c
@@ -1,7 +1,7 @@
-int double_ref(int x, int y) {
-    return x + (y*4);
+int double_ref(int x) {
+    return x * 2;
 }
 
-int double_imp(int x, int y) {
-    return x + (y << 2);
+int double_imp(int x) {
+    return x << 1;
 }
