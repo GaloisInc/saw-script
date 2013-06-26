e6808ac2ba2e2277ab311c8b3f65033d2009d944
Kyle Carter <kcarter@galois.com>
2013-06-26 10:29:48 -0700

    Readded BuildModules.hs

---
+++ src/SAWScript/BuildModules.hs
@@ -0,0 +1,165 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TupleSections #-}
+
+module SAWScript.BuildModules
+  ( buildModules
+  , ModuleParts (..)
+  ) where
+
+import SAWScript.AST
+import SAWScript.Compiler
+import SAWScript.Import (LoadedModules (..))
+
+import Control.Applicative
+import Control.Arrow
+import Control.Monad
+--import Control.Monad.State
+--import Data.Monoid
+import Data.Foldable (foldrM)
+import qualified Data.Graph as G
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Traversable as T
+
+-- Types -----------------------------------------------------------------------
+
+type Incoming = LoadedModules
+type Outgoing = [ModuleParts CheckedExpr]
+
+type UncheckedExpr = (Maybe (ExprSimple RawT),Maybe RawSigT)
+type CheckedExpr   = ExprSimple RawT
+
+data ModuleParts e = ModuleParts
+  { modName :: ModuleName
+  , modExprEnv :: Env e
+  , modTypeEnv :: Env RawT
+  , modDeps    :: S.Set ModuleName
+  } deriving (Show)
+
+newtype ModMap e = ModMap
+  { modMap :: M.Map ModuleName (ModuleParts e)
+  } deriving (Show)
+
+-- BuildEnv --------------------------------------------------------------------
+
+buildModules :: Compiler Incoming Outgoing
+buildModules = compiler "BuildEnv" $ \ms -> T.traverse (build >=> check) >=> assemble
+  $ M.assocs $ modules ms
+
+-- stage1: build tentative environment. expression vars may or may not have bound expressions,
+--   but may not have multiple bindings.
+--build :: [TopStmtSimple RawT] -> ModuleBuilder
+build :: (ModuleName,[TopStmtSimple RawT]) -> Err (ModuleParts UncheckedExpr)
+build (mn,ts) = foldrM modBuilder (ModuleParts mn M.empty M.empty S.empty) ts
+-- stage2: force every expression var to have exactly one bound expression.
+check :: ModuleParts UncheckedExpr -> Err (ModuleParts CheckedExpr)
+check (ModuleParts mn ee te ds) = ModuleParts mn <$> traverseWithKey ensureExprPresent ee <*> pure te <*> pure ds
+-- stage3: make a module out of the resulting envs
+assemble :: [ModuleParts CheckedExpr] -> Err Outgoing
+assemble mods = return $ buildQueue modM
+  where
+  modM = ModMap $ M.fromList [ (modName m,m) | m <- mods ]
+
+
+-- Every expression name must be bound to something
+ensureExprPresent :: Name -> UncheckedExpr -> Err (ExprSimple RawT)
+ensureExprPresent n met = case met of
+  (Just e,Just t) -> return $ updateAnnotation (Just t) e
+  (Just e,_     ) -> return e
+  _               -> noBindingErr n
+
+
+modBuilder :: TopStmtSimple RawT -> ModuleParts UncheckedExpr -> Err (ModuleParts UncheckedExpr)
+modBuilder t (ModuleParts mn ee te ds) = case t of
+  -- TypeDecls may not fail
+  TopTypeDecl n pt -> case M.lookup n ee of
+                      Just (_,Just _) -> multiDeclErr n
+                      _               -> return $ ModuleParts mn (intoExprEnv (newTypeDecl pt) n ee) te ds
+  -- Multiple binds to the same name will fail
+  TopBind n e      -> case M.lookup n ee of
+                      Just (Just _,_) -> multiDeclErr n
+                      _               -> return $ ModuleParts mn (intoExprEnv (newBind e) n ee) te ds
+  -- Multiple declarations of the same type synonym will fail
+  TypeDef n pt     -> if M.member n te
+                      then multiDeclErr n
+                      else return $ ModuleParts mn ee (intoTypeEnv n (newTypeSyn pt) te) ds
+  -- Multiple declarations of an abstract type will fail
+  AbsTypeDecl n    -> if M.member n te
+                      then multiDeclErr n
+                      else return $ ModuleParts mn ee (intoTypeEnv n newAbsType te) ds
+  -- Imports show dependencies
+  Import n _ _     -> return $ ModuleParts mn ee te (addDependency n ds)
+
+-- BuildEnv --------------------------------------------------------------------
+
+intoExprEnv :: (Maybe UncheckedExpr -> UncheckedExpr) -> Name -> Env UncheckedExpr -> Env UncheckedExpr
+intoExprEnv f n = M.alter (Just . f) n
+
+intoTypeEnv :: Name -> RawT -> Env RawT -> Env RawT
+intoTypeEnv n a = M.insert n a
+
+-- If the name is bound already, add the RawSigT to the others,
+--  otherwise start a new list.
+newTypeDecl :: RawSigT -> Maybe UncheckedExpr -> UncheckedExpr
+newTypeDecl pt = maybe (Nothing,Just pt) (second (Just pt `mplus`))
+
+-- The fst of the resulting tuple will always be (Just e).
+--  If the name wasn't bound already, bind it.
+newBind :: ExprSimple RawT -> Maybe UncheckedExpr -> UncheckedExpr
+newBind e = maybe (Just e,Nothing) (first $ const $ Just e)
+
+newTypeSyn :: RawSigT -> RawT
+newTypeSyn = Just
+
+newAbsType :: RawT
+newAbsType = Nothing
+
+addDependency :: ModuleName -> S.Set ModuleName -> S.Set ModuleName
+addDependency = S.insert
+
+-- Error Messages --------------------------------------------------------------
+
+multiDeclErr :: Name -> Err a 
+multiDeclErr n = fail ("Multiple declarations of '" ++ n ++ "'")
+
+noBindingErr :: Name -> Err a
+noBindingErr n = fail ("The type signature for '" ++ n ++ "' lacks an accompanying binding.")
+
+-- Dependency Analysis ---------------------------------------------------------
+
+buildQueue :: ModMap e -> [ModuleParts e]
+buildQueue mm = map (flip findModule mm . (vertModMap M.!)) depOrder
+  where
+  modNms     = M.keys $ modMap mm
+  numMs      = length modNms - 1
+  modVertMap = M.fromList $ zip modNms [0..]
+  vertModMap = M.fromList $ zip [0..] modNms
+
+  depOrder   = G.topSort $ G.buildG bounds edges
+  bounds     = (0,numMs)
+  edges      = [ (from,to)
+               | fromM    <- modNms
+               , let from =  modVertMap M.! fromM
+               , toM      <- S.toList $ modDeps $ findModule fromM mm
+               , let to   =  modVertMap M.! toM
+               ]
+
+findModule :: ModuleName -> ModMap e -> ModuleParts e
+findModule mn mm = modMap mm M.! mn
+
+-- Backward Compatibility ------------------------------------------------------
+
+#if __GLASGOW_HASKELL__ < 706
+fromSet :: Eq k => (k -> a) -> S.Set k -> M.Map k a
+fromSet f s = M.fromAscList [ (x, f x) | x <- S.toAscList s ]
+
+traverseWithKey :: (Applicative t, Ord k) => (k -> a -> t b) -> M.Map k a -> t (M.Map k b)
+traverseWithKey f s =
+  fmap M.fromList (T.traverse (\(k, v) -> fmap ((,) k) (f k v)) (M.toList s))
+#else
+fromSet :: (k -> a) -> S.Set k -> M.Map k a
+fromSet = M.fromSet
+
+traverseWithKey :: Applicative t => (k -> a -> t b) -> M.Map k a -> t (M.Map k b)
+traverseWithKey = M.traverseWithKey
+#endif
