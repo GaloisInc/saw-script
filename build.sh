8f1f92f2c3a76dca36349e4fb3810303bd58def0
Aaron Tomb <aarontomb@gmail.com>
2013-08-16 16:00:32 -0700

    Have SAWScript build JSS with SAWCore backend.

--- build.sh
+++ build.sh
@@ -18,6 +18,6 @@ cabal-dev install --force-reinstalls ./build/jvm-parser
 cabal-dev install --force-reinstalls ./build/llvm-pretty
 cabal-dev install --force-reinstalls ../Verinf
 cabal-dev install --force-reinstalls ../SAWCore
-cabal-dev install --force-reinstalls ../Java --constraint="statistics==0.10.3.1"
+cabal-dev install --force-reinstalls ../Java --constraint="statistics==0.10.3.1" -fuse-saw
 cabal-dev install --force-reinstalls ../LLVM
 cabal-dev install --force-reinstalls
