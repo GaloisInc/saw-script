dcf3fe01249c3e5e3abb11330ac94a6d53b141e9
Aaron Tomb <atomb@galois.com>
2013-08-22 15:28:43 -0700

    Test all sub-packages when building SAWScript.

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ done
 cabal-dev install --force-reinstalls ./build/abcBridge
 cabal-dev install --force-reinstalls ./build/jvm-parser
 cabal-dev install --force-reinstalls ./build/llvm-pretty
-cabal-dev install --force-reinstalls ../Verinf
-cabal-dev install --force-reinstalls ../SAWCore
-cabal-dev install --force-reinstalls ../Java --constraint="statistics==0.10.3.1"
-cabal-dev install --force-reinstalls ../LLVM
-cabal-dev install --force-reinstalls
+cabal-dev install --force-reinstalls ../Verinf --enable-tests
+cabal-dev install --force-reinstalls ../SAWCore --enable-tests
+cabal-dev install --force-reinstalls ../Java --constraint="statistics==0.10.3.1" --enable-tests
+cabal-dev install --force-reinstalls ../LLVM --enable-tests
+cabal-dev install --force-reinstalls --enable-tests
