26e4a2bbc9885564fe9007233f41578583809e77
Brian Huffman <huffman@galois.com>
2014-04-25 14:04:29 -0700

    Rewrite scTypeOf function to use a bound variable environment
    The function now ignores the type annotations on LocalVars.

--- src/Verifier/SAW/SharedTerm.hs
+++ src/Verifier/SAW/SharedTerm.hs
@@ -287,8 +287,14 @@ scTypeOfCtor sc ident =
 -- ensuring that it is well-formed. The full typechecking should use
 -- memoization on subterms. Perhaps the fast one won't need to?
 
+-- | Computes the type of a term as quickly as possible, assuming that
+-- the term is well-typed.
 scTypeOf :: forall s. SharedContext s -> SharedTerm s -> IO (SharedTerm s)
-scTypeOf sc t0 = State.evalStateT (memo t0) Map.empty
+scTypeOf sc t0 = scTypeOf' sc [] t0
+
+-- | A version for open terms; the list argument encodes the type environment.
+scTypeOf' :: forall s. SharedContext s -> [SharedTerm s] -> SharedTerm s -> IO (SharedTerm s)
+scTypeOf' sc env t0 = State.evalStateT (memo t0) Map.empty
   where
     memo :: SharedTerm s -> State.StateT (Map TermIndex (SharedTerm s)) IO (SharedTerm s)
     memo (STApp i t) = do
@@ -300,9 +306,7 @@ scTypeOf sc t0 = State.evalStateT (memo t0) Map.empty
           State.modify (Map.insert i x)
           return x
     sort :: SharedTerm s -> State.StateT (Map TermIndex (SharedTerm s)) IO Sort
-    sort t = do
-      STApp _ (FTermF (Sort s)) <- memo t
-      return s
+    sort t = asSort =<< memo t
     termf :: TermF (SharedTerm s) -> State.StateT (Map TermIndex (SharedTerm s)) IO (SharedTerm s)
     termf tf =
       case tf of
@@ -311,15 +315,17 @@ scTypeOf sc t0 = State.evalStateT (memo t0) Map.empty
           tx <- memo x
           lift $ reducePi sc tx y
         Lambda (PVar i _ _) tp rhs -> do
-          rtp <- memo rhs
+          rtp <- lift $ scTypeOf' sc (tp : env) rhs
           lift $ scTermF sc (Pi i tp rtp)
         Lambda _ _ _ -> error "scTypeOf Lambda"
         Pi _ tp rhs -> do
           ltp <- sort tp
-          rtp <- sort rhs
+          rtp <- asSort =<< lift (scTypeOf' sc (tp : env) rhs)
           lift $ scSort sc (max ltp rtp)
         Let defs rhs -> error "scTypeOf Let" defs rhs
-        LocalVar _ tp -> return tp
+        LocalVar i _
+          | i < length env -> lift $ incVars sc 0 (i + 1) (env !! i)
+          | otherwise      -> fail $ "Dangling bound variable: " ++ show (i - length env)
         Constant _ t -> memo t
     ftermf :: FlatTermF (SharedTerm s)
            -> State.StateT (Map TermIndex (SharedTerm s)) IO (SharedTerm s)
@@ -352,6 +358,12 @@ scTypeOf sc t0 = State.evalStateT (memo t0) Map.empty
         DoubleLit{} -> lift $ scFlatTermF sc (DataTypeApp preludeDoubleIdent [])
         StringLit{} -> lift $ scFlatTermF sc (DataTypeApp preludeStringIdent [])
         ExtCns ec   -> return $ ecType ec
+
+asSort :: Monad m => SharedTerm s -> m Sort
+asSort tp =
+  case tp of
+    STApp _ (FTermF (Sort s)) -> return s
+    _ -> fail $ "Not a sort: " ++ show tp
 
 -- | The inverse function to @scSharedTerm@.
 unshare :: forall s. SharedTerm s -> Term
