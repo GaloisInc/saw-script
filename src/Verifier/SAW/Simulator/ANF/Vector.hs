cb40e72428db297a9398854abed65536de2619c4
Brian Huffman <huffman@galois.com>
2016-03-22 13:19:17 -0700

    Implement more primitives in ANF solver (unsigned comparisons, neg/add/sub)

--- src/Verifier/SAW/Simulator/ANF/Vector.hs
+++ src/Verifier/SAW/Simulator/ANF/Vector.hs
@@ -26,29 +26,63 @@ import qualified Data.Vector as V
 
 type ANFV = Vector ANF
 
+-- | Bitvector equality
 eq :: ANFV -> ANFV -> ANF
 eq x y = V.foldr ANF.conj ANF.true (V.zipWith ANF.iff x y)
 
+-- | Unsigned less-than-or-equal
 ule :: ANFV -> ANFV -> ANF
-ule = error "unimplemented: ule"
+ule xv yv = go (V.toList xv) (V.toList yv)
+  where
+    go (x : xs) (y : ys) =
+      let z = go xs ys
+      in ANF.xor (ANF.conj y z) (ANF.conj (ANF.compl x) (ANF.xor y z))
+    go _ _ = ANF.true
 
+-- | Unsigned less-than
 ult :: ANFV -> ANFV -> ANF
-ult = error "unimplemented: ult"
+ult x y = ANF.compl (ule y x)
 
+-- | Signed less-than-or-equal
 sle :: ANFV -> ANFV -> ANF
 sle = error "unimplemented: sle"
 
+-- | Signed less-than
 slt :: ANFV -> ANFV -> ANF
 slt = error "unimplemented: slt"
 
+-- | Big-endian bitvector increment with carry
+increment :: [ANF] -> (ANF, [ANF])
+increment [] = (ANF.true, [])
+increment (x : xs) = (ANF.conj x c, ANF.xor x c : ys)
+  where (c, ys) = increment xs
+
+-- | Two's complement bitvector negation
 neg :: ANFV -> ANFV
-neg = error "unimplemented: neg"
+neg x = V.fromList (snd (increment (map ANF.compl (V.toList x))))
 
+-- | 1-bit full adder
+full_adder :: ANF -> ANF -> ANF -> (ANF, ANF)
+full_adder a b c = (carry, ANF.xor (ANF.xor a b) c)
+  where carry = ANF.xor (ANF.conj a b) (ANF.conj (ANF.xor a b) c)
+
+-- | Big-endian ripple-carry adder
+ripple_carry_adder :: [ANF] -> [ANF] -> ANF -> (ANF, [ANF])
+ripple_carry_adder [] _ c = (c, [])
+ripple_carry_adder _ [] c = (c, [])
+ripple_carry_adder (x : xs) (y : ys) c = (c'', z : zs)
+  where (c', zs) = ripple_carry_adder xs ys c
+        (c'', z) = full_adder x y c'
+
+-- | Two's complement bitvector addition
 add :: ANFV -> ANFV -> ANFV
-add = error "unimplemented: add"
+add x y =
+  V.fromList (snd (ripple_carry_adder (V.toList x) (V.toList y) ANF.false))
 
+-- | Two's complement bitvector subtraction
 sub :: ANFV -> ANFV -> ANFV
-sub = error "unimplemented: sub"
+sub x y =
+  V.fromList (snd (ripple_carry_adder (V.toList x) (map ANF.compl (V.toList y)) ANF.true))
 
 mul :: ANFV -> ANFV -> ANFV
 mul = error "unimplemented: mul"
@@ -66,8 +100,8 @@ srem :: ANFV -> ANFV -> ANFV
 srem = error "unimplemented: srem"
 
 -- | Polynomial multiplication. Note that the algorithm works the same
---   no matter which endianness convention is used.  Result length is
---   @max 0 (m+n-1)@, where @m@ and @n@ are the lengths of the inputs.
+-- no matter which endianness convention is used. Result length is
+-- @max 0 (m+n-1)@, where @m@ and @n@ are the lengths of the inputs.
 pmul :: ANFV -> ANFV -> ANFV
 pmul x y = V.generate (max 0 (m + n - 1)) coeff
   where
