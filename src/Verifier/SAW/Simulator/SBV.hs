8dbfa7edbe24967fb662afd30c43b36868d95b08
Brian Huffman <huffman@galois.com>
2016-03-31 13:54:09 -0700

    Add definition for new primitive "Prelude.error"

--- src/Verifier/SAW/Simulator/SBV.hs
+++ src/Verifier/SAW/Simulator/SBV.hs
@@ -165,6 +165,7 @@ constMap = Map.fromList
   , ("Prelude.coerce", Prims.coerceOp)
   , ("Prelude.bvNat", bvNatOp)
   , ("Prelude.bvToNat", Prims.bvToNatOp)
+  , ("Prelude.error", Prims.errorOp)
   -- Overloaded
   , ("Prelude.eq", eqOp)
   ]
