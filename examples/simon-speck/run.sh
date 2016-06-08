702b1b839d0f81293dce331a27b4a80506265f82
Robby <robby@galois.com>
2016-06-08 11:26:14 -0700

    Updated run.sh.

--- examples/simon-speck/run.sh
+++ examples/simon-speck/run.sh
@@ -3,6 +3,10 @@ COMMANDS="wget clang javac saw"
 for COMMAND in ${COMMANDS}; do
         type -P ${COMMAND} &>/dev/null && continue || { >&2 echo "${COMMAND} command not found."; exit 1; }
 done
+if [ -z "$JAVA_HOME" ]; then
+    echo "Need to set JAVA_HOME environment variable"
+    exit 1
+fi  
 if [ ! -e simon.cry ]; then
   wget -q https://github.com/GaloisInc/cryptol/raw/master/examples/contrib/simon.cry
 fi
@@ -18,7 +22,9 @@ clang -o simon-64-128 simon-64-128.c
 clang -c -emit-llvm -o simon-64-128.bc simon-64-128.c
 clang -o simon-128-128 simon-128-128.c
 clang -c -emit-llvm -o simon-128-128.bc simon-128-128.c
-javac SimonEngine.java
-javac SpeckEngine.java
+javac -g SimonEngine.java
+javac -g SpeckEngine.java
 saw simon.saw
 saw speck.saw
+saw -j $JAVA_HOME/jre/lib/rt.jar speck-java-bug-1.saw
+saw -j $JAVA_HOME/jre/lib/rt.jar speck-java-bug-2.saw
