aeb528b3d45252ec0d90cce2fff716d6c85876ea
Brian Huffman <huffman@galois.com>
2016-09-23 08:31:45 -0700

    Add FirstOrder{Type,Value} datatypes, like Finite{Type,Value} plus Integer

--- src/Verifier/SAW/FiniteValue.hs
+++ src/Verifier/SAW/FiniteValue.hs
@@ -43,29 +43,73 @@ data FiniteValue
   | FVRec (Map FieldName FiniteValue)
   deriving Eq
 
+-- | First-order types that can be encoded in an SMT solver.
+data FirstOrderType
+  = FOTBit
+  | FOTInt
+  | FOTVec Nat FirstOrderType
+  | FOTTuple [FirstOrderType]
+  | FOTRec (Map FieldName FirstOrderType)
+  deriving (Eq, Show)
+
+-- | Values inhabiting those first-order types.
+data FirstOrderValue
+  = FOVBit Bool
+  | FOVInt Integer
+  | FOVWord Nat Integer -- ^ a more efficient special case for 'FOVVec FOTBit _'.
+  | FOVVec FirstOrderType [FirstOrderValue]
+  | FOVTuple [FirstOrderValue]
+  | FOVRec (Map FieldName FirstOrderValue)
+  deriving Eq
+
+toFirstOrderType :: FiniteType -> FirstOrderType
+toFirstOrderType ft =
+  case ft of
+    FTBit      -> FOTBit
+    FTVec n t  -> FOTVec n (toFirstOrderType t)
+    FTTuple ts -> FOTTuple (map toFirstOrderType ts)
+    FTRec tm   -> FOTRec (fmap toFirstOrderType tm)
+
+toFirstOrderValue :: FiniteValue -> FirstOrderValue
+toFirstOrderValue fv =
+  case fv of
+    FVBit b    -> FOVBit b
+    FVWord w i -> FOVWord w i
+    FVVec t vs -> FOVVec (toFirstOrderType t) (map toFirstOrderValue vs)
+    FVTuple vs -> FOVTuple (map toFirstOrderValue vs)
+    FVRec vm   -> FOVRec (fmap toFirstOrderValue vm)
+
 instance Show FiniteValue where
+  showsPrec p fv = showsPrec p (toFirstOrderValue fv)
+
+instance Show FirstOrderValue where
   showsPrec _ fv =
     case fv of
-      FVBit b -> shows b
-      FVWord _ x -> shows x
-      FVVec _ vs -> showString "[" . commaSep (map shows vs) . showString "]"
-      FVTuple vs -> showString "(" . commaSep (map shows vs) . showString ")"
-      FVRec vm   -> showString "{" . commaSep (map showField (Map.assocs vm)) . showString "}"
+      FOVBit b    -> shows b
+      FOVInt i    -> shows i
+      FOVWord _ x -> shows x
+      FOVVec _ vs -> showString "[" . commaSep (map shows vs) . showString "]"
+      FOVTuple vs -> showString "(" . commaSep (map shows vs) . showString ")"
+      FOVRec vm   -> showString "{" . commaSep (map showField (Map.assocs vm)) . showString "}"
     where
       commaSep ss = foldr (.) id (intersperse (showString ",") ss)
       showField (field, v) = showString field . showString " = " . shows v
 
 ppFiniteValue :: PPOpts -> FiniteValue -> Doc
-ppFiniteValue opts = loop
+ppFiniteValue opts fv = ppFirstOrderValue opts (toFirstOrderValue fv)
+
+ppFirstOrderValue :: PPOpts -> FirstOrderValue -> Doc
+ppFirstOrderValue opts = loop
  where
  loop fv = case fv of
-   FVBit b
+   FOVBit b
      | b         -> text "True"
      | otherwise -> text "False"
-   FVWord _w i   -> ppNat opts i
-   FVVec _ xs    -> brackets (sep (punctuate comma (map loop xs)))
-   FVTuple xs    -> parens (sep (punctuate comma (map loop xs)))
-   FVRec xs      -> braces (sep (punctuate comma (map ppField (Map.toList xs))))
+   FOVInt i      -> integer i
+   FOVWord _w i  -> ppNat opts i
+   FOVVec _ xs   -> brackets (sep (punctuate comma (map loop xs)))
+   FOVTuple xs   -> parens (sep (punctuate comma (map loop xs)))
+   FOVRec xs     -> braces (sep (punctuate comma (map ppField (Map.toList xs))))
       where ppField (f,x) = pretty f <+> char '=' <+> loop x
 
 
@@ -83,6 +127,20 @@ fvVec t vs =
     fromBits :: [Bool] -> Integer
     fromBits = foldl (\n b -> 2*n + if b then 1 else 0) 0
 
+-- | Smart constructor
+fovVec :: FirstOrderType -> [FirstOrderValue] -> FirstOrderValue
+fovVec t vs =
+  case (t, traverse toBit vs) of
+    (FOTBit, Just bs) -> FOVWord (fromIntegral (length bs)) (fromBits bs)
+    _ -> FOVVec t vs
+  where
+    toBit :: FirstOrderValue -> Maybe Bool
+    toBit (FOVBit b) = Just b
+    toBit _ = Nothing
+
+    fromBits :: [Bool] -> Integer
+    fromBits = foldl (\n b -> 2*n + if b then 1 else 0) 0
+
 finiteTypeOf :: FiniteValue -> FiniteType
 finiteTypeOf fv =
   case fv of
@@ -92,6 +150,17 @@ finiteTypeOf fv =
     FVTuple vs -> FTTuple (map finiteTypeOf vs)
     FVRec vm   -> FTRec (fmap finiteTypeOf vm)
 
+firstOrderTypeOf :: FirstOrderValue -> FirstOrderType
+firstOrderTypeOf fv =
+  case fv of
+    FOVBit _    -> FOTBit
+    FOVInt _    -> FOTInt
+    FOVWord n _ -> FOTVec n FOTBit
+    FOVVec t vs -> FOTVec (fromIntegral (length vs)) t
+    FOVTuple vs -> FOTTuple (map firstOrderTypeOf vs)
+    FOVRec vm   -> FOTRec (fmap firstOrderTypeOf vm)
+
+-- | Compute the number of bits in the type
 sizeFiniteType :: FiniteType -> Int
 sizeFiniteType x =
   case x of
@@ -114,6 +183,22 @@ asFiniteType sc t = do
       -> FTRec <$> traverse (asFiniteType sc) tm
     _ -> fail $ "asFiniteType: unsupported argument type: " ++ show t'
 
+asFirstOrderType :: SharedContext -> Term -> IO FirstOrderType
+asFirstOrderType sc t = do
+  t' <- scWhnf sc t
+  case t' of
+    (R.asBoolType -> Just ())
+      -> return FOTBit
+    (R.asIntegerType -> Just ())
+      -> return FOTInt
+    (R.isVecType return -> Just (n R.:*: tp))
+      -> FOTVec n <$> asFirstOrderType sc tp
+    (R.asTupleType -> Just ts)
+      -> FOTTuple <$> traverse (asFirstOrderType sc) ts
+    (R.asRecordType -> Just tm)
+      -> FOTRec <$> traverse (asFirstOrderType sc) tm
+    _ -> fail $ "asFirstOrderType: unsupported argument type: " ++ show t'
+
 asFiniteTypePure :: (Termlike t) => t -> Maybe FiniteType
 asFiniteTypePure t =
   case t of
@@ -130,26 +215,38 @@ asFiniteTypePure t =
 
 -- | Convert a finite type to a Term.
 scFiniteType :: SharedContext -> FiniteType -> IO Term
-scFiniteType sc ft =
+scFiniteType sc ft = scFirstOrderType sc (toFirstOrderType ft)
+
+-- | Convert a finite type to a Term.
+scFirstOrderType :: SharedContext -> FirstOrderType -> IO Term
+scFirstOrderType sc ft =
   case ft of
-    FTBit      -> scBoolType sc
-    FTVec n t  -> do n' <- scNat sc n
-                     t' <- scFiniteType sc t
-                     scVecType sc n' t'
-    FTTuple ts -> scTupleType sc =<< traverse (scFiniteType sc) ts
-    FTRec tm   -> scRecordType sc =<< traverse (scFiniteType sc) tm
+    FOTBit      -> scBoolType sc
+    FOTInt      -> scIntegerType sc
+    FOTVec n t  -> do n' <- scNat sc n
+                      t' <- scFirstOrderType sc t
+                      scVecType sc n' t'
+    FOTTuple ts -> scTupleType sc =<< traverse (scFirstOrderType sc) ts
+    FOTRec tm   -> scRecordType sc =<< traverse (scFirstOrderType sc) tm
 
 -- | Convert a finite value to a SharedTerm.
 scFiniteValue :: SharedContext -> FiniteValue -> IO Term
-scFiniteValue sc fv =
+scFiniteValue sc fv = scFirstOrderValue sc (toFirstOrderValue fv)
+
+-- | Convert a finite value to a SharedTerm.
+scFirstOrderValue :: SharedContext -> FirstOrderValue -> IO Term
+scFirstOrderValue sc fv =
   case fv of
-    FVBit b    -> scBool sc b
-    FVWord n x -> scBvConst sc n x
-    FVVec t vs -> do t' <- scFiniteType sc t
-                     vs' <- traverse (scFiniteValue sc) vs
-                     scVector sc t' vs'
-    FVTuple vs -> scTuple sc =<< traverse (scFiniteValue sc) vs
-    FVRec vm   -> scRecord sc =<< traverse (scFiniteValue sc) vm
+    FOVBit b    -> scBool sc b
+    FOVInt i
+      | i >= 0  -> scNatToInt sc =<< scNat sc (fromInteger i)
+      | True    -> scIntNeg sc =<< scNatToInt sc =<< scNat sc (fromInteger (- i))
+    FOVWord n x -> scBvConst sc n x
+    FOVVec t vs -> do t' <- scFirstOrderType sc t
+                      vs' <- traverse (scFirstOrderValue sc) vs
+                      scVector sc t' vs'
+    FOVTuple vs -> scTuple sc =<< traverse (scFirstOrderValue sc) vs
+    FOVRec vm   -> scRecord sc =<< traverse (scFirstOrderValue sc) vm
 
 -- Parsing values from lists of booleans ---------------------------------------
 
