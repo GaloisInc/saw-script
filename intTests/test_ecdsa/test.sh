fe1d3c44cdab39a0fafefd155a6de9305f79318a
Aaron Tomb <atomb@galois.com>
2017-04-06 15:59:23 -0700

    Remove unnecessary JDK location for ECDSA test
    Fixes running it on Windows.

--- intTests/test_ecdsa/test.sh
+++ intTests/test_ecdsa/test.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 set -e
 
-JDK=`(java -verbose 2>&1) | grep Opened | head -1 | cut -d ' ' -f 2 | cut -d ']' -f 1`
 mkdir -p tmp
 cp -r ../../examples/ecdsa/* tmp
 cd tmp
-${SAW} -j ${JDK} -j ecdsa.jar ecdsa.saw
+${SAW} -j ecdsa.jar ecdsa.saw
 cd ..
 rm -r tmp
