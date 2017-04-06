c484deff90b6809f3f7541dabc6599535f234afb
Aaron Tomb <atomb@galois.com>
2017-04-06 13:45:35 -0700

    Use ecdsa.jar for integration tests
    This avoids the need for the build machine to have `javac` installed.

--- intTests/test_ecdsa/test.sh
+++ intTests/test_ecdsa/test.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 
+JDK=`(java -verbose 2>&1) | grep Opened | head -1 | cut -d ' ' -f 2 | cut -d ']' -f 1`
 mkdir -p tmp
 cp -r ../../examples/ecdsa/* tmp
 cd tmp
-SAW=${SAW} make verify
+${SAW} -j ${JDK} -j ecdsa.jar ecdsa.saw
 cd ..
 rm -r tmp
