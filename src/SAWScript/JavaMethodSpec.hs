2b338b8f9ceebc444b95469638b9765663eb36fb
Aaron Tomb <atomb@galois.com>
2017-07-27 09:52:54 -0700

    Update for GHC 8.2.1

--- src/SAWScript/JavaMethodSpec.hs
+++ src/SAWScript/JavaMethodSpec.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternGuards #-}
