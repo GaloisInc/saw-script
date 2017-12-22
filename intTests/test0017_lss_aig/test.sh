cb483bf3eb4372b2f8a17bcfe4e968b54bcca633
Aaron Tomb <atomb@galois.com>
2017-12-22 08:39:33 -0800

    Enable branch sat checking for some tests
    With the changes to SAWCore, they were no longer terminating. We should
    identify why, since it should be easy to detect termination. Adjustments
    to default rewrite rules?

--- intTests/test0017_lss_aig/test.sh
+++ intTests/test0017_lss_aig/test.sh
@@ -2,7 +2,7 @@ set -e
 . ../sat.sh
 mkdir -p tmp
 
-$LSS --backend=saw lss_saw_backend_test.bc
+$LSS --satbranches --backend=saw lss_saw_backend_test.bc
 $LSS               lss_default_backend_test.bc
 $SAW test.saw
 
