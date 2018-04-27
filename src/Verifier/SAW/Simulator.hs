7c300f19d6391e8a9305e7038969525fb7688ba1
Eddy Westbrook <westbrook@galois.com>
2018-04-27 16:42:51 -0700

    updated simulator to new Term constructs

--- src/Verifier/SAW/Simulator.hs
+++ src/Verifier/SAW/Simulator.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE TupleSections #-}
 
 {- |
 Module      : Verifier.SAW.Simulator
@@ -32,7 +33,6 @@ import Prelude hiding (mapM)
 #if !MIN_VERSION_base(4,8,0)
 import Control.Applicative ((<$>))
 #endif
-import Control.Lens ((^.))
 import Control.Monad (foldM, liftM)
 import Control.Monad.Fix (MonadFix(mfix))
 import Control.Monad.Identity (Identity)
@@ -48,8 +48,6 @@ import qualified Data.IntMap as IMap
 import Data.Traversable
 import qualified Data.Vector as V
 --import qualified Debug.Trace as Debug
-
-import Verifier.SAW.Prelude.Constants
 
 import Verifier.SAW.SharedTerm
 import Verifier.SAW.TypedAST
@@ -77,104 +75,19 @@ data SimulatorConfig l =
 ------------------------------------------------------------
 -- Evaluation of function definitions
 
-{-# SPECIALIZE
-  matchThunk :: Pat -> ThunkIn Id l -> Id (Maybe (Map Int (ThunkIn Id l))) #-}
-{-# SPECIALIZE
-  matchThunk :: Pat -> ThunkIn IO l -> IO (Maybe (Map Int (ThunkIn IO l))) #-}
-
--- | Pattern matching for values.
-matchThunk :: VMonad l => Pat -> Thunk l -> EvalM l (Maybe (Map Int (Thunk l)))
-matchThunk p x =
-  case p of
-    PVar _ i _  -> return $ Just (Map.singleton i x)
-    PUnused _ _ -> return $ Just Map.empty
-    PUnit       -> do v <- force x
-                      case v of
-                        VUnit -> matchThunks [] []
-                        _ -> return Nothing
-    PPair p1 p2 -> do v <- force x
-                      case v of
-                        VPair x1 x2 -> matchThunks [p1, p2] [x1, x2]
-                        _ -> return Nothing
-    PEmpty      -> do v <- force x
-                      case v of
-                        VEmpty -> matchThunks [] []
-                        _ -> return Nothing
-    PField p1 p2 p3 -> do v <- force x
-                          case v of
-                            VField x1 x2 x3 -> matchThunks [p1, p2, p3] [ready (VString x1), x2, ready x3]
-                            _ -> return Nothing
-    PCtor i ps  -> do v <- force x
-                      case v of
-                        VCtorApp s xv | i == s -> matchThunks ps (V.toList xv)
-                        VNat 0 | i == preludeZeroIdent -> matchThunks ps []
-                        VNat n | i == preludeSuccIdent ->
-                          matchThunks ps [ready (VNat (n-1))]
-                        _                      -> return Nothing
-    PString s   -> do v <- force x
-                      case v of
-                        VString s' | s == s' -> matchThunks [] []
-                        _ -> return Nothing
-
-{-# SPECIALIZE
-  matchThunks ::
-    [Pat] -> [ThunkIn Id l] -> Id (Maybe (Map Int (ThunkIn Id l))) #-}
-{-# SPECIALIZE
-  matchThunks ::
-    [Pat] -> [ThunkIn IO l] -> IO (Maybe (Map Int (ThunkIn IO l))) #-}
-
--- | Simultaneous pattern matching for lists of values.
-matchThunks :: VMonad l =>
-  [Pat] -> [Thunk l] -> EvalM l (Maybe (Map Int (Thunk l)))
-matchThunks [] [] = return $ Just Map.empty
-matchThunks [] (_ : _) = return Nothing
-matchThunks (_ : _) [] = return Nothing
-matchThunks (p : ps) (x : xs) = do
-  mm1 <- matchThunk p x
-  case mm1 of
-    Nothing -> return Nothing
-    Just m1 -> do
-      mm2 <- matchThunks ps xs
-      case mm2 of
-        Nothing -> return Nothing
-        Just m2 -> return $ Just (Map.union m1 m2)
-
-
 {-# SPECIALIZE evalDef :: (Term -> OpenValueIn Id l) -> Def -> MValueIn Id l #-}
 {-# SPECIALIZE evalDef :: (Term -> OpenValueIn IO l) -> Def -> MValueIn IO l #-}
 
 -- | Evaluator for pattern-matching function definitions,
 -- parameterized by an evaluator for right-hand sides.
 evalDef :: forall l. VMonad l => (Term -> OpenValue l) -> Def -> MValue l
-evalDef rec (Def ident NoQualifier _ eqns) = vFuns [] arity
-  where
-    arity :: Int
-    arity = lengthDefEqn (head eqns)
-
-    lengthDefEqn :: DefEqn -> Int
-    lengthDefEqn (DefEqn ps _) = length ps
-
-    vFuns :: [Thunk l] -> Int -> MValue l
-    vFuns xs 0 = tryEqns eqns (reverse xs)
-    vFuns xs n = return $ VFun (\x -> vFuns (x : xs) (n - 1))
-
-    tryEqns :: [DefEqn] -> [Thunk l] -> MValue l
-    tryEqns [] _ = fail $ "Pattern match failure: " ++ show ident
-    tryEqns (eqn : eqns') xs =
-      do mm <- tryEqn eqn xs
-         case mm of
-           Just m -> return m
-           Nothing -> tryEqns eqns' xs
-
-    tryEqn :: DefEqn -> [Thunk l] -> EvalM l (Maybe (Value l))
-    tryEqn (DefEqn ps rhs) xs =
-      do minst <- matchThunks ps xs
-         case minst of
-           Nothing -> return Nothing
-           Just inst -> let env = reverse (Map.elems inst) in liftM Just (rec rhs env)
-
-evalDef _ (Def ident PrimQualifier _ _) = fail $ unwords ["attempted to evaluate primitive", show ident]
-evalDef _ (Def ident AxiomQualifier _ _) = fail $ unwords ["attempted to evaluate axiom", show ident]
+evalDef recEval (Def _ NoQualifier _ (Just body)) = recEval body []
+evalDef _ (Def ident NoQualifier _ Nothing) =
+  fail $ unwords ["attempted to evaluate definition with no body", show ident]
+evalDef _ (Def ident PrimQualifier _ _) =
+  fail $ unwords ["attempted to evaluate primitive", show ident]
+evalDef _ (Def ident AxiomQualifier _ _) =
+  fail $ unwords ["attempted to evaluate axiom", show ident]
 
 
 ------------------------------------------------------------
@@ -205,58 +118,68 @@ evalTermF :: forall l. (VMonadLazy l, MonadFix (EvalM l), Show (Extra l)) =>
           -> (Term -> OpenValue l)      -- ^ Evaluator for subterms under binders
           -> (Term -> MValue l)         -- ^ Evaluator for subterms in the same bound variable context
           -> TermF Term -> OpenValue l
-evalTermF cfg lam rec tf env =
+evalTermF cfg lam recEval tf env =
   case tf of
-    App t1 t2               -> do v <- rec t1
-                                  x <- rec' t2
+    App t1 t2               -> do v <- recEval t1
+                                  x <- recEvalDelay t2
                                   apply v x
     Lambda _ _ t            -> return $ VFun (\x -> lam t (x : env))
-    Pi _ t1 t2              -> do v <- rec t1
+    Pi _ t1 t2              -> do v <- recEval t1
                                   return $ VPiType v (\x -> lam t2 (x : env))
     LocalVar i              -> force (env !! i)
-    Constant i t ty         -> do v <- rec ty
-                                  maybe (rec t) id (simUninterpreted cfg i v)
+    Constant i t ty         -> do v <- recEval ty
+                                  maybe (recEval t) id (simUninterpreted cfg i v)
     FTermF ftf              ->
       case ftf of
         GlobalDef ident     -> simGlobal cfg ident
         UnitValue           -> return VUnit
         UnitType            -> return VUnitType
-        PairValue x y       -> do tx <- rec' x
-                                  ty <- rec' y
+        PairValue x y       -> do tx <- recEvalDelay x
+                                  ty <- recEvalDelay y
                                   return $ VPair tx ty
-        PairType x y        -> do vx <- rec x
-                                  vy <- rec y
+        PairType x y        -> do vx <- recEval x
+                                  vy <- recEval y
                                   return $ VPairType vx vy
-        PairLeft x          -> valPairLeft =<< rec x
-        PairRight x         -> valPairRight =<< rec x
+        PairLeft x          -> valPairLeft =<< recEval x
+        PairRight x         -> valPairRight =<< recEval x
         EmptyValue          -> return VEmpty
         EmptyType           -> return VEmptyType
-        FieldValue f x y    -> do VString s <- rec f
-                                  tx <- rec' x
-                                  vy <- rec y
+        FieldValue f x y    -> do VString s <- recEval f
+                                  tx <- recEvalDelay x
+                                  vy <- recEval y
                                   return $ VField s tx vy
-        FieldType f x y     -> do VString s <- rec f
-                                  vx <- rec x
-                                  vy <- rec y
+        FieldType f x y     -> do VString s <- recEval f
+                                  vx <- recEval x
+                                  vy <- recEval y
                                   return $ VFieldType s vx vy
-        RecordSelector t k  -> do vt <- rec t
-                                  VString s <- rec k
+        RecordSelector t k  -> do vt <- recEval t
+                                  VString s <- recEval k
                                   valRecordSelect s vt
-        CtorApp ident ts    -> do v <- simGlobal cfg ident
-                                  xs <- mapM rec' ts
-                                  foldM apply v xs
-        DataTypeApp i ts    -> liftM (VDataType i) $ mapM rec ts
+        CtorApp ident ps ts -> do v <- simGlobal cfg ident
+                                  ps' <- mapM recEvalDelay ps
+                                  ts' <- mapM recEvalDelay ts
+                                  foldM apply v (ps' ++ ts')
+        DataTypeApp i ps ts -> liftM (VDataType i) $ mapM recEval (ps ++ ts)
+        RecursorApp d ps p_ret cs_fs ixs arg ->
+          VRecursorApp d <$> mapM recEval ps <*> recEval p_ret <*>
+          mapM (\(c,f) -> (c,) <$> recEval f) cs_fs <*>
+          mapM recEval ixs <*> recEval arg
+        RecordType elem_tps ->
+          VRecordType <$> mapM (\(fld,t) -> (fld,) <$> recEval t) elem_tps
+        RecordValue elems   ->
+          VRecordValue <$> mapM (\(fld,t) -> (fld,) <$> recEval t) elems
+        RecordProj t fld    -> VRecordProj <$> recEval t <*> return fld
         Sort {}             -> return VType
         NatLit n            -> return $ VNat n
-        ArrayValue _ tv     -> liftM VVector $ mapM rec' tv
+        ArrayValue _ tv     -> liftM VVector $ mapM recEvalDelay tv
         FloatLit float      -> return $ VFloat float
         DoubleLit double    -> return $ VDouble double
         StringLit s         -> return $ VString s
-        ExtCns ec           -> do v <- rec (ecType ec)
+        ExtCns ec           -> do v <- recEval (ecType ec)
                                   simExtCns cfg (ecVarIndex ec) (ecName ec) v
   where
-    rec' :: Term -> EvalM l (Thunk l)
-    rec' = delay . rec
+    recEvalDelay :: Term -> EvalM l (Thunk l)
+    recEvalDelay = delay . recEval
 
 
 {-# SPECIALIZE evalTerm ::
@@ -267,10 +190,10 @@ evalTermF cfg lam rec tf env =
 -- | Evaluator for unshared terms.
 evalTerm :: (VMonadLazy l, MonadFix (EvalM l), Show (Extra l)) =>
             SimulatorConfig l -> Term -> OpenValue l
-evalTerm cfg t env = evalTermF cfg lam rec (unwrapTermF t) env
+evalTerm cfg t env = evalTermF cfg lam recEval (unwrapTermF t) env
   where
     lam = evalTerm cfg
-    rec t' = evalTerm cfg t' env
+    recEval t' = evalTerm cfg t' env
 
 {-# SPECIALIZE evalTypedDef ::
   Show (Extra l) => SimulatorConfigIn Id l -> Def -> MValueIn Id l #-}
@@ -283,31 +206,31 @@ evalTypedDef cfg = evalDef (evalTerm cfg)
 
 {-# SPECIALIZE evalGlobal ::
   Show (Extra l) =>
-  Module ->
+  ModuleMap ->
   Map Ident (ValueIn Id l) ->
   (VarIndex -> String -> ValueIn Id l -> MValueIn Id l) ->
   (String -> ValueIn Id l -> Maybe (MValueIn Id l)) ->
   Id (SimulatorConfigIn Id l) #-}
 {-# SPECIALIZE evalGlobal ::
   Show (Extra l) =>
-  Module ->
+  ModuleMap ->
   Map Ident (ValueIn IO l) ->
   (VarIndex -> String -> ValueIn IO l -> MValueIn IO l) ->
   (String -> ValueIn IO l -> Maybe (MValueIn IO l)) ->
   IO (SimulatorConfigIn IO l) #-}
 evalGlobal :: forall l. (VMonadLazy l, MonadFix (EvalM l), Show (Extra l)) =>
-              Module -> Map Ident (Value l) ->
+              ModuleMap -> Map Ident (Value l) ->
               (VarIndex -> String -> Value l -> MValue l) ->
               (String -> Value l -> Maybe (EvalM l (Value l))) ->
               EvalM l (SimulatorConfig l)
-evalGlobal m0 prims extcns uninterpreted = do
-   checkPrimitives m0 prims
+evalGlobal modmap prims extcns uninterpreted = do
+   checkPrimitives modmap prims
    mfix $ \cfg -> do
      thunks <- mapM delay (globals cfg)
      return (SimulatorConfig (global thunks) extcns uninterpreted)
   where
     ms :: [Module]
-    ms = m0 : Map.elems (m0^.moduleImports)
+    ms = HashMap.elems modmap
 
     global :: HashMap Ident (Thunk l) -> Ident -> MValue l
     global thunks ident =
@@ -321,7 +244,7 @@ evalGlobal m0 prims extcns uninterpreted = do
       [ (ctorName ct, return (vCtor (ctorName ct) [] (ctorType ct))) |
         m <- ms, ct <- moduleCtors m ] ++
       [ (defIdent td, evalTypedDef cfg td) |
-        m <- ms, td <- moduleDefs m, not (null (defEqs td)) ] ++
+        m <- ms, td <- moduleDefs m, defBody td /= Nothing ] ++
       Map.assocs (fmap return prims) -- Later mappings take precedence
 
     -- Convert a constructor to a value by creating a function that takes in one
@@ -338,10 +261,10 @@ noExtCns _ name _ = fail $ "evalTermF ExtCns unimplemented (" ++ name ++ ")"
 --   are implemented, and that terms with implementations are not
 --   overridden.
 checkPrimitives :: forall l. (VMonadLazy l, MonadFix (EvalM l), Show (Extra l))
-                => Module
+                => ModuleMap
                 -> Map Ident (Value l)
                 -> EvalM l ()
-checkPrimitives m0 prims = do
+checkPrimitives modmap prims = do
    -- FIXME this is downgraded to a warning temporarily while we work out a
    -- solution to issue GaloisInc/saw-script#48
    --   when (not $ null unimplementedPrims) (fail $ unimplementedMsg)
@@ -354,8 +277,8 @@ checkPrimitives m0 prims = do
         _overrideMsg = unwords $
             ("WARNING overridden definitions:" : (map show overridePrims))
 
-        primSet = Set.fromList $ map defIdent $ allModulePrimitives m0
-        defSet  = Set.fromList $ map defIdent $ allModuleActualDefs m0
+        primSet = Set.fromList $ map defIdent $ allModulePrimitives modmap
+        defSet  = Set.fromList $ map defIdent $ allModuleActualDefs modmap
         implementedPrims = Map.keysSet prims
 
         unimplementedPrims = Set.toList $ Set.difference primSet implementedPrims
@@ -431,11 +354,11 @@ evalClosedTermF :: (VMonadLazy l, MonadFix (EvalM l), Show (Extra l)) =>
                    SimulatorConfig l
                 -> IntMap (Thunk l)
                 -> TermF Term -> MValue l
-evalClosedTermF cfg memoClosed tf = evalTermF cfg lam rec tf []
+evalClosedTermF cfg memoClosed tf = evalTermF cfg lam recEval tf []
   where
     lam = evalOpen cfg memoClosed
-    rec (Unshared tf') = evalTermF cfg lam rec tf' []
-    rec (STApp{ stAppIndex = i }) =
+    recEval (Unshared tf') = evalTermF cfg lam recEval tf' []
+    recEval (STApp{ stAppIndex = i }) =
       case IMap.lookup i memoClosed of
         Just x -> force x
         Nothing -> fail "evalClosedTermF: internal error"
@@ -501,11 +424,11 @@ evalLocalTermF :: (VMonadLazy l, MonadFix (EvalM l), Show (Extra l)) =>
                    SimulatorConfig l
                 -> IntMap (Thunk l) -> IntMap (Thunk l)
                 -> TermF Term -> OpenValue l
-evalLocalTermF cfg memoClosed memoLocal tf0 env = evalTermF cfg lam rec tf0 env
+evalLocalTermF cfg memoClosed memoLocal tf0 env = evalTermF cfg lam recEval tf0 env
   where
     lam = evalOpen cfg memoClosed
-    rec (Unshared tf) = evalTermF cfg lam rec tf env
-    rec (STApp{ stAppIndex = i }) =
+    recEval (Unshared tf) = evalTermF cfg lam recEval tf env
+    recEval (STApp{ stAppIndex = i }) =
       case IMap.lookup i memoLocal of
         Just x -> force x
         Nothing -> fail "evalLocalTermF: internal error"
