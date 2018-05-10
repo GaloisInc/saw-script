3668afad2ddb542b0490c9f14dd086f2e94c7f7d
Eddy Westbrook <westbrook@galois.com>
2018-05-10 11:56:20 -0700

    updated Prelude.hs to use new, simpler defineModuleFromFileWithFns function

--- src/Verifier/SAW/Prelude.hs
+++ src/Verifier/SAW/Prelude.hs
@@ -20,13 +20,9 @@ import Verifier.SAW.ParserUtils
 import Verifier.SAW.Prelude.Constants
 import Verifier.SAW.SharedTerm
 
-$(runDecWriter $ do
-    prelude <- defineModuleFromFile [] "preludeModule" "prelude/Prelude.sawcore"
-    declareSharedModuleFns "Prelude" (decVal prelude)
- )
+$(defineModuleFromFileWithFns "preludeModule" "prelude/Prelude.sawcore")
 
 scEq :: SharedContext -> Term -> Term -> IO Term
 scEq sc x y = do
   xty <- scTypeOf sc x
-  eqOp <- scApplyPrelude_eq sc
-  eqOp xty x y
+  scApplyPrelude_eq sc xty x y
