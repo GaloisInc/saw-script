54b89d3fb52b707f1427cae00d0791f154bb3f45
Eddy Westbrook <westbrook@galois.com>
2018-05-25 13:15:18 -0700

    added compile-time type-checking for the Prelude, as a sanity check

--- src/Verifier/SAW.hs
+++ src/Verifier/SAW.hs
@@ -7,6 +7,8 @@ Stability   : experimental
 Portability : non-portable (language extensions)
 -}
 
+{-# LANGUAGE TemplateHaskell #-}
+
 module Verifier.SAW
   ( module Verifier.SAW.SharedTerm
   , module Verifier.SAW.ExternalFormat
@@ -18,3 +20,7 @@ module Verifier.SAW
 import Verifier.SAW.SharedTerm
 import Verifier.SAW.Prelude
 import Verifier.SAW.ExternalFormat
+
+-- The following type-checks the Prelude at compile time, as a sanity check
+import Language.Haskell.TH
+$(runIO (mkSharedContext >>= \sc -> scLoadPreludeModule sc >> return []))
