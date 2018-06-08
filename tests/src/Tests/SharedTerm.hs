f262e6afc940fe2e686a719001a2c731f9def74a
Brian Huffman <huffman@galois.com>
2018-06-08 08:51:26 -0700

    Adapt tests to Bool becoming a primitive.

--- tests/src/Tests/SharedTerm.hs
+++ tests/src/Tests/SharedTerm.hs
@@ -6,7 +6,7 @@ Stability   : experimental
 Portability : non-portable (language extensions)
 -}
 
-module Tests.SharedTerm 
+module Tests.SharedTerm
   ( sharedTermTests
   ) where
 
@@ -34,5 +34,5 @@ preludeSharedSmokeTest :: TestTree
 preludeSharedSmokeTest =
   testCase "preludeSharedSmokeTest" $ do
     sc <- mkSharedContext preludeModule
-    void $ scPrelude_Bool sc
+    void $ scApplyPrelude_Bool sc
     return ()
