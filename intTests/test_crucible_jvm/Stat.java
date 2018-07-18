f5236eec563f91eccc4e6f9a4102ade382c394fc
Stephanie Weirich <sweirich@galois.com>
2018-07-18 09:40:42 -0700

    test runner shows jvm bytecode (and recompiles)

--- intTests/test_crucible_jvm/Stat.java
+++ intTests/test_crucible_jvm/Stat.java
@@ -1,10 +1,12 @@
-class STAT {
-    static int y = 0;
+class Stat {
+    static int y = 1+1;
 
     static int g_imp (int x) { return 0; }
     
     static int f_imp (int x) { return g_imp(x) + 1; }
 
-    static int f_ref (int x) { return 1; }
+    static int f_ref (int x) { return 3; }
+
+    public Stat () {} 
 }
 	
