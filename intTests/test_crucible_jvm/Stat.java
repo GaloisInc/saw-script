aaf45570043c48f99f5c1d978f1b8c8d5afff55f
Stephanie Weirich <sweirich@galois.com>
2018-07-24 14:44:47 -0700

    updated test cases

--- intTests/test_crucible_jvm/Stat.java
+++ intTests/test_crucible_jvm/Stat.java
@@ -1,12 +1,12 @@
 class Stat {
-    static int y = 1+1;
+    static int y = 1+3;
 
-    static int g_imp (int x) { return y; }
+    static int g_imp (int x) { return x+y; }
     
-    static int f_imp (int x) { return g_imp(x) + 1; }
+    static int f_imp (int x) { return y + 1; }
 
-    static int f_ref (int x) { return 4; }
+    static int f_ref (int x) { return 5; }
 
-    public Stat () {} 
+    //public Stat () {} 
 }
 	
--- intTests/test_crucible_jvm/test.sh
+++ intTests/test_crucible_jvm/test.sh
@@ -1,3 +1,6 @@
+make Stat.class
 make Dyn.class
-javap -c Dyn
-stack exec saw dyn_crucible.saw
+javap -c Stat
+cabal new-exec saw stat_crucible.saw
+cabal new-exec saw dyn_crucible.saw
+cabal new-exec saw statdyn_crucible.saw
