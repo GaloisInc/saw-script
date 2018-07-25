3dcf79e7a139c6c46d95fd29ab4a1b91124007f9
Stephanie Weirich <sweirich@galois.com>
2018-07-25 16:55:52 -0700

    new test cases for invokevirtual
    Tests for invoking methods through and interface, and invoking
    overridden methods.

--- intTests/test_crucible_jvm/Dyn.java
+++ intTests/test_crucible_jvm/Dyn.java
@@ -1,12 +1,12 @@
-class Dyn {
-    int x = 0;
-
+class Dyn implements Iface {
+    int x;
     Dyn d;
 
-    int g (int y) {
-	return x + y;
+    public int g (int y) {
+	return this.x + y;
     }
-    
+
+    // instance variable access
     static int f_imp (int y) {
 	Dyn d = new Dyn();
 	return d.x + y;
@@ -18,26 +18,35 @@ class Dyn {
     }
 
     
-    static int f_ref (int y) { return y; }
+    static int f_ref (int y) { return y + 2; }
 
     Dyn() {
-	this.x = 0;
+	this.x = 2;
 	this.d = null;
     }
 
     
-    Dyn(int y) {
+    Dyn(int y, Dyn b0) {
 	this.x = y;
 	this.d = new Dyn();
     }
 
     static int h_imp (int y) {
-	Dyn d1 = new Dyn (y);
+	Dyn d1 = new Dyn ();
+	d1.d = new Dyn(y,null);
 	return d1.d.g(4);
     }
 
     static int h_ref (int y) {
 	return y+4;
+    }
+
+
+    static int i_imp (int y) {
+	Iface d = new Dyn ();
+
+	return d.g(y);
+
     }
    
     
--- intTests/test_crucible_jvm/test.sh
+++ intTests/test_crucible_jvm/test.sh
@@ -1,6 +1,6 @@
 make Stat.class
 make Dyn.class
-javap -c Stat
-cabal new-exec saw stat_crucible.saw
-cabal new-exec saw dyn_crucible.saw
-cabal new-exec saw statdyn_crucible.saw
+make Sub.class
+javap -c Sub
+cabal new-exec saw sub_crucible.saw
+
