ce6670170a696c55d809c1d93b3b3ea956b00de4
Kevin Quick <kquick@galois.com>
2018-09-05 10:39:02 -0700

    Handle build configuration without git or checkout.

--- Setup.hs
+++ Setup.hs
@@ -1,10 +1,12 @@
+import Control.Exception
 import Control.Monad (unless)
 import Distribution.Simple
 import Distribution.Simple.BuildPaths (autogenPackageModulesDir)
 import Distribution.PackageDescription (emptyHookedBuildInfo, allBuildInfo)
-import System.Directory (createDirectoryIfMissing)
+import System.Directory (createDirectoryIfMissing, findExecutable)
 import System.FilePath ((</>))
-import System.Process (readProcess)
+import System.Process (readProcessWithExitCode)
+import System.Exit
 
 main = defaultMainWithHooks myHooks
   where myHooks = simpleUserHooks { buildHook = myBuild }
@@ -13,7 +15,16 @@ myBuild pd lbi uh flags = do
   let dir = autogenPackageModulesDir lbi
   createDirectoryIfMissing True dir
 
-  desc <- readProcess "git" ["describe", "--always", "--dirty"] ""
+  hasGit <- findExecutable "git"
+
+  let gitfailure :: SomeException -> IO (ExitCode, String, String)
+      gitfailure _e = return (ExitFailure 1, "<non-dev-build> ", "")
+      middle (_,v,_) = v
+
+  desc <- case hasGit of
+            Just git -> middle <$> readProcessWithExitCode "git" ["describe", "--always", "--dirty"] ""
+                                   `catch` gitfailure
+            Nothing -> return "<VCS-less build> "
 
   writeFile (dir </> "GitRev.hs") $ unlines
     [ "module GitRev where"
