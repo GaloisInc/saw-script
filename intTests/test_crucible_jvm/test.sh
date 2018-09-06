1444faa9536ef784ba1379f09cf2dd20d02a62fb
Stephanie Weirich <sweirich@galois.com>
2018-09-06 15:55:08 -0400

    update with RTJAR location

--- intTests/test_crucible_jvm/test.sh
+++ intTests/test_crucible_jvm/test.sh
@@ -1,14 +1,17 @@
-## rt.jar file
-#make Stat.class
-#make Dyn.class
-#make Sub.class
-#make Arr.class
-#make Obj.class
-#make TestStr.class
-# javap -c TestStr.class
-$SAW stat_crucible.saw
-$SAW dyn_crucible.saw
-$SAW sub_crucible.saw
-$SAW teststr_crucible.saw
-# $SAW -j $RTJAR arr_crucible.saw
+## location of rt.jar file
+## this will probably need to change for your system
+RTJAR=/Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home/jre/lib/rt.jar
+SAW="cabal new-exec saw -- "
+
+make Stat.class
+make Dyn.class
+make Sub.class
+make Arr.class
+make Obj.class
+
+$SAW --jars $RTJAR stat_crucible.saw
+$SAW --jars $RTJAR dyn_crucible.saw
+$SAW --jars $RTJAR sub_crucible.saw
+$SAW --jars $RTJAR teststr_crucible.saw
+
 
