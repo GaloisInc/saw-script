ae3f104e53bb6b1f0d2420998e69cb5498bf89b9
Eddy Westbrook <westbrook@galois.com>
2018-10-12 14:22:46 -0700

    more work on SepTypes.hs...?

--- src/SAWScript/SepTypes.hs
+++ src/SAWScript/SepTypes.hs
@@ -27,7 +27,7 @@ import Lang.Crucible.CFG.Core
 import Lang.Crucible.Types
 import Lang.Crucible.LLVM.Extension
 
-import qualified Data.Parameterized.Context as Ctx
+import Data.Parameterized.Context
 import Data.Parameterized.Ctx
 
 import SAWScript.CrucibleLLVM
@@ -35,6 +35,12 @@ import SAWScript.CrucibleBuiltins
 import SAWScript.TopLevel
 import SAWScript.Value
 import SAWScript.Options
+
+
+----------------------------------------------------------------------
+-- * Representing Permissions
+----------------------------------------------------------------------
+
 
 -- | A "proof" that the given 'CrucibleType' is "functional", meaning that it
 -- contains no references or function types, and so can be directly represented
@@ -50,7 +56,7 @@ data FunCrucibleType (tp :: CrucibleType) where
 
 -- FIXME: for recursive types, require the unrolling to be functional...
 
-type FunCrucibleCtx = Ctx.Assignment FunCrucibleType
+type FunCrucibleCtx = Assignment FunCrucibleType
 
 -- | Convert a functional Crucible type to a SAW type
 funTypeToSAW :: FunCrucibleType tp -> OpenTermM OpenTerm
@@ -60,13 +66,13 @@ funTypeToSAW _ = error "funTypeToSAW unimplemented!"
 -- forms here, namely, constants, variables, and a few arithmetic operations,
 -- because some of the memory operations need to know this information; e.g.,
 -- @malloc@ must know the size of the memory being allocated.
-data StaticExpr tp where
+data StaticExpr (ctx :: Ctx CrucibleType) tp where
   -- | A Crucible variable for an LLVM value that we know is related to the
   -- given SAW variable
-  StaticLLVMVar :: HasPtrWidth w => OpenTerm -> StaticExpr (LLVMPointerType w)
+  StaticLLVMVar :: HasPtrWidth w => Reg ctx tp -> StaticExpr tp
 
   -- | A bitvector literal
-  StaticBVLit :: (1 <= w) => NatRepr w -> Integer -> StaticExpr (BVType w)
+  StaticBVLit :: (1 <= w) => NatRepr w -> Integer -> StaticExpr ctx (BVType w)
   -- | A bitvector addition
   StaticBVAdd :: (1 <= w) => NatRepr w -> StaticExpr (BVType w) ->
                  StaticExpr (BVType w) -> StaticExpr (BVType w)
@@ -86,59 +92,66 @@ data StaticExpr tp where
   StaticString :: Text -> StaticExpr StringType
 
   -- | A static struct
-  StaticStruct :: Ctx.Assignment StaticExpr ctx -> StaticExpr (StructType ctx)
+  StaticStruct :: Assignment StaticExpr s_ctx ->
+                  StaticExpr (StructType s_ctx)
 
   -- | A statically-rolled LLVM_pointer recursive value
-  StaticRollLLVM :: HasPtrWidth w =>
-                    StaticExpr (StructType (EmptyCtx ::> NatType ::> BVType w)) ->
-                    StaticExpr (LLVMPointerType w)
+  StaticRollLLVM ::
+    HasPtrWidth w =>
+    StaticExpr (StructType (EmptyCtx ::> NatType ::> BVType w)) ->
+    StaticExpr (LLVMPointerType w)
 
 
 -- | A value permission is a relation between a Crucible value and a SAW value,
 -- along with a specification of what updates are allowed to the Crucible state
 -- via the value
-data ValuePerm (tp :: CrucibleType) where
+data ValuePerm (ctx :: Ctx CrucibleType) (tp :: CrucibleType) where
   -- | Represents a functional (i.e., with no references or functions) Crucible
   -- value directly in SAW as itself, with an optional static expression that
   -- gives the SAW value if it is known
-  FunValuePerm :: FunCrucibleType tp -> Maybe (StaticExpr tp) -> ValuePerm tp
+  FunValuePerm :: FunCrucibleType tp -> Maybe (StaticExpr tp) ->
+                  ValuePerm ctx tp
 
   -- | Represents LLVM values of a fixed, known value as the unit object
   LLVMFixedWordPerm :: (1 <= w) => NatRepr w -> Integer ->
-                       ValuePerm (LLVMPointerType w)
+                       ValuePerm ctx (LLVMPointerType w)
 
-  -- | Represents an LLVM value that points into the heap
-  LLVMPtrPerm :: (1 <= w) => NatRepr w -> Maybe String -> [LLVMFieldPerm w] ->
-                 ValuePerm (LLVMPointerType w)
+  -- | Represents an LLVM value that points to a struct in the heap
+  LLVMStructPerm :: (1 <= w) => NatRepr w -> Maybe String ->
+                    [LLVMFieldPerm ctx w] ->
+                    ValuePerm ctx (LLVMPointerType w)
 
   -- | Represents an LLVM value as either one or the other representations
-  LLVMSumPerm :: ValuePerm (LLVMPointerType w) ->
-                 ValuePerm (LLVMPointerType w) ->
-                 ValuePerm (LLVMPointerType w)
+  LLVMSumPerm :: ValuePerm ctx (LLVMPointerType w) ->
+                 ValuePerm ctx (LLVMPointerType w) ->
+                 ValuePerm ctx (LLVMPointerType w)
 
 
 -- FIXME HERE: lifetime modality for perms
 
 
-data LLVMFieldPerm w =
+data LLVMFieldPerm ctx w =
   LLVMFieldPerm
-  { fieldPermPerm :: Either (ValuePerm (LLVMPointerType w)) String,
+  { fieldPermPerm :: Either (ValuePerm ctx (LLVMPointerType w)) String,
     -- ^ Either a value permission or a recursive permission that refers to an
     -- enclosing 'LLVMPtrPerm' by name
     fieldPermLevel :: LLVMFieldPermLevel,
     -- ^ Whether we currently hold write, read, or no perms to this field
-    fieldPermBorrow :: Maybe BorrowDest
+    fieldPermBorrow :: Maybe (BorrowDest ctx w)
     -- ^ Whether this field perm is currently being borrowed
   }
 
 data LLVMFieldPermLevel = FieldLvlW | FieldLvlR | FieldLvlNone
 
- -- FIXME HERE: how to refer to local Crucible variables in the following? Do we
- -- need all our perms to be indexed by a Crucible ctx?
-data BorrowDest
-data BorrowSrc
+-- | The register that borrowed a permission
+newtype BorrowDest ctx w = BorrowDest (Reg ctx (LLVMPointerType w))
 
--- FIXME HERE: need to refer to local Crucible variables here too...
+-- | The field, i.e., pointer + offset, from which a permission was borrowed
+data BorrowSrc ctx w
+     -- | A borrow from a field, given by number, of a struct permission
+  = BorrowSrcField (Reg ctx (LLVMPointerType w)) Int
+
+-- FIXME HERE: define top-level perms
 data Perm
 
 -- FIXME HERE: GlobalSymbol is not exported from crucible-llvm, so this is just
@@ -146,15 +159,19 @@ data Perm
 data GlobalSymbol
 
 -- | A permission to a global symbol
-data GlobalPerm where
-  GlobalPerm :: GlobalSymbol -> ValuePerm tp -> GlobalPerm
+data GlobalPerm ctx where
+  GlobalPerm :: HasPtrWidth w => GlobalSymbol ->
+                ValuePerm ctx (LLVMPointerType w) ->
+                GlobalPerm ctx
 
 data PermSet (ctx :: Ctx CrucibleType) =
   PermSet
-  { permSetVars :: Ctx.Assignment ValuePerm ctx
-  , permSetConsts :: [GlobalPerm] }
+  { permSetVars :: Assignment (ValuePerm ctx) ctx
+  , permSetConsts :: [GlobalPerm ctx] }
 
 -- FIXME: add lifetimes to PermSets
+
+
 
 
 extractCFG :: SharedContext -> AnyCFG (LLVM arch) -> IO Term
