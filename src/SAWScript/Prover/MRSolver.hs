d860cc83379d53c86a262576db9661ae9c99da50
Kevin Quick <kquick@galois.com>
2019-02-22 09:41:53 -0800

    Add pretty representation for function terms.

--- src/SAWScript/Prover/MRSolver.hs
+++ src/SAWScript/Prover/MRSolver.hs
@@ -170,6 +170,7 @@ instance Pretty MRTerm where
   pretty (MRTermComp comp) = pretty comp
   pretty (MRTermCompFun f) = pretty f
   pretty (MRTermWHNFComp norm) = pretty norm
+  pretty (MRTermFunName nm) = pretty ("function" :: String) <+> pretty nm
 
 instance Pretty FailCtx where
   pretty (FailCtxCmp t1 t2) =
