8d99a7f8ced8a4a96013a0c49cfef39942c74c01
Eddy Westbrook <westbrook@galois.com>
2019-03-29 13:49:19 -0700

    changed back to Diffs instead of Embeddings for mapping between contexts; added the uncbind operation

--- src/SAWScript/Heapster/CtxMonad.hs
+++ src/SAWScript/Heapster/CtxMonad.hs
@@ -60,31 +60,14 @@ instance MonadError err (BackM err) where
 -- | A context-dependent type is a type that depends on a context
 type CType k = Ctx k -> *
 
--- | Our notion of context embedding is a mapping from variables in one context
--- to variables in another
-newtype Embedding ctx1 ctx2 =
-  Embedding { unEmbedding :: forall a. Index ctx1 a -> Index ctx2 a }
-
-instance Cat.Category Embedding where
-  id = Embedding id
-  Embedding g . Embedding f = Embedding (g . f)
-
--- | Make an embedding from a context into an extension of that context
-mkDiffEmbedding :: Diff ctx1 ctx2 -> Embedding ctx1 ctx2
-mkDiffEmbedding d = Embedding (extendIndex' d)
-
--- | Make an embedding that adds one type to a context
-mkEmbedding1 :: Embedding ctx (ctx ::> a)
-mkEmbedding1 = Embedding skipIndex
-
--- | A valid contextual type is one that can be mapped via 'Embedding's
+-- | A valid contextual type is one that can be mapped via context extensions
 class ValidCType a where
-  mapContext :: Embedding ctx ctx' -> a ctx -> a ctx'
+  mapContext :: Diff ctx ctx' -> a ctx -> a ctx'
 
 -- | A valid unary contextual type function is one that maps valid contextual
 -- types to valid contextual types
 class ValidCType1 (f :: CType k -> CType k) where
-  mapContext1 :: ValidCType a => Embedding ctx ctx' -> f a ctx -> f a ctx'
+  mapContext1 :: ValidCType a => Diff ctx ctx' -> f a ctx -> f a ctx'
 
 instance {-# INCOHERENT #-} (ValidCType1 f, ValidCType a) => ValidCType (f a) where
   mapContext = mapContext1
@@ -93,7 +76,7 @@ instance {-# INCOHERENT #-} (ValidCType1 f, ValidCType a) => ValidCType (f a) wh
 -- contextual types to a valid contextual types
 class ValidCType2 (f :: CType k -> CType k -> CType k) where
   mapContext2 :: (ValidCType a, ValidCType b) =>
-                    Embedding ctx ctx' -> f a b ctx -> f a b ctx'
+                 Diff ctx ctx' -> f a b ctx -> f a b ctx'
 
 instance {-# INCOHERENT #-} (ValidCType2 f, ValidCType a) => ValidCType1 (f a) where
   mapContext1 = mapContext2
@@ -126,29 +109,32 @@ newtype CConst a (ctx :: Ctx k) = CConst { unCConst :: a }
 instance ValidCType (CConst a) where
   mapContext _ (CConst a) = CConst a
 
--- | The function type-in-context, that can be applied in any embedding of the
+-- | The function type-in-context, that can be applied in any extension of the
 -- current context
 infixr 0 :->:
 newtype (:->:) a b (ctx :: Ctx k) =
-  CFun { unCFun :: forall ctx'. Embedding ctx ctx' -> a ctx' -> b ctx' }
+  CFun { unCFun :: forall ctx'. Diff ctx ctx' -> a ctx' -> b ctx' }
 
 instance ValidCType2 (:->:) where
-  mapContext2 emb (CFun f) = CFun $ \emb' -> f (emb' Cat.. emb)
+  mapContext2 diff (CFun f) = CFun $ \diff' -> f (diff' Cat.. diff)
 
 -- | The contextual type for a variable in the context
 newtype CVar (tp :: k) (ctx :: Ctx k) = CVar { unCVar :: Index ctx tp }
 
 instance ValidCType (CVar tp) where
-  mapContext (Embedding f) (CVar i) = CVar $ f i
+  mapContext diff (CVar i) = CVar $ extendIndex' diff i
+
+instance ValidCType Size where
+  mapContext diff sz = extSize sz diff
 
 -- | Represents a contextual type inside a binding for another variable
 newtype CBind (tp :: k) (a :: CType k) (ctx :: Ctx k) =
   CBind { unCBind :: forall ctx'.
-                     Embedding ctx ctx' -> Size ctx' -> a (ctx' ::> tp) }
+                     Diff ctx ctx' -> Size ctx' -> a (ctx' ::> tp) }
 
 instance ValidCType1 (CBind tp) where
-  mapContext1 emb (CBind b) =
-    CBind $ \emb' sz -> b (emb' Cat.. emb) sz
+  mapContext1 diff (CBind b) =
+    CBind $ \diff' sz -> b (diff' Cat.. diff) sz
 
 
 ----------------------------------------------------------------------
@@ -157,55 +143,62 @@ instance ValidCType1 (CBind tp) where
 
 -- | An "expression context" is a sequence
 --
--- > BaseCtx ctx0 :::> '(ctx1, emb1) :::> ... :::> '(ctxn, embn)
+-- > BaseCtx ctx0 :<+>: '(ctx1, sz1) :<+>: ... :<+>: '(ctxn, szn)
 --
 -- of contexts bound at each contextual lambda (see 'clam') containing the
--- current expression. That is the above expression context is used for the body
--- of a contextual lambda that is nested @n@ levels deep.
+-- current expression. That is, the above expression context is used for the
+-- body of a contextual lambda that is nested @n@ levels deep. This expression
+-- context is used to represent (using 'CtxOfExprCtx') the @n@-ary append
 --
--- Each @embi@ is a type-level variable that 'Reifies' and 'Embedding' from
--- @ctx(i-1)@ to @ctxi@. This gives GHC enough information to automatically
--- derive an embedding (using the 'WeakensTo' class, below) from any previous
--- context to any later context in the list, which in turn allows us to weaken
--- expressions without having to manually put in all the coercions.
+-- > ctx0 <+> ctx1 <+> ... <+> ctxn
+--
+-- of the contexts contained in it. Intuitively, this is because each contextual
+-- lambda could be called in an extension of the previous context.
+--
+-- Each @szi@ is a type-level variable that 'Reifies' a 'Diff' from @ctx(i-1)@
+-- to @ctxi@. This gives GHC enough information to automatically derive a 'Diff'
+-- (using the 'WeakensTo' class, below) from any previous context to any later
+-- context in the list, which in turn allows us to weaken expressions without
+-- having to manually put in all the coercions.
 data ExprCtx k1 k2
   = BaseCtx (Ctx k1)
-  | ExprCtx k1 k2 :::> (Ctx k1, k2)
+  | ExprCtx k1 k2 :<+>: (Ctx k1, k2)
 
--- | Defines the constraint required for type variable @emb@ in the expression
--- context @ectx :::> (ctx, emb)@
-type EmbConstr (ectx :: ExprCtx k *) (ctx :: Ctx k) (emb :: *) =
-  Reifies emb (Embedding (CtxOfExprCtx ectx) ctx)
+-- | Defines the constraint required for type variable @sz@ in the expression
+-- context @ectx :<+>: (ctx, sz)@
+type SzConstr (ectx :: ExprCtx k *) (ctx :: Ctx k) (sz :: *) =
+  Reifies sz (Size ctx)
 
 -- | Convert an expression context to the context it reprsents
 type family CtxOfExprCtx (ctx :: ExprCtx k *) :: Ctx k where
   CtxOfExprCtx (BaseCtx ctx) = ctx
-  CtxOfExprCtx (_ :::> '(ctx, _)) = ctx
+  CtxOfExprCtx (ectx :<+>: '(ctx, _)) = CtxOfExprCtx ectx <+> ctx
 
--- | A "weakening" of expression contexts is an 'Embedding' from the
--- 'CtxOfExprCtx' of one to that of the other
+-- | A "weakening" of expression contexts is a 'Diff' from the 'CtxOfExprCtx' of
+-- one to that of the other
 newtype Weakening ectx1 ectx2 =
-  Weakening (Embedding (CtxOfExprCtx ectx1) (CtxOfExprCtx ectx2))
+  Weakening (Diff (CtxOfExprCtx ectx1) (CtxOfExprCtx ectx2))
 
-instance Cat.Category Weakening where
-  id = Weakening Cat.id
-  Weakening emb2 . Weakening emb1 = Weakening (emb2 Cat.. emb1)
+-- | Any expression context weakens to itself
+weakeningRefl :: Weakening ectx ectx
+weakeningRefl = Weakening noDiff
 
--- | One step of weakening
-weakening1 :: EmbConstr ectx ctx emb => Proxy emb ->
-              Weakening ectx (ectx :::> '(ctx, emb))
-weakening1 emb = Weakening $ reflect emb
+-- | Add a context to the right of an existing weakening
+weakeningCons :: SzConstr ectx ctx sz => Weakening ectx1 ectx2 -> Proxy sz ->
+                 Weakening ectx1 (ectx2 :<+>: '(ctx, sz))
+weakeningCons (Weakening diff) sz =
+  Weakening $ appendDiff (reflect sz) Cat.. diff
 
 -- | Typeclass version of 'Weakening'
 class WeakensTo ectx1 ectx2 where
   weakensTo :: Weakening ectx1 ectx2
 
 instance WeakensTo ectx ectx where
-  weakensTo = Cat.id
+  weakensTo = weakeningRefl
 
-instance {-# INCOHERENT #-} (WeakensTo ectx1 ectx2, EmbConstr ectx2 ctx emb) =>
-                            WeakensTo ectx1 (ectx2 :::> '(ctx, emb)) where
-  weakensTo = weakening1 Proxy Cat.. weakensTo
+instance {-# INCOHERENT #-} (WeakensTo ectx1 ectx2, SzConstr ectx2 ctx sz) =>
+                            WeakensTo ectx1 (ectx2 :<+>: '(ctx, sz)) where
+  weakensTo = weakeningCons weakensTo Proxy
 
 -- | A contextual expression of contextual type @a@ in expression context @ectx@
 newtype CExpr (a :: CType k) (ectx :: ExprCtx k *) =
@@ -226,24 +219,25 @@ cweaken :: (ValidCType a, WeakensTo ectx1 ectx2) =>
 cweaken = cweakenPf weakensTo
 
 -- | Helper function for 'clam'
-clamH :: (forall ctx emb. EmbConstr ectx ctx emb =>
-          CExpr a (ectx :::> '(ctx, emb)) -> CExpr b (ectx :::> '(ctx, emb))) ->
+clamH :: (forall ctx sz. SzConstr ectx ctx sz =>
+          CExpr a (ectx :<+>: '(ctx, sz)) -> CExpr b (ectx :<+>: '(ctx, sz))) ->
          CExpr (a :->: b) ectx
 clamH f =
-  CExpr $ CFun $ \emb a ->
-  reify emb $ \(p :: Proxy emb) -> unCExpr $ f @_ @emb (CExpr a)
+  CExpr $ CFun $ \diff a ->
+  case diffIsAppend diff of
+    IsAppend (sz :: Size ctx'') ->
+      reify sz $ \(p :: Proxy s) ->
+      unCExpr $ f @ctx'' @s (CExpr a)
 
 -- | The type of a binding expression
 type CExprBinder a b ectx =
-  (forall ctx emb. EmbConstr ectx ctx emb =>
+  (forall ctx sz. SzConstr ectx ctx sz =>
    (forall ectx'.
-    WeakensTo (ectx :::> '(ctx, emb)) ectx' => CExpr a ectx') ->
-   CExpr b (ectx :::> '(ctx, emb)))
+    WeakensTo (ectx :<+>: '(ctx, sz)) ectx' => CExpr a ectx') ->
+   CExpr b (ectx :<+>: '(ctx, sz)))
 
 -- | Build an expression for a contextual function 
-clam :: ValidCType a =>
-        CExprBinder a b ectx ->
-        CExpr (a :->: b) ectx
+clam :: ValidCType a => CExprBinder a b ectx -> CExpr (a :->: b) ectx
 clam f = clamH (\a -> f $ cweaken a)
 
 -- | Apply a contextual function expression
@@ -286,11 +280,15 @@ cunconst (CExpr (CConst a)) = a
 
 cbindH :: (CVar tp :->: a) ctx -> CBind tp a ctx
 cbindH f =
-  CBind $ \emb sz -> unCFun f (mkEmbedding1 Cat.. emb) (CVar $ nextIndex sz)
+  CBind $ \diff sz -> unCFun f (extendRight diff) (CVar $ nextIndex sz)
 
 -- | Bind a variable in the context of an expression
 cbind :: CExprBinder (CVar tp) a ectx -> CExpr (CBind tp a) ectx
 cbind f = cOp1 cbindH (clam f)
+
+uncbind :: CExpr Size ectx -> CExpr (CBind tp a) ectx ->
+           a (CtxOfExprCtx ectx ::> tp)
+uncbind (CExpr sz) (CExpr b) = unCBind b noDiff sz
 
 test1 :: ValidCType a => CExpr (a :->: a) ectx
 test1 = clam $ \x -> x
