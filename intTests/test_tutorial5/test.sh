7857a4ff65c22333a92ff662c0a8e657d3458b07
Aaron Tomb <atomb@galois.com>
2019-06-10 08:51:07 -0700

    Add tests for more tutorial code

---
+++ intTests/test_tutorial5/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+mkdir -p tmp
+cp ../../doc/tutorial/code/* tmp
+cd tmp
+$SAW des-cryptol2.saw
+$SAW des3.saw
+$SAW dotprod.saw
+$SAW double.saw
+$SAW picosat.saw
+cd ..
+rm -r tmp
