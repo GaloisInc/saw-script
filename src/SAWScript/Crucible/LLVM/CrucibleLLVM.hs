66caf4e0e98785f1cb80754691013592356126ea
Langston Barrett <langston.barrett@gmail.com>
2019-08-19 13:14:11 -0700

    Revert "Merge pull request #528 from langston-barrett/override-match-verify"
    This reverts commit 798977cae2d2d0ab609d3d0459b2d2bac8003aa3, reversing
    changes made to 83fb416ab23844a6d96888c660592e8eec331128.

--- src/SAWScript/Crucible/LLVM/CrucibleLLVM.hs
+++ src/SAWScript/Crucible/LLVM/CrucibleLLVM.hs
@@ -42,7 +42,6 @@ module SAWScript.Crucible.LLVM.CrucibleLLVM
     -- * Re-exports from "Lang.Crucible.LLVM.MemType"
   , SymType(MemType, Alias, VoidType)
   , MemType(..)
-  , memTypeAlign
   , memTypeSize
   , fiOffset
   , fiType
@@ -149,7 +148,7 @@ import Lang.Crucible.LLVM.Intrinsics
 import Lang.Crucible.LLVM.MemType
   (SymType(MemType, Alias, VoidType),
    MemType(..),
-   Ident, memTypeAlign, memTypeSize, fiOffset, fiType,
+   Ident, memTypeSize, fiOffset, fiType,
    siFields, siFieldInfo, siFieldOffset, siFieldTypes, siIsPacked,
    mkStructInfo, ppMemType)
 
