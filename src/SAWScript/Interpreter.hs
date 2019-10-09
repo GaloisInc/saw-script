7b9ff70213918b64a9e95db650913e95552dcaa5
Samuel Breese <chameco@users.noreply.github.com>
2019-10-09 11:16:41 -0400

    Automatically generate SAWScript boilerplate from LLVM AST (#529)
    This commit introduces three `Experimental` SAWScript functions:
    
        `crucible_llvm_array_size_profile : LLVMModule -> String -> CrucibleSetup () -> TopLevel [Profile]` symbolically executes a function and returns a list of profiles. These describe the sizes of buffers referenced by pointer arguments to all functions called during symbolic execution.
        `llvm_boilerplate : String -> LLVMModule -> [Profile] -> TopLevel ()` writes SAWScript proof "boilerplate" (method specs, calls to `crucible_llvm_verify` with appropriate overrides, etc.) to the path specified in the first argument. If profiles are available, they are used to guess array sizes.
        `llvm_boilerplate_info : LLVMModule -> [Profile] -> TopLevel ()` prints an internal representation of the boilerplate (for debugging).

--- src/SAWScript/Interpreter.hs
+++ src/SAWScript/Interpreter.hs
@@ -82,6 +82,7 @@ import qualified SAWScript.Crucible.Common.MethodSpec as CMS
 import qualified SAWScript.Crucible.JVM.BuiltinsJVM as CJ
 import           SAWScript.Crucible.LLVM.Builtins
 import           SAWScript.Crucible.JVM.Builtins
+import           SAWScript.Crucible.LLVM.Boilerplate
 import qualified SAWScript.Crucible.LLVM.MethodSpecIR as CIR
 
 -- Cryptol
@@ -1590,6 +1591,16 @@ primitives = Map.fromList
     Current
     [ "Load an LLVM bitcode file and return a handle to it." ]
 
+  , prim "llvm_boilerplate_info" "LLVMModule -> [Profile] -> TopLevel ()"
+    (pureVal llvm_boilerplate_info)
+    Experimental
+    [ "Print information from an LLVM module relevant to boilerplate generation." ]
+
+  , prim "llvm_boilerplate" "String -> LLVMModule -> [Profile] -> TopLevel ()"
+    (pureVal llvm_boilerplate)
+    Experimental
+    [ "Generate boilerplate for the definitions in an LLVM module." ]
+
   , prim "caseSatResult"       "{b} SatResult -> b -> (Term -> b) -> b"
     (\_ _ -> toValueCase caseSatResultPrim)
     Current
@@ -1891,6 +1902,17 @@ primitives = Map.fromList
     [ "Return a CrucibleMethodSpec corresponding to a CrucibleSetup block,"
     , "as would be returned by crucible_llvm_verify but without performing"
     , "any verification."
+    ]
+
+  , prim "crucible_llvm_array_size_profile"
+    "LLVMModule -> String -> CrucibleSetup () -> TopLevel [Profile]"
+    (bicVal crucible_llvm_array_size_profile)
+    Experimental
+    [ "Symbolically execute the function named by the second parameter in"
+    , "the module specified by the first. The third parameter may be used"
+    , "to specify arguments. Returns profiles specifying the sizes of buffers"
+    , "referred to by pointer arguments for the function and all other functions"
+    , "it calls (recursively), to be passed to llvm_boilerplate."
     ]
 
   , prim "crucible_array"
