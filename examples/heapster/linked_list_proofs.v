b5ebe34d6983b037e086432150e0b58778789d11
Eddy Westbrook <westbrook@galois.com>
2020-05-28 09:29:37 -0700

    added more complex specs of is_elem

--- examples/heapster/linked_list_proofs.v
+++ examples/heapster/linked_list_proofs.v
@@ -54,17 +54,50 @@ Fixpoint is_elem_spec (x:bitvector 64) (l:W64List) : CompM {_:bitvector 64 & uni
   end.
 *)
 
-Definition is_elem_spec (x:bitvector 64) :
+Definition is_elem_fun (x:bitvector 64) :
   list {_:bitvector 64 & unit} -> CompM {_:bitvector 64 & unit} :=
   list_rect (fun _ => CompM {_:bitvector 64 & unit})
             (returnM (existT _ (bvNat 64 0) tt))
             (fun y l' rec =>
                if bvEq 64 (projT1 y) x then returnM (existT _ (bvNat 64 1) tt) else rec).
 
-Arguments is_elem_spec /.
+Arguments is_elem_fun /.
 
-Lemma is_elem_correct : refinesFun is_elem is_elem_spec.
+Lemma is_elem_fun_ref : refinesFun is_elem is_elem_fun.
 Proof.
-  unfold is_elem, is_elem__tuple_fun, is_elem_spec.
+  unfold is_elem, is_elem__tuple_fun, is_elem_fun.
   prove_refinement.
 Qed.
+
+(* The pure version of is_elem *)
+Definition is_elem_pure (x:bitvector 64) (l:list {_:bitvector 64 & unit})
+  : {_:bitvector 64 & unit} :=
+  (list_rect (fun _ => {_:bitvector 64 & unit})
+             (existT _ (bvNat 64 0) tt)
+             (fun y l' rec =>
+                if bvEq 64 (projT1 y) x then existT _ (bvNat 64 1) tt else rec) l).
+
+Arguments is_elem_pure /.
+
+Lemma is_elem_pure_ref : refinesFun is_elem (fun x l => returnM (is_elem_pure x l)).
+Proof.
+Admitted.
+
+Definition orM {A} (m1 m2:CompM A) : CompM A :=
+  existsM (fun (b:bool) => if b then m1 else m2).
+
+Definition assertM (P:Prop) : CompM unit :=
+  existsM (fun (_:P) => returnM tt).
+
+(* The specification of is_elem: returns 1 if  *)
+Definition is_elem_spec (x:bitvector 64) (l:list {_:bitvector 64 & unit})
+  : CompM {_:bitvector 64 & unit} :=
+  orM
+    (assertM (List.In (existT _ x tt) l) >> returnM (existT _ (bvNat 64 1) tt))
+    (assertM (~ List.In (existT _ x tt) l) >> returnM (existT _ (bvNat 64 0) tt)).
+
+Arguments is_elem_spec /.
+
+Lemma is_elem_spec_ref : refinesFun is_elem is_elem_spec.
+Proof.
+Admitted.
