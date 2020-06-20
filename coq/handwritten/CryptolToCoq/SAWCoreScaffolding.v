27c6e80dcab382e557afd02b108a2e597644a00a
Eddy Westbrook <westbrook@galois.com>
2020-06-20 12:51:00 -0700

    trying to rewrite the translation of record types

--- coq/handwritten/CryptolToCoq/SAWCoreScaffolding.v
+++ coq/handwritten/CryptolToCoq/SAWCoreScaffolding.v
@@ -214,3 +214,81 @@ Definition intModMul : forall (n : Nat), (IntMod n) -> (IntMod n) -> IntMod n
   := fun _ => ZModulo.mul.
 Definition intModNeg : forall (n : Nat), (IntMod n) -> IntMod n
   := fun _ => ZModulo.opp.
+
+
+(***
+ *** A simple typeclass-based implementation of SAW record types
+ ***
+ *** The idea is to support a projection term recordProj e "field" on an element
+ *** e of a record type without having to find "field" in the record type of e,
+ *** by using typeclass resolution to find it for us.
+ ***)
+
+(* The empty record type *)
+Variant RecordNilType : Type :=
+  RecordNilElem : RecordNilType.
+
+(* A non-empty record type *)
+Variant RecordConsType (str:String.string) (tp:Type) (rest_tp:Type) : Type :=
+  RecordConsElem (x:tp) (rest:rest_tp) : RecordConsType str tp rest_tp.
+Arguments RecordConsElem str%string_scope {tp rest_tp} x rest.
+
+(* Get the head element of a non-empty record type *)
+Definition recordHead {str tp rest_tp} (r:RecordConsType str tp rest_tp) : tp :=
+  match r with
+  | RecordConsElem _ x _ => x
+  end.
+
+(* Get the tail of a non-empty record type *)
+Definition recordTail {str tp rest_tp} (r:RecordConsType str tp rest_tp) : rest_tp :=
+  match r with
+  | RecordConsElem _ _ rest => rest
+  end.
+
+(* A list of fields with their associated types *)
+Definition Fields : Type := list (String.string * Type).
+
+(* Build the record type for a list of fields *)
+Fixpoint RecordType (flds : Fields) : Type :=
+  match flds with
+  | nil => RecordNilType
+  | cons (str,tp) flds' => RecordConsType str tp (RecordType flds')
+  end.
+
+(* Build an element of the empty record type; the explicit type annotation tells
+Coq how to type-check it *)
+Definition RecordNil : RecordType nil := RecordNilElem.
+
+(* Build up an element of a non-empty record type; the explicit type annotation
+tells Coq how to type-check it *)
+Definition RecordCons str {tp flds} :
+  tp -> RecordType flds -> RecordType (cons (str,tp) flds) := RecordConsElem str.
+Arguments RecordCons str%string_scope {tp flds} _ _.
+
+(* Give us a way to project a specific field out a record type *)
+Class RecordProjFun (str:String.string) (tp:Type) (flds:Fields) : Type :=
+  recordProjFun : RecordType flds -> tp.
+
+(* Projection function for the first field in a record type *)
+Instance RecordProjFun_Base str tp flds : RecordProjFun str tp (cons (str,tp) flds) :=
+  recordHead.
+
+(* Projection function for some other field in a record type *)
+Instance RecordProjFun_Step str tp str' tp' flds (rproj:RecordProjFun str tp flds)
+  : RecordProjFun str tp (cons (str',tp') flds) :=
+  fun elem => rproj (recordTail elem).
+
+(* Perform a record projection, using typeclass resolution to find the
+projection we want using the string name *)
+Definition RecordProj {flds} (x:RecordType flds) str {tp} `{RecordProjFun str tp flds} : tp :=
+  recordProjFun x.
+Arguments RecordProj {flds} !x str%string_scope {tp} {_}.
+
+(* Some tests *)
+Definition recordTest1 := RecordCons "fld1" 0 (RecordCons "fld2" true RecordNil).
+(* Check recordTest1. *)
+
+Definition recordTest2 := RecordProj recordTest1 "fld1".
+(* Check recordTest2. *)
+
+(* Definition recordTestFail := RecordProj recordTest1 "fld3". *)
