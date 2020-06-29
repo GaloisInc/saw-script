08f201f93c18f51553f157b540b39e2e12a29065
Rob Dockins <rdockins@galois.com>
2020-06-29 10:15:49 -0700

    Updates to track `record-maps` branch

--- src/SAWScript/Builtins.hs
+++ src/SAWScript/Builtins.hs
@@ -110,6 +110,7 @@ import qualified Cryptol.Eval.Type as C (evalType)
 import qualified Cryptol.Eval.Value as C (fromVBit, fromVWord)
 import qualified Cryptol.Eval.Concrete.Value as C (Concrete(..), bvVal)
 import qualified Cryptol.Utils.Ident as C (packIdent, packModName)
+import qualified Cryptol.Utils.RecordMap as C (recordFromFields)
 import Cryptol.Utils.PP (pretty)
 
 import qualified SAWScript.SBVParser as SBV
@@ -184,7 +185,7 @@ readSBV path unintlst =
           SBV.TFun t1 t2 -> C.tFun (toCType t1) (toCType t2)
           SBV.TVec n t   -> C.tSeq (C.tNum n) (toCType t)
           SBV.TTuple ts  -> C.tTuple (map toCType ts)
-          SBV.TRecord bs -> C.tRec [ (C.packIdent n, toCType t) | (n, t) <- bs ]
+          SBV.TRecord bs -> C.tRec (C.recordFromFields [ (C.packIdent n, toCType t) | (n, t) <- bs ])
 
 
 
@@ -1203,7 +1204,7 @@ defaultTypedTerm opts sc cfg tt@(TypedTerm schema trm)
       case ty of
         C.TCon tc ts   -> C.TCon tc (map (plainSubst s) ts)
         C.TUser f ts t -> C.TUser f (map (plainSubst s) ts) (plainSubst s t)
-        C.TRec fs      -> C.TRec [ (x, plainSubst s t) | (x, t) <- fs ]
+        C.TRec fs      -> C.TRec (fmap (plainSubst s) fs)
         C.TVar x       -> C.apSubst s (C.TVar x)
 
 eval_size :: C.Schema -> TopLevel Integer
