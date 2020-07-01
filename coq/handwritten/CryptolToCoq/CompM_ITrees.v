402425ab7fe612aa066dadac9952f75a1830214b
Eddy Westbrook <westbrook@galois.com>
2020-07-01 16:21:24 -0700

    got bind_satisfies_bind working!

--- coq/handwritten/CryptolToCoq/CompM_ITrees.v
+++ coq/handwritten/CryptolToCoq/CompM_ITrees.v
@@ -4,6 +4,7 @@
 
 From Coq Require Export Morphisms Setoid.
 From ITree Require Export ITree ITreeFacts.
+From Paco Require Import paco.
 
 Variant SpecEvent (E:Type -> Type) (A:Type) : Type :=
 | Spec_vis : E A -> SpecEvent E A
@@ -18,22 +19,37 @@ Arguments Spec_exists {E A}.
 (* An ITree that defines a set of ITrees *)
 Definition itree_spec E A : Type := itree (SpecEvent E) A.
 
-CoInductive itree_satisfies_spec {E A} : itree_spec E A -> itree E A -> Prop :=
-| Satisfies_Ret a : itree_satisfies_spec (Ret a) (Ret a)
+Variant satisfiesF {E A} (satisfies : itree_spec E A -> itree E A -> Prop)
+  : itree_spec E A -> itree E A -> Prop :=
+| Satisfies_Ret a : satisfiesF satisfies (Ret a) (Ret a)
 | Satisfies_TauL spec tree :
-    itree_satisfies_spec spec tree -> itree_satisfies_spec (Tau spec) tree
+    satisfies spec tree -> satisfiesF satisfies (Tau spec) tree
 | Satisfies_TauR spec tree :
-    itree_satisfies_spec spec tree -> itree_satisfies_spec spec (Tau tree)
+    satisfies spec tree -> satisfiesF satisfies spec (Tau tree)
 | Satisfies_Vis X (e:E X) spec tree :
-    (forall x, itree_satisfies_spec (spec x) (tree x)) ->
-    itree_satisfies_spec (Vis (Spec_vis e) spec) (Vis e tree)
+    (forall x, satisfies (spec x) (tree x)) ->
+    satisfiesF satisfies (Vis (Spec_vis e) spec) (Vis e tree)
 | Satisfies_Forall X spec tree :
-    (forall x:X, itree_satisfies_spec (spec x) tree) ->
-    itree_satisfies_spec (Vis Spec_forall spec) tree
+    (forall x:X, satisfies (spec x) tree) ->
+    satisfiesF satisfies (Vis Spec_forall spec) tree
 | Satisfies_Exists X spec tree :
-    (exists x:X, itree_satisfies_spec (spec x) tree) ->
-    itree_satisfies_spec (Vis Spec_exists spec) tree
+    (exists x:X, satisfies (spec x) tree) ->
+    satisfiesF satisfies (Vis Spec_exists spec) tree
 .
+
+Definition satisfies {E A} spec tree := paco2 (@satisfiesF E A) bot2 spec tree.
+
+Lemma satisfiesF_mono E A : monotone2 (@satisfiesF E A).
+Proof.
+  intros spec tree r1 r2 sats sub12. destruct sats; constructor.
+  { apply sub12. assumption. }
+  { apply sub12. assumption. }
+  { intros; apply sub12. apply H. }
+  { intros; apply sub12. apply H. }
+  { destruct H as [ x H ]. exists x. apply sub12. apply H. }
+Qed.
+
+Hint Resolve satisfiesF_mono : paco.
 
 (* The proposition that a is returned by an itree along some path *)
 Inductive is_itree_retval {E A} : itree E A -> A -> Prop :=
@@ -46,105 +62,109 @@ Inductive is_itree_retval {E A} : itree E A -> A -> Prop :=
 Infix ">>=" := ITree.bind (at level 58, left associativity).
 Notation "m1 >> m2" := (m1 >>= fun _ => m2) (at level 58, left associativity).
 
-CoFixpoint itree_satisfies_untau_l' E A spec tree
-           (sats:@itree_satisfies_spec E A spec tree) :
-  forall spec', spec = Tau spec' -> itree_satisfies_spec spec' tree.
+Lemma satisfies_untau_l' E A spec tree (sats:@satisfies E A spec tree) spec' :
+  spec = Tau spec' -> satisfies spec' tree.
 Proof.
-  destruct sats; intros; try discriminate.
-  - injection H; intro e; rewrite <- e. assumption.
-  - apply Satisfies_TauR. apply (itree_satisfies_untau_l' _ _ spec); assumption.
+  revert spec tree sats spec'; pcofix CIH; intros.
+  punfold sats. destruct sats; try discriminate.
+  - injection H0; intro e; rewrite <- e.
+    pclearbot.
+    eapply paco2_mon_bot; [ eassumption | intros; assumption ].
+  - pfold. apply Satisfies_TauR. pclearbot. right.
+    apply (CIH _ _ H). assumption.
 Qed.
 
-Lemma itree_satisfies_untau_l E A spec tree :
-  @itree_satisfies_spec E A (Tau spec) tree ->
-  itree_satisfies_spec spec tree.
+Lemma satisfies_untau_l E A spec tree :
+  @satisfies E A (Tau spec) tree ->
+  satisfies spec tree.
 Proof.
-  intros sats. apply (itree_satisfies_untau_l' E A _ _ sats _ eq_refl).
+  intros sats. apply (satisfies_untau_l' E A _ _ sats _ eq_refl).
 Qed.
 
-CoFixpoint itree_satisfies_untau_r' E A spec tree
-           (sats:@itree_satisfies_spec E A spec tree) :
-  forall tree', tree = Tau tree' -> itree_satisfies_spec spec tree'.
+Lemma satisfies_untau_r' E A spec tree (sats:@satisfies E A spec tree) tree' :
+  tree = Tau tree' -> satisfies spec tree'.
 Proof.
-  destruct sats; intros; try discriminate.
-  - apply Satisfies_TauL. apply (itree_satisfies_untau_r' _ _ _ tree); assumption.
-  - injection H; intro e; rewrite <- e. assumption.
-  - apply Satisfies_Forall; intro.
-    apply (itree_satisfies_untau_r' _ _ _ tree (H x) _ H0).
-  - destruct H as [ x H ]. apply Satisfies_Exists. exists x.
-    apply (itree_satisfies_untau_r' _ _ _ tree H _ H0).
+  revert spec tree sats tree'; pcofix CIH; intros.
+  punfold sats. destruct sats; try discriminate.
+  - pfold. apply Satisfies_TauL. right. pclearbot. apply (CIH _ _ H). assumption.
+  - injection H0; intro e. rewrite <- e. pclearbot.
+    eapply paco2_mon_bot; [ eassumption | intros; assumption ].
+  - pfold. apply Satisfies_Forall; intro. right.
+    pclearbot. apply (CIH _ _ (H x)). assumption.
+  - destruct H as [ x H ]. pfold. apply Satisfies_Exists. exists x.
+    pclearbot. right. apply (CIH _ _ H). assumption.
 Qed.
 
-Lemma itree_satisfies_untau_r E A spec tree :
-  @itree_satisfies_spec E A spec (Tau tree) ->
-  itree_satisfies_spec spec tree.
+Lemma satisfies_untau_r E A spec tree :
+  @satisfies E A spec (Tau tree) ->
+  satisfies spec tree.
 Proof.
-  intros sats. apply (itree_satisfies_untau_r' E A _ _ sats _ eq_refl).
+  intros sats. apply (satisfies_untau_r' E A _ _ sats _ eq_refl).
 Qed.
 
-CoFixpoint itree_satisfies_eutt_l E A spec tree
-           (sats:@itree_satisfies_spec E A spec tree)
+Lemma satisfies_eutt_l E A spec tree (sats:@satisfies E A spec tree)
            spec' (e:eutt eq spec spec') :
-  itree_satisfies_spec spec' tree.
+  satisfies spec' tree.
 Proof.
+  (*
+  revert spec tree sats spec' e; pcofix CIH; intros.
+  punfold e. unfold eqit_ in e. rewrite itree_eta.
+  destruct e.
+  { unfold satisfies in sats.
+    eapply paco2_mon_bot; [ eassumption | ].
+  destruct e.
+   *)
 Admitted.
 
-CoFixpoint itree_satisfies_eutt_r E A spec tree
-           (sats:@itree_satisfies_spec E A spec tree)
+Lemma satisfies_eutt_r E A spec tree
+           (sats:@satisfies E A spec tree)
            tree' (e:eutt eq tree tree') :
-  itree_satisfies_spec spec tree'.
+  satisfies spec tree'.
 Proof.
 Admitted.
 
-Instance Proper_eq_itree_itree_satisfies_spec_impl E A :
-  Proper (eutt eq ==> eutt eq ==> iff) (@itree_satisfies_spec E A).
+Instance Proper_eutt_satisfies_impl E A :
+  Proper (eutt eq ==> eutt eq ==> iff) (@satisfies E A).
 Proof.
   intros spec1 spec2 e_spec tree1 tree2 e_tree.
   split; intro H.
-  - eapply itree_satisfies_eutt_l; [ | eassumption ].
-    eapply itree_satisfies_eutt_r; [ | eassumption ].
+  - eapply satisfies_eutt_l; [ | eassumption ].
+    eapply satisfies_eutt_r; [ | eassumption ].
     assumption.
-  - eapply itree_satisfies_eutt_l; [ | symmetry; eassumption ].
-    eapply itree_satisfies_eutt_r; [ | symmetry; eassumption ].
+  - eapply satisfies_eutt_l; [ | symmetry; eassumption ].
+    eapply satisfies_eutt_r; [ | symmetry; eassumption ].
     assumption.
 Qed.
 
-
-Lemma bind_vis_eutt E A B C (e:E A) (tree:A -> itree E B) (f:B -> itree E C) :
-  eutt eq (Vis e tree >>= f) (Vis e (fun x => tree x >>= f)).
+Instance Proper_eutt_paco2_satisfies_impl E A r :
+  Proper (eutt eq ==> eutt eq ==> iff) (paco2 (@satisfiesF E A) r).
 Proof.
 Admitted.
-
-Lemma bind_tau_eutt E A B (tree:itree E A) (f:A -> itree E B) :
-  eutt eq (Tau tree >>= f) (Tau (tree >>= f)).
-Proof.
-Admitted.
-
 
 Lemma bind_satisfies_bind E A B (P:itree_spec E A) (Q:A -> itree_spec E B)
       (m:itree E A) (f:A -> itree E B) :
-  itree_satisfies_spec P m ->
-  (forall a, is_itree_retval m a -> itree_satisfies_spec (Q a) (f a)) ->
-  itree_satisfies_spec (P >>= Q) (m >>= f).
+  satisfies P m ->
+  (forall a, is_itree_retval m a -> satisfies (Q a) (f a)) ->
+  satisfies (P >>= Q) (m >>= f).
 Proof.
-  intro sats; revert P m sats B Q f. cofix CIH. intros P m sats; destruct sats; intros.
-  { rewrite bind_ret_l. rewrite bind_ret_l. apply H. constructor. }
-  { rewrite bind_tau_eutt. apply Satisfies_TauL. apply CIH; assumption. }
-  { rewrite bind_tau_eutt. apply Satisfies_TauR. apply CIH; [ assumption | ].
-    intros a iirv; apply H. apply iirv_tau; assumption. }
-  { rewrite bind_vis_eutt. rewrite bind_vis_eutt.
-    apply Satisfies_Vis. intro.
-    apply CIH; [ apply H | ]. intros a iirv. apply H0.
+  intro sats; revert P m sats. pcofix CIH.
+  intros P m sats; punfold sats; destruct sats; intros.
+  { rewrite bind_ret_l. rewrite bind_ret_l.
+    eapply paco2_mon_bot; [ apply H0; constructor | intros; assumption ]. }
+  { rewrite bind_tau. pfold. apply Satisfies_TauL. pclearbot. right.
+    apply CIH; assumption. }
+  { rewrite bind_tau. pfold. apply Satisfies_TauR. pclearbot. right.
+    apply CIH; [ assumption | ]. intros. apply H0. apply iirv_tau; assumption. }
+  { rewrite bind_vis. rewrite bind_vis. pfold. pclearbot.
+    apply Satisfies_Vis. intro. right. apply CIH; [ apply H | ].
+    intros a iirv. apply H0.
     apply (iirv_vis _ _ _ x). assumption. }
-  { rewrite bind_vis_eutt. apply Satisfies_Forall. intros.
+  { rewrite bind_vis. pfold. pclearbot. apply Satisfies_Forall. intros. right.
     apply CIH; [ apply (H x) | apply H0 ]. }
-  { rewrite bind_vis_eutt. apply Satisfies_Exists.
-    destruct H as [ x H ]. exists x.
+  { rewrite bind_vis. pfold. pclearbot. apply Satisfies_Exists.
+    destruct H as [ x H ]. exists x. right. pclearbot.
     apply CIH; [ apply H | apply H0 ]. }
 Qed.
-
-
-FIXME HERE: the above does not work because of casts!
 
 
 
@@ -155,5 +175,4 @@ Inductive CompMEvent : Type -> Type :=
 
 (* Our computations are sets of ITrees. That is, they are really more like
 specifications of computations *)
-Definition CompM (A:Type) : Type := itree CompMEvent A -> Prop.
-
+Definition CompM (A:Type) : Type := itree_spec CompMEvent A.
