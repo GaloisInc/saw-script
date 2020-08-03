ed5e8e0ccb51caaa17b7d7105027d45a400e3fa4
Chris Phifer <cphifer@galois.com>
2020-08-03 15:51:16 -0400

    Rearranged C file to reflect proof script more closely

--- intTests/test0060/test.c
+++ intTests/test0060/test.c
@@ -1,9 +1,9 @@
 #include <inttypes.h>
 
-uint32_t *incr_p(uint32_t *p) {
-    return p + 1;
-}
-
 uint32_t *id_p(uint32_t *p) {
     return p;
+}
+
+uint32_t *incr_p(uint32_t *p) {
+    return p + 1;
 }
