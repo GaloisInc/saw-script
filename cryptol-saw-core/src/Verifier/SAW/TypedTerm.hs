113d241b3171a857e4d87670c64f6d5fd4960869
Brian Huffman <huffman@galois.com>
2020-08-11 10:38:36 -0700

    Turn comments into haddock comments.

--- cryptol-saw-core/src/Verifier/SAW/TypedTerm.hs
+++ cryptol-saw-core/src/Verifier/SAW/TypedTerm.hs
@@ -19,9 +19,10 @@ import Verifier.SAW.SharedTerm
 
 -- Typed terms -----------------------------------------------------------------
 
-{- Within SAWScript, we represent an object language term as a SAWCore
-shared term paired with a Cryptol type schema. The Cryptol type is
-used for type inference/checking of inline Cryptol expressions. -}
+-- | Within SAWScript, we represent an object language term as a
+-- SAWCore shared term paired with a Cryptol type schema. The Cryptol
+-- type is used for type inference/checking of inline Cryptol
+-- expressions.
 
 data TypedTerm =
   TypedTerm
@@ -41,9 +42,9 @@ mkTypedTerm sc trm = do
 
 -- Typed modules ---------------------------------------------------------------
 
-{- In SAWScript, we can refer to a Cryptol module as a first class
-value. These are represented simply as maps from names to typed
-terms. -}
+-- | In SAWScript, we can refer to a Cryptol module as a first class
+-- value. These are represented simply as maps from names to typed
+-- terms.
 
 data CryptolModule =
   CryptolModule (Map C.Name C.TySyn) (Map C.Name TypedTerm)
