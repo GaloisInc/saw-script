0fe4d004617554130c86896cb035a452729af0e3
Brian Huffman <huffman@galois.com>
2020-08-20 10:03:54 -0700

    Add several new operations on `TypedTerm`s.
    * applyTypedTerm
    * applyTypedTerms
    * defineTypedTerm
    * tupleTypedTerm
    * destTupleTypedTerm

--- cryptol-saw-core/src/Verifier/SAW/TypedTerm.hs
+++ cryptol-saw-core/src/Verifier/SAW/TypedTerm.hs
@@ -7,6 +7,7 @@ Stability   : provisional
 -}
 module Verifier.SAW.TypedTerm where
 
+import Control.Monad (foldM)
 import Data.Map (Map)
 import qualified Data.Map as Map
 
@@ -44,6 +45,49 @@ mkTypedTerm sc trm = do
   ty <- scTypeOf sc trm
   ct <- scCryptolType sc ty
   return $ TypedTerm (C.Forall [] [] ct) trm
+
+-- | Apply a function-typed 'TypedTerm' to an argument. This operation
+-- fails if the first 'TypedTerm' does not have a monomorphic function
+-- type.
+applyTypedTerm :: SharedContext -> TypedTerm -> TypedTerm -> IO TypedTerm
+applyTypedTerm sc (TypedTerm schema1 t1) (TypedTerm _schema2 t2) =
+  case C.tIsFun =<< C.isMono schema1 of
+    Nothing -> fail "applyTypedTerm: not a function type"
+    Just (_, cty') -> TypedTerm (C.tMono cty') <$> scApply sc t1 t2
+
+-- | Apply a 'TypedTerm' to a list of arguments. This operation fails
+-- if the first 'TypedTerm' does not have a function type of
+-- sufficient arity.
+applyTypedTerms :: SharedContext -> TypedTerm -> [TypedTerm] -> IO TypedTerm
+applyTypedTerms sc = foldM (applyTypedTerm sc)
+
+-- | Create an abstract defined constant with the specified name and body.
+defineTypedTerm :: SharedContext -> String -> TypedTerm -> IO TypedTerm
+defineTypedTerm sc name (TypedTerm schema t) =
+  do ty <- scTypeOf sc t
+     TypedTerm schema <$> scConstant sc name t ty
+
+-- | Make a tuple value from a list of 'TypedTerm's. This operation
+-- fails if any 'TypedTerm' in the list has a polymorphic type.
+tupleTypedTerm :: SharedContext -> [TypedTerm] -> IO TypedTerm
+tupleTypedTerm sc tts =
+  case traverse (C.isMono . ttSchema) tts of
+    Nothing -> fail "tupleTypedTerm: invalid polymorphic term"
+    Just ctys ->
+      TypedTerm (C.tMono (C.tTuple ctys)) <$> scTuple sc (map ttTerm tts)
+
+-- | Given a 'TypedTerm' with a tuple type, return a list of its
+-- projected components. This operation fails if the 'TypedTerm' does
+-- not have a tuple type.
+destTupleTypedTerm :: SharedContext -> TypedTerm -> IO [TypedTerm]
+destTupleTypedTerm sc (TypedTerm schema t) =
+  case C.tIsTuple =<< C.isMono schema of
+    Nothing -> fail "asTupleTypedTerm: not a tuple type"
+    Just ctys ->
+      do let len = length ctys
+         let idxs = take len [1 ..]
+         ts <- traverse (\i -> scTupleSelector sc t i len) idxs
+         pure $ zipWith TypedTerm (map C.tMono ctys) ts
 
 -- First order types and values ------------------------------------------------
 
