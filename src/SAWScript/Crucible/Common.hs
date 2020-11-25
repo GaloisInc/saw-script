f1ae175d18f373fe0fe89b6f9399d86ec3db3e05
Brian Huffman <huffman@galois.com>
2020-11-24 18:21:25 -0800

    Update saw-script and submodules to use `prettyprinter` package.
    This commit includes the following submodule PRs:
    - GaloisInc/what4#77
    - GaloisInc/crucible#586
    - GaloisInc/macaw#178
    - GaloisInc/elf-edit#20

--- src/SAWScript/Crucible/Common.hs
+++ src/SAWScript/Crucible/Common.hs
@@ -27,30 +27,33 @@ import qualified What4.ProgramLoc as W4 (plSourceLoc)
 
 import System.Directory (createDirectoryIfMissing)
 import System.FilePath ((</>))
-import qualified Text.PrettyPrint.ANSI.Leijen as PP hiding ((<$>), (<>))
+import qualified Prettyprinter as PP
 
 -- | The symbolic backend we use for SAW verification
 type Sym = SAWCoreBackend Nonce.GlobalNonceGenerator Yices.Connection (W4.Flags W4.FloatReal)
 
-ppAbortedResult :: (forall l args. GlobalPair Sym (SimFrame Sym ext l args) -> PP.Doc)
+ppAbortedResult :: (forall l args. GlobalPair Sym (SimFrame Sym ext l args) -> PP.Doc ann)
                 -> AbortedResult Sym ext
-                -> PP.Doc
+                -> PP.Doc ann
 ppAbortedResult _ (AbortedExec InfeasibleBranch _) =
-  PP.text "Infeasible branch"
+  PP.pretty "Infeasible branch"
 ppAbortedResult ppGP (AbortedExec abt gp) = do
-  ppAbortExecReason abt PP.<$$> ppGP gp
+  PP.vcat
+    [ ppAbortExecReason abt
+    , ppGP gp
+    ]
 ppAbortedResult ppGP (AbortedBranch loc _predicate trueBranch falseBranch) =
   PP.vcat
-    [ PP.text "Both branches aborted after a symbolic branch."
-    , PP.text "Location of control-flow branching:"
-    , PP.indent 2 (PP.text (show (W4.plSourceLoc loc)))
-    , PP.text "Message from the true branch:"
+    [ PP.pretty "Both branches aborted after a symbolic branch."
+    , PP.pretty "Location of control-flow branching:"
+    , PP.indent 2 (PP.pretty (W4.plSourceLoc loc))
+    , PP.pretty "Message from the true branch:"
     , PP.indent 2 (ppAbortedResult ppGP trueBranch)
-    , PP.text "Message from the false branch:"
+    , PP.pretty "Message from the false branch:"
     , PP.indent 2 (ppAbortedResult ppGP falseBranch)
     ]
 ppAbortedResult _ (AbortedExit ec) =
-  PP.text "Branch exited:" PP.<+> PP.text (show ec)
+  PP.pretty "Branch exited:" PP.<+> PP.viaShow ec
 
 setupProfiling ::
   IsSymInterface sym =>
