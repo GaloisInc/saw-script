a20fdada3c982c08b2dfc95dfd184ffca303d712
Karl Smeltzer <karl@galois.com>
2020-12-07 15:17:19 -0800

    remove upstream CI stuff

--- .github/PropertiesTest.java
+++
@@ -1,7 +0,0 @@
-public class PropertiesTest {
-    public static void main(String[] args)
-        throws Exception {
-        String value = System.getProperty("sun.boot.class.path");
-        System.out.println(value);
-    }
-}
