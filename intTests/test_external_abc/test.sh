a2587b28da09000d35ddad5ca6a28c173978109f
Aaron Tomb <atomb@galois.com>
2020-12-09 09:54:48 -0800

    Add tests for external ABC interaction

--- intTests/test_external_abc/test.sh
+++ intTests/test_external_abc/test.sh
@@ -7,3 +7,7 @@ $SAW test.saw
 abc -q "%read offline_verilog.prove0.v; $SCRIPT" | grep "are equivalent"
 abc -q "%read write_verilog_unsat.v; $SCRIPT" | grep "are equivalent"
 abc -q "%read write_verilog_sat.v; $SCRIPT" | grep "are NOT EQUIVALENT"
+
+z3 w4_offline_smtlib2.prove0.smt2 | grep "^unsat$"
+z3 write_smtlib2_w4_sat.smt2 | grep "^sat$"
+z3 write_smtlib2_w4_unsat.smt2 | grep "^unsat$"
