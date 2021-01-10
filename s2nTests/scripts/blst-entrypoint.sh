6f1a30d938e0a6688d622409fd9c42141697afd3
Samuel Breese <samuel@chame.co>
2021-01-09 21:12:40 -0500

    Update blst-verification test in CI

--- s2nTests/scripts/blst-entrypoint.sh
+++ s2nTests/scripts/blst-entrypoint.sh
@@ -1,32 +1,15 @@
 #!/usr/bin/env bash
 set -xe
 
-cd /saw-script/blst-verification
+cd /workdir
 ./scripts/install.sh
 cp /saw-bin/saw bin/saw
 
-export PATH=/saw-script/blst-verification/bin:$PATH
-export CRYPTOLPATH=/saw-script/blst-verification/cryptol-specs:/saw-script/blst-verification/spec
+export PATH=/workdir/bin:$PATH
+export CRYPTOLPATH=/workdir/cryptol-specs:/workdir/spec
 
 ./scripts/build_x86.sh
-
-# TODO: replace this section with ./scripts/build_llvm.sh once the absolute
-# path in that script is removed.
-(
-    mkdir -p build/llvm
-    rm -r build/llvm
-    cp -r blst build/llvm
-    cd /saw-script/blst-verification/build/llvm
-    for p in /saw-script/blst-verification/patches/*
-    do
-        patch -p1 -t < "$p"
-    done
-    export CFLAGS='-g -fPIC -Wall -Wextra -Werror'
-    export CC=wllvm
-    export LLVM_COMPILER=clang
-    ./build.sh
-    extract-bc --bitcode libblst.a
-)
+./scripts/build_llvm.sh
 
 ./scripts/prove.sh
 ./scripts/check.sh | if grep False; then exit 1; fi
