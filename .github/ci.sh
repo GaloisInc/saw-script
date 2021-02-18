6f173cb74fda8cb88022dbd0838705a5c7519632
Lisanna Dettwyler <lisanna@galois.com>
2021-02-17 22:29:56 -0800

    Bump cryptol to include support for libBF>=0.6
    libBF is not included in most of cryptol's freeze files, causing builds
    of cryptol at the prior commit to start failing once libBF 0.6 was
    released. Cryptol is built from the submodule by nightly and for bindist
    packaging. Bumping the submodule to 6e3c0816 includes support for libBF
    0.6 resolving these build failures (for now).
    
    The freeze file updates also remove the need to use allow-newer for the
    build to work.

--- .github/ci.sh
+++ .github/ci.sh
@@ -120,7 +120,6 @@ build() {
   ghc_ver="$(ghc --numeric-version)"
   cp cabal.GHC-"$ghc_ver".config cabal.project.freeze
   cabal v2-update
-  echo "allow-newer: all" >> cabal.project.local
   pkgs=(saw)
   if $IS_WIN; then
     echo "flags: -builtin-abc" >> cabal.project.local
@@ -128,7 +127,6 @@ build() {
   else
     pkgs+=(saw-remote-api)
   fi
-  echo "allow-newer: all" >> cabal.project.local
   tee -a cabal.project > /dev/null < cabal.project.ci
   if ! retry cabal v2-build "$@" "${pkgs[@]}"; then
     if [[ "$RUNNER_OS" == "macOS" ]]; then
