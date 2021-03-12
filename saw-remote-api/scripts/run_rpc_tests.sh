1cc20a356ba01856f49f33cd6437d4d23fab15d5
Lisanna Dettwyler <lisanna@galois.com>
2021-03-12 01:45:04 -0800

    fixes

--- saw-remote-api/scripts/run_rpc_tests.sh
+++ saw-remote-api/scripts/run_rpc_tests.sh
@@ -14,7 +14,7 @@ echo "Setting up python environment for remote server clients..."
 poetry install
 
 export SAW_SERVER=$(which saw-remote-api)
-if [[ -x "$SAW_SERVER" ]]; then
+if [[ ! -x "$SAW_SERVER" ]]; then
   echo "could not locate saw-remote-api executable - try executing with cabal v2-exec"
   exit 1
 fi
