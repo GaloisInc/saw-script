e9c894752df267a3e6f933ed71e5102039573114
Rob Dockins <rdockins@galois.com>
2021-04-13 09:40:21 -0700

    Add a new experimental "extract_uninterp" command.
    This command, when given a term at first-order type, will
    symbolically evaluate the term and extract any occurrences
    of functions mentioned in the given list as fresh variables.
    The arguments passed to the function are collected together into
    a tuple and returned together with the fresh variables generated
    to stand in for those occurrences.

--- src/SAWScript/Interpreter.hs
+++ src/SAWScript/Interpreter.hs
@@ -34,7 +34,7 @@ import Control.Applicative
 import Data.Traversable hiding ( mapM )
 #endif
 import qualified Control.Exception as X
-import Control.Monad (unless, (>=>))
+import Control.Monad ((>=>))
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as BS
 import Data.Foldable (foldrM)
@@ -606,12 +606,10 @@ set_color b = do
 
 print_value :: Value -> TopLevel ()
 print_value (VString s) = printOutLnTop Info s
-print_value (VTerm t) = do
+print_value (VTerm t) | null (getAllExts (ttTerm t)) = do
   sc <- getSharedContext
   cenv <- fmap rwCryptol getTopLevelRW
   let cfg = meSolverConfig (CEnv.eModuleEnv cenv)
-  unless (null (getAllExts (ttTerm t))) $
-    fail "term contains symbolic variables"
   sawopts <- getOptions
   t' <- io $ defaultTypedTerm sawopts sc cfg t
   opts <- fmap rwPPOpts getTopLevelRW
@@ -895,6 +893,12 @@ primitives = Map.fromList
     [ "Take a list of 'fresh_symbolic' variable and another term containing"
     , "those variables, and return a new lambda abstraction over the list of"
     , "variables."
+    ]
+
+  , prim "extract_uninterp" "[String] -> Term -> TopLevel (Term, [(String,[(Term, Term)])])"
+    (pureVal extract_uninterp)
+    Experimental
+    [ "Docs TODO!!"
     ]
 
   , prim "sbv_uninterpreted"   "String -> Term -> TopLevel Uninterp"
