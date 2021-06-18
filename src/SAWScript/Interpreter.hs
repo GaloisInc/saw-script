788aae115b8ab4a955c3ab564363bf0504e0dd3f
Rob Dockins <rdockins@galois.com>
2021-06-18 08:31:31 -0700

    Update the "extract_uninterp" command.
    This now emits warnings in case some of the returned
    arguments become captured, and allows the user to specify
    a list of terms to be treated as opaque by the evaluator.

--- src/SAWScript/Interpreter.hs
+++ src/SAWScript/Interpreter.hs
@@ -923,18 +923,24 @@ primitives = Map.fromList
     Experimental
     [ "Apply Cryptol defaulting rules to the given term." ]
 
-  , prim "extract_uninterp" "[String] -> Term -> TopLevel (Term, [(String,[(Term, Term)])])"
+  , prim "extract_uninterp" "[String] -> [String] -> Term -> TopLevel (Term, [(String,[(Term, Term)])])"
     (pureVal extract_uninterp)
     Experimental
     [ "Given a list of names of functions to treat as uninterpreted and a term, find all occurrences"
-    , "of the named functions and extract them. This will return pair, consisting of a rewritten"
-    , "term and a list of replacements.  The rewritten term will have each fully-applied occurrence of"
-    , "the named functions replaced by a fresh constant of the return type of the function."
-    , "The list of replacements consists of pairs (one for each named function) giving the name"
-    , "of that function and the replacement values for that function. The replacement values are"
-    , "a list of pairs of terms, one for each occurence that was replaced.  The first term in each"
-    , "pair gives the fresh constant appearing in the rewritten term.  The second term will be a"
-    , "tuple containing the arguments to the replaced function."
+    , "of the named functions and extract them."
+    , ""
+    , "The first argument is the list of \'uninterpreted\' functions to extract."
+    , "The second argument is a list of values to treat as opaque; they will not be unfolded during evaluation."
+    , "The third argument is the term to extract from."
+    , ""
+    , "This operation will return pair, consisting of a rewritten term and a list of replacements."
+    , "The rewritten term will have each fully-applied occurrence of the named functions replaced"
+    , "by a fresh constant of the return type of the function. The list of replacements consists"
+    , "of pairs (one for each named function) giving the name of that function and the replacement"
+    , "values for that function. The replacement values are a list of pairs of terms, one for each"
+    , "occurence that was replaced.  The first term in each  pair gives the fresh constant appearing"
+    , "in the rewritten term.  The second term will be a tuple containing the arguments to the"
+    , "replaced function."
     ]
 
   , prim "sbv_uninterpreted"   "String -> Term -> TopLevel Uninterp"
