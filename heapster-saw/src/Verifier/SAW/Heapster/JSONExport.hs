931d8023e3376371bfbd8d4fbca6a46f1a2fba92
Eric Mertens <emertens@galois.com>
2021-07-28 16:30:20 -0700

    Document JSONExport

--- heapster-saw/src/Verifier/SAW/Heapster/JSONExport.hs
+++ heapster-saw/src/Verifier/SAW/Heapster/JSONExport.hs
@@ -1,26 +1,27 @@
-{-# Language TemplateHaskell #-}
-{-# Language PolyKinds #-}
-{-# Language GADTs #-}
-{-# Language RankNTypes #-}
-{-# Language OverloadedStrings #-}
-{-# Language FlexibleContexts, FlexibleInstances, DefaultSignatures #-}
-{-# Language DataKinds #-}
-{-# Language PatternSynonyms #-}
-{-# Language ParallelListComp #-}
-{-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE OverloadedLists, OverloadedStrings #-}
+{-# LANGUAGE ParallelListComp #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-orphans #-} -- hobbits instances for Value
+module Verifier.SAW.Heapster.JSONExport
+  (JsonExport, JsonExport1, ppToJson)
+  where
 
-module Verifier.SAW.Heapster.JSONExport where
-
-import Control.Monad
 import Data.Aeson ( ToJSON(toJSON), Value(..), object )
 import Data.Binding.Hobbits
-import Data.BitVector.Sized ( BV, pattern BV )
+import Data.BitVector.Sized ( BV, asUnsigned )
 import Data.Kind (Type)
 import Data.Parameterized.BoolRepr ( BoolRepr )
 import Data.Parameterized.Context ( Assignment )
 import Data.Parameterized.Nonce (Nonce, indexValue)
 import Data.Parameterized.TraversableFC ( FoldableFC(toListFC) )
 import Data.Text (Text)
+import Data.Traversable (for)
 import Data.Type.RList ( mapToList )
 import GHC.Natural (Natural)
 import Lang.Crucible.FunctionHandle ( FnHandle )
@@ -39,54 +40,57 @@ instance NuMatching Value where
 instance Liftable Value where
     mbLift = unClosed . mbLift . fmap unsafeClose
 
-ppToJson :: PPInfo -> ValuePerm x -> Value
-ppToJson = jsonExport
+-- | Uniformly export the algebraic datatype structure
+-- Heapster permissions.
+ppToJson :: JsonExport a => PPInfo -> a -> Value
+ppToJson ppi = let ?ppi = ppi in jsonExport
 
+-- | Class of types that can be uniformly exported as JSON
+-- using the Heapster pretty-printing information for names
 class JsonExport a where
-    jsonExport :: PPInfo -> a -> Value
-    default jsonExport :: ToJSON a => PPInfo -> a -> Value
-    jsonExport _ = toJSON
+    jsonExport :: (?ppi::PPInfo) => a -> Value
+    default jsonExport :: ToJSON a => (?ppi::PPInfo) => a -> Value
+    jsonExport = toJSON
 
 instance JsonExport (Name (t :: CrucibleType)) where
-    jsonExport ppi x = toJSON (permPrettyString ppi x)
+    jsonExport = toJSON . permPrettyString ?ppi
 
 instance JsonExport1 f => JsonExport (Assignment f x) where
-    jsonExport ppi x = toJSON (toListFC (jsonExport1 ppi) x)
+    jsonExport = toJSON . toListFC jsonExport1
 
 instance JsonExport1 f => JsonExport (RAssign f x) where
-    jsonExport ppi x = toJSON (mapToList (jsonExport1 ppi) x)
+    jsonExport = toJSON . mapToList jsonExport1
 
 instance JsonExport b => JsonExport (Mb (a :: RList CrucibleType) b) where
-    jsonExport ppi mb = mbLift $ flip nuMultiWithElim1 mb $ \names body ->
+    jsonExport mb = mbLift $ flip nuMultiWithElim1 mb $ \names body ->
         object [
-            ("args", jsonExport ppi names),
-            ("body", jsonExport ppi body)
+            ("args", jsonExport names),
+            ("body", jsonExport body)
         ]
 
 instance JsonExport (Nonce a b) where
-    jsonExport _ = toJSON . indexValue
+    jsonExport = toJSON . indexValue
 
 instance JsonExport Bytes where
-    jsonExport _ = toJSON . show
+    jsonExport = toJSON . show -- Show instance is pretty
 
 instance JsonExport Ident where
-    jsonExport _ = toJSON . show
+    jsonExport = toJSON . show -- Show instance is pretty
 
 instance JsonExport FunctionName where
-    jsonExport _ = toJSON . show
+    jsonExport = toJSON . show -- Show instance is pretty
 
 instance JsonExport a => JsonExport (Maybe a) where
-    jsonExport _ Nothing = Null
-    jsonExport ppi (Just x) = jsonExport ppi x
+    jsonExport = maybe Null jsonExport
 
 instance (JsonExport a, JsonExport b) => JsonExport (a,b) where
-    jsonExport ppi (x,y) = toJSON (jsonExport ppi x, jsonExport ppi y)
+    jsonExport (x,y) = toJSON (jsonExport x, jsonExport y)
 
 instance JsonExport a => JsonExport [a] where
-    jsonExport ppi xs = toJSON (jsonExport ppi <$> xs)
+    jsonExport xs = toJSON (jsonExport <$> xs)
 
 instance JsonExport (BV n) where
-    jsonExport _ (BV n) = toJSON n
+    jsonExport = toJSON . asUnsigned
 
 instance JsonExport Natural
 instance JsonExport Integer
@@ -95,9 +99,10 @@ instance JsonExport Bool
 instance JsonExport Text
 instance {-# OVERLAPPING #-} JsonExport String
 
+-- | 'JsonExport' lifted to work on types with higher kinds
 class JsonExport1 f where
-    jsonExport1 :: PPInfo -> f a -> Value
-    default jsonExport1 :: JsonExport (f a) => PPInfo -> f a -> Value
+    jsonExport1 :: (?ppi::PPInfo) => f a -> Value
+    default jsonExport1 :: JsonExport (f a) => (?ppi::PPInfo) => f a -> Value
     jsonExport1 = jsonExport
 
 instance JsonExport1 BaseTypeRepr
@@ -107,43 +112,60 @@ instance JsonExport1 LOwnedPerm
 instance JsonExport1 PermExpr
 instance JsonExport1 ValuePerm
 
-let newNames :: String -> Int -> TH.Q [TH.Name]
-    newNames base n = forM [0..n-1] $ \i -> TH.newName (base ++ show i)
+-- This code generates generic JSON generation instances for
+-- algebraic data types.
+--
+-- - All instances will generate an object.
+-- - The object will have a @tag@ field containing the name
+--   of the constructor used.
+-- - Record constructors will add each record field to the
+--   object using the field name
+-- - Normal constructors with fields will have a field called
+--   @contents@. If this constructor has more than one parameter
+--   the @contents@ field will have a list. Otherwise it will
+--   have a single element.
+let fields :: String -> TH.ConstructorVariant -> [TH.ExpQ] -> TH.ExpQ
 
-    build :: String -> TH.ConstructorVariant -> [TH.ExpQ] -> TH.ExpQ
-    -- Record, use record field names as JSON field names
-    build tag (TH.RecordConstructor fieldNames) xs =
+    -- Record constructor, use record field names as JSON field names
+    fields tag (TH.RecordConstructor fieldNames) xs =
         TH.listE
           $ [| ("tag", tag) |]
           : [ [| (n, $x) |] | n <- TH.nameBase <$> fieldNames | x <- xs]
 
     -- No fields, so just report the constructor tag
-    build tag _ []  = [| [("tag", tag)] |]
+    fields tag _ []  = [| [("tag", tag)] |]
 
-    -- One field, just report that field
-    build tag _ [x] = [| [("tag", tag), ("contents", $x)] |]
+    -- One field, just report that field as @contents@
+    fields tag _ [x] = [| [("tag", tag), ("contents", $x)] |]
 
-    -- Multiple fields, report them as a list
-    build tag _ xs  = [| [("tag", tag), ("contents", toJSON $(TH.listE xs))] |]
+    -- Multiple fields, report them as a list as @contents@
+    fields tag _ xs  = [| [("tag", tag), ("contents", Array $(TH.listE xs))] |]
 
+    clauses :: TH.DatatypeInfo -> [TH.ClauseQ]
+    clauses info =
+        [do fieldVars <- for [0..length (TH.constructorFields con)-1] $ \i ->
+                            TH.newName ("x" ++ show i)
+            TH.clause
+              [TH.conP (TH.constructorName con) (TH.varP <$> fieldVars)]
+              (TH.normalB [|
+                object
+                    $(fields
+                        (TH.nameBase (TH.constructorName con))
+                        (TH.constructorVariant con)
+                        [ [| jsonExport $(TH.varE v) |] | v <- fieldVars ]) |])
+              []
+        | con <- TH.datatypeCons info ]
+
+    generateJsonExport :: TH.Name -> TH.DecQ
     generateJsonExport n =
       do info <- TH.reifyDatatype n
-         tVars <- newNames "a" (length (TH.datatypeInstTypes info))
-         let t = foldl TH.AppT (TH.ConT n) (TH.VarT <$> tVars)
-         [d| instance JsonExport $(pure t) where
-                jsonExport _ppi x = $(
-                    TH.caseE [|x|] [
-                      do fieldVars <- newNames "x" (length (TH.constructorFields con))
-                         TH.match
-                            (TH.conP (TH.constructorName con) (TH.varP <$> fieldVars))
-                            (TH.normalB [| object
-                            $(build
-                                (TH.nameBase (TH.constructorName con))
-                                (TH.constructorVariant con)
-                                [ [| jsonExport _ppi $(TH.varE v) |] | v <- fieldVars ]) |])
-                            []
-                         | con <- TH.datatypeCons info ]) |]
+         let t = foldl TH.appT (TH.conT n)
+               $ zipWith (\c _ -> TH.varT (TH.mkName [c])) ['a'..]
+               $ TH.datatypeInstTypes info
+         TH.instanceD (TH.cxt []) [t|JsonExport $t|]
+           [TH.funD 'jsonExport (clauses info)]
 
+    typesNeeded :: [TH.Name]
     typesNeeded =
         [''AtomicPerm, ''BaseTypeRepr, ''BoolRepr, ''BVFactor, ''BVProp,
         ''BVRange, ''CruCtx, ''FloatInfoRepr, ''FloatPrecisionRepr,
@@ -154,5 +176,5 @@ let newNames :: String -> Int -> TH.Q [TH.Name]
         ''PermExpr, ''PermOffset, ''StringInfoRepr, ''SymbolRepr, ''TypeRepr,
         ''ValuePerm, ''RWModality]
 
- in concat <$> traverse generateJsonExport typesNeeded
+ in traverse generateJsonExport typesNeeded
 
