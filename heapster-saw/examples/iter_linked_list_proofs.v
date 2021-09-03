b8e0859d6003e334c0bb5da78f6d72e69d98071e
Matthew Yacavone <matthew@yacavone.net>
2021-09-02 21:15:29 -0400

    get everything for iter is_elem working thanks to a block hint from Eddy

--- heapster-saw/examples/iter_linked_list_proofs.v
+++ heapster-saw/examples/iter_linked_list_proofs.v
@@ -20,6 +20,74 @@ Proof.
   simpl; f_equal; eauto.
 Qed.
 
+Hint Rewrite appendList_Nil_r : refinesM.
+
+
+Lemma no_errors_is_elem :
+  refinesFun is_elem (fun _ _ => noErrorsSpec).
+Proof.
+  unfold is_elem, is_elem__tuple_fun.
+  prove_refinement_match_letRecM_l.
+  - exact (fun _ _ => noErrorsSpec).
+  unfold noErrorsSpec.
+  prove_refinement.
+Qed.
+
+Definition is_elem_pure (x:bitvector 64) (l:list (bitvector 64))
+  : bitvector 64 :=
+  list_rect (fun _ => bitvector 64)
+            (intToBv 64 0)
+            (fun y l' rec => if bvEq 64 y x then intToBv 64 1 else rec) l.
+
+Lemma is_elem_pure_ref :
+  refinesFun is_elem (fun x l => returnM (is_elem_pure x l)).
+Proof.
+  unfold is_elem, is_elem__tuple_fun.
+  prove_refinement_match_letRecM_l.
+  - exact (fun x l => returnM (is_elem_pure x l)).
+  unfold is_elem_pure.
+  prove_refinement.
+  rewrite appendList_Nil_r; reflexivity.
+Qed.
+
+Definition is_elem_spec (x:bitvector 64) (l:list (bitvector 64))
+  : CompM (bitvector 64) :=
+  orM (assertM (List.In x l) >> returnM (intToBv 64 1))
+      (assertM (~ List.In x l) >> returnM (intToBv 64 0)).
+
+Lemma is_elem_spec_ref : refinesFun is_elem is_elem_spec.
+Proof.
+  unfold is_elem, is_elem__tuple_fun, is_elem_spec.
+  prove_refinement_match_letRecM_l.
+  - exact is_elem_spec.
+  unfold is_elem_spec.
+  prove_refinement.
+  (* The a0 = [] case. *)
+  - continue_prove_refinement_right.
+    easy.
+  (* The a0 = (s1 :: a1) case where a = s1. *)
+  - continue_prove_refinement_left.
+    left; easy.
+  (* The a0 = (s1 :: a1) case where a <> s1, and we inductively assume
+     the left assertion of our specification (in the letRec) *)
+  - continue_prove_refinement_left.
+    rewrite appendList_Nil_r in e_assert.
+    right; easy.
+  (* The a0 = (s1 :: a1) case where a <> s1, and we inductively assume
+     the right assertion of our specification (in the letRec) *)
+  - continue_prove_refinement_right.
+    rewrite appendList_Nil_r in e_assert.
+    intros []; easy.
+  (* The a0 = (s1 :: a1) case where a <> s1, and we inductively assume
+     the left assertion of our specification (out of the letRec) *)
+  - continue_prove_refinement_left.
+    rewrite appendList_Nil_r in e_assert; easy.
+  (* The a0 = (s1 :: a1) case where a <> s1, and we inductively assume
+     the right assertion of our specification (out of the letRec) *)
+  - continue_prove_refinement_right.
+    rewrite appendList_Nil_r in e_assert; easy.
+Qed.
+
 
 Definition incr_list_invar :=
   @list_rect (bitvector 64) (fun _ => Prop) True
