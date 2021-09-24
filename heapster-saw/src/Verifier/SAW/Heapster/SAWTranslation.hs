8215901364e9673bb940b382d36c2d7bb3c081a3
Eddy Westbrook <westbrook@galois.com>
2021-09-24 06:44:04 -0700

    whoops, fixed the translation of SImpl_ElimLLVMBlockField

--- heapster-saw/src/Verifier/SAW/Heapster/SAWTranslation.hs
+++ heapster-saw/src/Verifier/SAW/Heapster/SAWTranslation.hs
@@ -2944,7 +2944,7 @@ translateSimplImpl (ps0 :: Proxy ps0) mb_simpl m = case mbMatch mb_simpl of
     do ttrans <- translateSimplImplOutHead mb_simpl
        withPermStackM id
          (\(pctx :>: ptrans) ->
-           pctx :>: typeTransF ttrans [transTerm1 ptrans])
+           pctx :>: typeTransF (tupleTypeTrans ttrans) [transTerm1 ptrans])
          m
 
   [nuMP| SImpl_IntroLLVMBlockArray _ _ |] ->
