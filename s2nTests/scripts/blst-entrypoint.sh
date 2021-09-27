bd1c6913675c5cf2427f3a38df0e6a0bbb9a578c
Aaron Tomb <atomb@galois.com>
2021-09-27 09:21:13 -0700

    Make binaries executable in BLS tests

--- s2nTests/scripts/blst-entrypoint.sh
+++ s2nTests/scripts/blst-entrypoint.sh
@@ -7,6 +7,7 @@ cp /saw-bin/saw bin/saw
 
 wget --quiet -O solvers.zip "https://github.com/GaloisInc/what4-solvers/releases/download/snapshot-20210917/ubuntu-18.04-bin.zip"
 (cd bin && unzip -o ../solvers.zip)
+chmod +x bin/*
 
 export PATH=/workdir/bin:$PATH
 export CRYPTOLPATH=/workdir/cryptol-specs:/workdir/spec
