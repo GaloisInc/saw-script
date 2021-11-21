38d6d853565015543b4d90b9f63f3da95138ac93
Eddy Westbrook <westbrook@galois.com>
2021-11-21 15:15:07 -0800

    added the incr_u64_ptr_byte example to c_data to test out the new field split and truncate rules

--- heapster-saw/examples/c_data.c
+++ heapster-saw/examples/c_data.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+/* Increment the first byte pointed to by a 64-bit word pointer */
+void incr_u64_ptr_byte (uint64_t *x) {
+  uint8_t *x_byte = (uint8_t*)x;
+  (*x_byte)++;
+}
+
 typedef struct padded_struct {
   uint64_t padded1;
   uint8_t padded2;
@@ -8,6 +14,7 @@ typedef struct padded_struct {
   uint8_t padded4;
 } padded_struct;
 
+/* Allocated a padded_struct */
 padded_struct *alloc_padded_struct (void) {
   padded_struct *ret = malloc (sizeof(padded_struct));
   ret->padded1 = 0;
@@ -17,6 +24,7 @@ padded_struct *alloc_padded_struct (void) {
   return ret;
 }
 
+/* Increment all fields of a padded_struct */
 void padded_struct_incr_all (padded_struct *p) {
   p->padded1++;
   p->padded2++;
