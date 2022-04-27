00879423d6851852d1ffb969cf3c5301a181ec0c
Chris Phifer <cphifer@galois.com>
2022-04-27 15:24:59 -0500

    Change Value type to be just u64

--- heapster-saw/examples/interp.rs
+++ heapster-saw/examples/interp.rs
@@ -10,16 +10,10 @@ pub enum PrimOp2 {
     AddOp, MulOp
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum Lit {
-    BoolLit (bool),
-    IntLit (u64),
-}
-
 #[derive(Debug, Clone, PartialEq)]
 #[repr(u64)]
 pub enum Expr {
-    LitExpr(Lit),
+    LitExpr(u64),
     VarExpr(Var),
     Apply2Expr(PrimOp2, Box<Expr>, Box<Expr>)
 }
@@ -36,11 +30,7 @@ pub enum Stmt {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum Value {
-    BoolValue (bool),
-    IntValue (u64),
-    StringValue (String)
-}
+pub struct Value(u64);
 
 #[derive(Debug, Clone, PartialEq)]
 #[repr(u64)]
@@ -48,7 +38,6 @@ pub enum Error {
     UnboundVar(Var),
     UnknownFun(FunName),
     Unimplemented,
-    IncorrectType,
     WrongNumberOfArgs,
 }
 
@@ -67,27 +56,6 @@ pub struct Env {
 pub fn fresh_env_from_env <'a> (e : &'a Env) -> Env {
     Env { var_map : Vec::new (),
           fun_map : e.fun_map.clone () }
-}
-
-pub fn value_bool (v:Value) -> Result<bool,Error> {
-    match v {
-        Value::BoolValue (b) => Ok (b),
-        _ => Err (Error::IncorrectType),
-    }
-}
-
-pub fn value_u64 (v:Value) -> Result<u64,Error> {
-    match v {
-        Value::IntValue (i) => Ok (i),
-        _ => Err (Error::IncorrectType)
-    }
-}
-
-pub fn lit_value <'a> (l:&'a Lit) -> Value {
-    match l {
-        Lit::BoolLit (b) => Value::BoolValue (*b),
-        Lit::IntLit (i) => Value::IntValue (*i),
-    }
 }
 
 pub fn ref_ref_eq <'a, 'b, 'c, T: Eq> (v1 : &'a T, v2 : &'b&'c T) -> bool {
@@ -112,48 +80,46 @@ pub fn fun_lookup <'a,'b> (env : &'a Env, f : &'b FunName) -> Result<&'a FunDef,
     }
 }
 
-pub fn eval_primop2 <'a> (f : &'a PrimOp2, v1: Value, v2:Value) -> Result<Value,Error> {
+pub fn eval_primop2 <'a> (f : &'a PrimOp2, v1: Value, v2:Value) -> Value {
     match f {
-        PrimOp2::AddOp => { let i1 = value_u64 (v1)?;
-                            let i2 = value_u64 (v2)?;
-                            Ok (Value::IntValue (i1 + i2)) },
-        PrimOp2::MulOp => { let i1 = value_u64 (v1)?;
-                            let i2 = value_u64 (v2)?;
-                            Ok (Value::IntValue (i1 + i2)) },
+        PrimOp2::AddOp => { let Value(v1) = v1;
+                            let Value(v2) = v2;
+                            Value(v1 + v2) },
+        PrimOp2::MulOp => { let Value(v1) = v1;
+                            let Value(v2) = v2;
+                            Value(v1 * v2) },
     }
 }
 
 pub fn eval_expr <'a, 'b> (env : &'a Env, e : &'b Expr) -> Result<Value,Error> {
     match e {
-        Expr::LitExpr (l) => Ok (lit_value (l)),
+        Expr::LitExpr (l) => Ok(Value(*l)),
         Expr::VarExpr (x) => env_lookup (env, x),
         Expr::Apply2Expr (f, arg1, arg2) =>
             { let v1 = eval_expr (env, arg1)?;
               let v2 = eval_expr (env, arg2)?;
-              eval_primop2 (f, v1, v2) }
+              Ok(eval_primop2 (f, v1, v2)) }
     }
 }
 
 pub fn eval_stmt <'a, 'b> (env : &'a mut Env, stmt : &'b Stmt) -> Result<Value,Error> {
     match stmt {
         Stmt::Return (x) => env_lookup (env, x),
-        Stmt::Skip => Ok (Value::IntValue (0)),
+        Stmt::Skip => Ok (Value(0)),
         Stmt::Assign (x,e) =>
         { let v = eval_expr (env, e) ?;
           env_update (env, x, v);
-          Ok (Value::IntValue (0))
+          Ok (Value(0))
         }
         Stmt::IfStmt (e, stmt1, stmt2) =>
-        { let v = eval_expr (env, e) ?;
-          let b = value_bool (v) ?;
-          if b { eval_stmt (env, stmt1) } else { eval_stmt (env, stmt2) }
+        { let Value(v) = eval_expr (env, e) ?;
+          if v != 0 { eval_stmt (env, stmt1) } else { eval_stmt (env, stmt2) }
         },
         Stmt::WhileStmt (e, body) =>
-        { let v = eval_expr (env, e) ?;
-          let b = value_bool (v) ?;
-          if b { let _ = eval_stmt (env, body) ?;
-                 eval_stmt (env, stmt) }
-          else { Ok (Value::IntValue (0)) }}
+        { let Value(v) = eval_expr (env, e) ?;
+          if v != 0 { let _ = eval_stmt (env, body) ?;
+                      eval_stmt (env, stmt) }
+          else { Ok (Value(0)) }}
         Stmt::CallStmt (f, args) =>
         { let f_def = fun_lookup (env, f) ?;
           let mut new_env = fresh_env_from_env (env);
