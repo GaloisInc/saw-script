24276e38860fdc5d4e52d6c60eea9946989317c2
Samuel Breese <samuel@chame.co>
2022-05-08 19:36:57 -0400

    Refactor a bit

--- saw-remote-api/src/SAWServer/Eval.hs
+++ saw-remote-api/src/SAWServer/Eval.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -23,6 +24,7 @@ import Data.Aeson
 import qualified Argo
 import qualified Argo.Doc as Doc
 import CryptolServer.Data.Expression ( Expression(..), getCryptolExpr )
+import Verifier.SAW.TypedTerm (TypedTerm)
 import qualified SAWScript.Builtins as SB
 import qualified SAWScript.Value as SV
 import SAWServer
@@ -32,29 +34,25 @@ import SAWServer
 import SAWServer.CryptolExpression ( CryptolModuleException(..), getTypedTermOfCExp )
 import SAWServer.TopLevel ( tl )
 
-data EvalIntParams cryptolExpr =
-  EvalIntParams
+data EvalParams a cryptolExpr =
+  EvalParams
   { evalIntExpr :: cryptolExpr
   }
 
-instance (FromJSON cryptolExpr) => FromJSON (EvalIntParams cryptolExpr) where
+instance (FromJSON cryptolExpr) => FromJSON (EvalParams a cryptolExpr) where
   parseJSON =
-    withObject "SAW/eval int params" $ \o ->
-    EvalIntParams <$> o .: "expr"
+    withObject "SAW/eval params" $ \o ->
+    EvalParams <$> o .: "expr"
 
-data EvalIntResult =
-  EvalIntResult
-  { evalIntValue :: Integer
+data EvalResult a =
+  EvalResult
+  { evalValue :: a
   }
 
-instance ToJSON EvalIntResult where
-  toJSON r = object [ "value" .= evalIntValue r ]
+instance ToJSON a => ToJSON (EvalResult a) where
+  toJSON r = object [ "value" .= evalValue r ]
 
-evalIntDescr :: Doc.Block
-evalIntDescr =
-  Doc.Paragraph [ Doc.Text "Attempt to evaluate the given term to a concrete integer." ]
-
-instance Doc.DescribedMethod (EvalIntParams cryptolExpr) EvalIntResult where
+instance Doc.DescribedMethod (EvalParams Integer cryptolExpr) (EvalResult Integer) where
   parameterFieldDescription =
     [ ("expr",
        Doc.Paragraph [Doc.Text "The Cryptol expression to evaluate."])
@@ -64,43 +62,7 @@ instance Doc.DescribedMethod (EvalIntParams cryptolExpr) EvalIntResult where
       Doc.Paragraph [Doc.Text "The integer value of the expresssion."])
     ]
 
-evalInt :: EvalIntParams Expression -> Argo.Command SAWState EvalIntResult
-evalInt params = do
-  state <- Argo.getState
-  fileReader <- Argo.getFileReader
-  let cenv = SV.rwCryptol (view sawTopLevelRW state)
-      bic = view sawBIC state
-  cexp <- getCryptolExpr $ evalIntExpr params
-  (eterm, warnings) <- liftIO $ getTypedTermOfCExp fileReader (SV.biSharedContext bic) cenv cexp
-  t <- case eterm of
-         Right (t, _) -> return t -- TODO: report warnings
-         Left err -> throw $ CryptolModuleException err warnings
-  i <- tl $ SB.eval_int t
-  pure $ EvalIntResult i
-
-data EvalBoolParams cryptolExpr =
-  EvalBoolParams
-  { evalBoolExpr :: cryptolExpr
-  }
-
-instance (FromJSON cryptolExpr) => FromJSON (EvalBoolParams cryptolExpr) where
-  parseJSON =
-    withObject "SAW/eval bool params" $ \o ->
-    EvalBoolParams <$> o .: "expr"
-
-data EvalBoolResult =
-  EvalBoolResult
-  { evalBoolValue :: Bool
-  }
-
-instance ToJSON EvalBoolResult where
-  toJSON r = object [ "value" .= evalBoolValue r ]
-
-evalBoolDescr :: Doc.Block
-evalBoolDescr =
-  Doc.Paragraph [ Doc.Text "Attempt to evaluate the given term to a concrete boolean." ]
-
-instance Doc.DescribedMethod (EvalBoolParams cryptolExpr) EvalBoolResult where
+instance Doc.DescribedMethod (EvalParams Bool cryptolExpr) (EvalResult Bool) where
   parameterFieldDescription =
     [ ("expr",
        Doc.Paragraph [Doc.Text "The Cryptol expression to evaluate."])
@@ -110,16 +72,30 @@ instance Doc.DescribedMethod (EvalBoolParams cryptolExpr) EvalBoolResult where
       Doc.Paragraph [Doc.Text "The boolean value of the expresssion."])
     ]
 
-evalBool :: EvalBoolParams Expression -> Argo.Command SAWState EvalBoolResult
-evalBool params = do
+eval :: (TypedTerm -> SV.TopLevel a) -> EvalParams a Expression -> Argo.Command SAWState (EvalResult a)
+eval f params = do
   state <- Argo.getState
   fileReader <- Argo.getFileReader
   let cenv = SV.rwCryptol (view sawTopLevelRW state)
       bic = view sawBIC state
-  cexp <- getCryptolExpr $ evalBoolExpr params
+  cexp <- getCryptolExpr $ evalIntExpr params
   (eterm, warnings) <- liftIO $ getTypedTermOfCExp fileReader (SV.biSharedContext bic) cenv cexp
   t <- case eterm of
          Right (t, _) -> return t -- TODO: report warnings
          Left err -> throw $ CryptolModuleException err warnings
-  b <- tl $ SB.eval_bool t
-  pure $ EvalBoolResult b
+  i <- tl $ f t
+  pure $ EvalResult i
+
+evalIntDescr :: Doc.Block
+evalIntDescr =
+  Doc.Paragraph [ Doc.Text "Attempt to evaluate the given term to a concrete integer." ]
+
+evalInt :: EvalParams Integer Expression -> Argo.Command SAWState (EvalResult Integer)
+evalInt = eval SB.eval_int
+
+evalBoolDescr :: Doc.Block
+evalBoolDescr =
+  Doc.Paragraph [ Doc.Text "Attempt to evaluate the given term to a concrete boolean." ]
+
+evalBool :: EvalParams Bool Expression -> Argo.Command SAWState (EvalResult Bool)
+evalBool = eval SB.eval_bool
