bd40d9095397f6692a15570e727aef049399bcbf
Rob Dockins <rdockins@galois.com>
2022-06-09 15:48:06 -0700

    Update saw-remote-api

--- saw-remote-api/src/SAWServer.hs
+++ saw-remote-api/src/SAWServer.hs
@@ -242,6 +242,7 @@ initialState readFileFn =
                 , rwPathSatSolver = CC.PathSat_Z3
                 , rwSkipSafetyProofs = False
                 , rwSingleOverrideSpecialCase = False
+                , rwSequentGoals = False
                 }
      return (SAWState emptyEnv bic [] ro rw M.empty)
 
