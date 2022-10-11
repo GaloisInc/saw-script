c82ec7171ebfd240f9437eb7f8fb6cc0a3fe8e1f
Ryan Scott <rscott@galois.com>
2022-10-11 09:44:12 -0400

    Minor documentation fix for heapster_assume_fun_rename
    `nm_t` is a typo and is actually meant to refer to `nm_to`.
    
    [ci skip]

--- src/SAWScript/Interpreter.hs
+++ src/SAWScript/Interpreter.hs
@@ -4068,7 +4068,7 @@ primitives = Map.fromList
     "HeapsterEnv -> String -> String -> String -> String -> TopLevel HeapsterEnv"
     (bicVal heapster_assume_fun_rename)
     Experimental
-    [ "heapster_assume_fun_rename nm nm_t perms trans assumes that function nm"
+    [ "heapster_assume_fun_rename nm nm_to perms trans assumes that function nm"
     , " has permissions perms and translates to the SAW core term trans. If"
     , " trans is not an identifier then it is bound to the defined name nm_to."
     ]
