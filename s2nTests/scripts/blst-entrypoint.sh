8095d01d5a8e0cc21508e641bee812177d552e35
Samuel Breese <samuel@chame.co>
2022-11-21 13:14:02 -0500

    Try 22.04 binaries in BLST proof

--- s2nTests/scripts/blst-entrypoint.sh
+++ s2nTests/scripts/blst-entrypoint.sh
@@ -5,7 +5,7 @@ cd /workdir
 ./scripts/install.sh
 cp /saw-bin/saw bin/saw
 
-wget --quiet -O solvers.zip "https://github.com/GaloisInc/what4-solvers/releases/download/snapshot-20220902/ubuntu-20.04-bin.zip"
+wget --quiet -O solvers.zip "https://github.com/GaloisInc/what4-solvers/releases/download/snapshot-20220902/ubuntu-22.04-bin.zip"
 (cd bin && unzip -o ../solvers.zip)
 chmod +x bin/*
 
