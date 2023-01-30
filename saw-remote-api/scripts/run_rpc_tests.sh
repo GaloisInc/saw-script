23cdc79bbc3f8c0deab62d9bd0dc51ee039b0496
Ryan Scott <rscott@galois.com>
2023-01-30 17:39:32 -0500

    Python: Bump argo-client, mypy, cryptol versions
    This patch:
    
    * Bumps the `mypy` lower bounds to avoid incurring a dependency on `typed-ast`,
      which is being EOL'd soon. See python/typed_ast#179.
    * Bumps the `argo` submodule to bring in the changes from
      GaloisInc/argo#195, which makes corresponding changes on the `argo` side.
    * Bumps the `cryptol` submodule to bring in the changes from
      GaloisInc/cryptol#1493, which makes the corresponding changes on the `cryptol`
      side.

--- saw-remote-api/scripts/run_rpc_tests.sh
+++ saw-remote-api/scripts/run_rpc_tests.sh
@@ -18,7 +18,8 @@ poetry install
 
 echo "Typechecking code with mypy..."
 # Don't run mypy on tests/ yet, as it doesn't play well with mypy. See #1125.
-run_test poetry run mypy saw_client/
+run_test poetry run mypy --install-types --non-interactive saw_client/ || true
+run_test poetry run mypy --install-types --non-interactive saw_client/
 
 export SAW_SERVER=$(which saw-remote-api)
 if [[ ! -x "$SAW_SERVER" ]]; then
