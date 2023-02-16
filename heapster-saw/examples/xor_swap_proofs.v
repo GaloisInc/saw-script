10aacd5485abd53d368d13a22a6ae166e874de62
scuellar <cuellar.s@gmail.com>
2023-02-16 13:37:24 -0500

    update proofs xor

--- heapster-saw/examples/xor_swap_proofs.v
+++ heapster-saw/examples/xor_swap_proofs.v
@@ -7,40 +7,77 @@ From CryptolToCoq Require Import SAWCoreVectorsAsCoqVectors.
 From CryptolToCoq Require Import SAWCoreBitvectors.
 
 From CryptolToCoq Require Import SAWCorePrelude.
-From CryptolToCoq Require Import CompMExtra.
+(* From CryptolToCoq Require Import CompMExtra. *)
+From CryptolToCoq Require Import SpecMExtra.
+From EnTree  Require Import Automation.
 
 Require Import Examples.xor_swap_gen.
 Import xor_swap.
 
+Set Bullet Behavior "Strict Subproofs".
 
-Definition xor_swap_spec x1 x2 :
-  CompM (bitvector 64 * bitvector 64) :=
-  returnM (x2, x1).
+Definition xor_swap_spec (x1 x2: bitvector 64) := (x2, x1).
 Arguments xor_swap_spec /.
 
-Lemma no_errors_xor_swap : refinesFun xor_swap (fun _ _ => noErrorsSpec).
-Proof.
-  unfold xor_swap, xor_swap__tuple_fun, noErrorsSpec.
-  time "no_errors_xor_swap" prove_refinement.
-Qed.
 
-
-Lemma bvXor_twice_r n x y :
-  SAWCorePrelude.bvXor n (SAWCorePrelude.bvXor n x y) y = x.
+(* First prove safety (i.e. no errors) *)
+Lemma xor_swap_not_error (x y: bitvector 64) :
+  spec_refines eqPreRel eqPostRel eq
+    (xor_swap x y)
+    (total_spec (fun _ =>  True) (fun xy r => True ) (x,y)).
 Proof.
-  rewrite <- bvXor_assoc. rewrite bvXor_same. rewrite bvXor_zero. reflexivity.
-Qed.
-
-Lemma bvXor_twice_l n x y :
-  SAWCorePrelude.bvXor n (SAWCorePrelude.bvXor n y x) y = x.
-Proof.
-  rewrite bvXor_comm. rewrite bvXor_assoc.
-  rewrite bvXor_same. rewrite bvXor_comm. rewrite bvXor_zero. reflexivity.
-Qed.
-
-Lemma xor_swap_correct : refinesFun xor_swap xor_swap_spec.
-Proof.
+  intros.
+  unfold xor_swap, xor_swap__bodies.
   prove_refinement.
-  subst. rewrite bvXor_twice_r. rewrite bvXor_twice_l.
+  - wellfounded_none.
+  - prepost_case 0 0.
+    + exact (x0 = x2 /\ x1 = x3).
+    + exact (r_x = r_x1 /\ r_x0 = r_x2).
+    + prepost_exclude_remaining.
+  - prove_refinement_continue.
+Qed.
+      
+Local Hint Extern 10 (spec_refines eqPreRel eqPostRel eq (xor_swap _ _) _) =>
+        simple apply xor_swap_not_error: refine_proofs.
+
+
+(* | Nice notation for better reading in this file*)
+Local Infix "^^" := (SAWCorePrelude.bvXor 64) (at level 30).
+Lemma bvXor_twice_l:
+  forall x1 x2 : bitvector 64, x2 ^^ (x2 ^^ x1) = x1.
+Proof.
+  intros; rewrite bvXor_assoc, bvXor_same, bvXor_comm, bvXor_zero.
   reflexivity.
 Qed.
+
+Lemma bvXor_twice_r:
+  forall x1 x2 : bitvector 64, (x1 ^^ x2) ^^ x2 = x1.
+Proof.
+  intros; rewrite <- bvXor_assoc, bvXor_same, bvXor_zero.
+  reflexivity.
+Qed.    
+
+(* | Now we can prove correctness *)
+Lemma xor_swap_spec_ref (x y: bitvector 64) :
+  spec_refines eqPreRel eqPostRel eq
+    (xor_swap x y)
+    (total_spec
+       (fun _ =>  True)
+       (fun xy r => r = xor_swap_spec (fst xy) (snd xy))
+       (x,y)).
+Proof.
+  intros.
+  unfold xor_swap, xor_swap__bodies.
+  prove_refinement.
+  - wellfounded_none.
+  - prepost_case 0 0.
+    + exact (x0 = x2 /\ x1 = x3).
+    + exact (r_x = r_x1 /\ r_x0 = r_x2).
+    + prepost_exclude_remaining.
+  - prove_refinement_continue.
+    + apply bvXor_twice_l.
+    + apply bvXor_twice_r.
+Qed.
+      
+Local Hint Extern 10 (spec_refines eqPreRel eqPostRel eq (xor_swap _ _) _) =>
+       simple apply xor_swap_spec_ref: refine_proofs.
