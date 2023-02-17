4f0449955b548645b06ad1259f5f725723eda85e
scuellar <cuellar.s@gmail.com>
2023-02-16 20:59:06 -0500

    BROKEN: c_data_proofs.v is broken. pushing to get assitance.

--- heapster-saw/examples/c_data_proofs.v
+++ heapster-saw/examples/c_data_proofs.v
@@ -6,18 +6,17 @@ From CryptolToCoq Require Import SAWCoreVectorsAsCoqVectors.
 From CryptolToCoq Require Import SAWCoreBitvectors.
 
 From CryptolToCoq Require Import SAWCorePrelude.
-From CryptolToCoq Require Import CompMExtra.
+From EnTree  Require Import Automation.
 
+Require Import Examples.common.
 Require Import Examples.c_data_gen.
 Import c_data.
 
 Import SAWCorePrelude.
 
-Lemma no_errors_incr_u64_ptr_byte :
-  refinesFun incr_u64_ptr_byte (fun _ => noErrorsSpec).
-  unfold incr_u64_ptr_byte, incr_u64_ptr_byte__tuple_fun, noErrorsSpec.
-  time "no_errors_incr_u64_ptr_byte" prove_refinement.
-Qed.
+Lemma no_errors_incr_u64_ptr_byte x:
+  spec_refines_eq (incr_u64_ptr_byte x) (safety_spec (x)).
+Proof. solve_trivial_spec 0 0. Qed.
 
 Lemma no_errors_alloc_padded_struct :
   refinesFun alloc_padded_struct noErrorsSpec.
