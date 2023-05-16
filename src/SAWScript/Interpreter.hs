3a71946f4e3f8730b04afc8e132e889366ecbd61
Sam Cowger <sam@galois.com>
2023-05-16 09:49:33 -0700

    Alert users to fragility of memoization indices

--- src/SAWScript/Interpreter.hs
+++ src/SAWScript/Interpreter.hs
@@ -1849,7 +1849,9 @@ primitives = Map.fromList
     , "without generating `let` bindings for the provided indices. For"
     , "example, `print_goal_inline [1,9,3]` will print the goal without"
     , "inlining the variables that would otherwise be abstracted as `x@1`,"
-    , " `x@9`, and `x@3`."
+    , " `x@9`, and `x@3`. These indices are assigned deterministically with"
+    , "regard to a particular goal, but are not persistent across goals. As"
+    , "such, this should be used primarily when debugging a proof."
     ]
   , prim "write_goal" "String -> ProofScript ()"
     (pureVal write_goal)
