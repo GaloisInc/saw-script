b45236b7e1bbe999a5bc66c1b3d3c43bc5105d1d
Sam Cowger <sam@galois.com>
2023-05-24 14:20:47 -0700

    Work on statement/block/expression truncation after point

--- saw-lsp/src/Handlers/Custom/InterpretToPoint.hs
+++ saw-lsp/src/Handlers/Custom/InterpretToPoint.hs
@@ -6,25 +6,32 @@
 module Handlers.Custom.InterpretToPoint where
 
 import Control.Lens ((^.))
-import Control.Monad.IO.Class (MonadIO (liftIO))
-import Control.Monad.Reader (asks)
-import Data.Aeson as Aeson
+import Data.Aeson qualified as Aeson
 import Data.Aeson.Types (parseEither)
-import Data.Bifunctor (Bifunctor (first), second)
-import Data.List (intercalate)
+import Data.Bifunctor (first)
+import Data.Maybe (mapMaybe)
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Error (internalError)
 import Language.LSP.Server
-import Language.LSP.Types hiding (Position)
+import Language.LSP.Types
+  ( MessageType (MtError),
+    Method (CustomMethod),
+    NormalizedUri,
+    RequestMessage,
+    ResponseError,
+    SMethod (SCustomMethod, SWindowShowMessage),
+    ShowMessageParams (ShowMessageParams),
+    toNormalizedUri,
+  )
 import Language.LSP.Types.Lens qualified as LSP
 import Language.LSP.VFS (VirtualFile, virtualFileText)
 import Monad
-import REPL (command)
-import SAW (InterpretParams (..), Position, interpretToPoint)
+import SAW (InterpretParams (..), Position (..))
+import SAWScript.AST (Expr (..), Stmt (..), prettyWholeModule)
 import SAWScript.Lexer (lexSAW)
 import SAWScript.Parser (parseModule)
-import SAWScript.AST (Stmt)
+import SAWScript.Position (Pos (..), getPos)
 
 handleInterpretToPoint :: Handlers ServerM
 handleInterpretToPoint = requestHandler (SCustomMethod "$/interpretToPoint") doInterp
@@ -35,45 +42,112 @@ doInterp ::
   ServerM ()
 doInterp request responder =
   do
-    sendNotification SWindowShowMessage (ShowMessageParams MtError "where did I go so wrong?")
-    responder (Left (internalError "nah brah"))
-    -- InterpretParams {..} <- liftEither (fromParams ps)
-    -- vf <- getVF (toNormalizedUri uri)
-    -- let text = virtualFileText vf
-    -- undefined
+    debug "doInterp"
+    interpParams <- liftEither (fromParams ps)
+    debug' (show interpParams)
+    fileText <- virtualFileText <$> resolveUri (toNormalizedUri (uri interpParams))
+    fileStmts <- liftEither (first (internalError . Text.pack) (parseFile fileText))
+    let Position l c = posn interpParams
+        mark = Position (l + 1) (c + 1)
+    case truncateAfterPoint mark fileStmts of
+      [] ->
+        let msg = Text.pack $ "would truncate all " <> show (length fileStmts) <> " statements"
+         in sendNotification SWindowShowMessage (ShowMessageParams MtError msg)
+      stmts ->
+        do
+          let orig = show (length fileStmts)
+              new = show (length stmts)
+              msg = Text.pack $ "would truncate " <> orig <> " statements to " <> new <> " statements"
+          debug' (show (prettyWholeModule stmts))
+          sendNotification SWindowShowMessage (ShowMessageParams MtError msg)
   where
     ps = request ^. LSP.params
 
-getVF :: NormalizedUri -> ServerM VirtualFile
-getVF uri =
+resolveUri :: NormalizedUri -> ServerM VirtualFile
+resolveUri uri =
   do
     vfM <- getVirtualFile uri
     liftMaybe (internalError "file not found") vfM
 
-interp :: Text -> Position -> ServerM String
-interp text posn =
+parseFile :: Text -> Either String [Stmt]
+parseFile text =
   do
-    stmts <- liftEither parsed
-    undefined
-  where
-    lexed = lexSAW "" (Text.unpack text)
-    parsed = first (\e -> internalError ("parse error: " <> Text.pack (show e))) $ parseModule lexed
+    let lexed = lexSAW "TODO" (Text.unpack text)
+    first show (parseModule lexed)
 
-  
+truncateAfterPoint :: Position -> [Stmt] -> [Stmt]
+truncateAfterPoint mark = mapMaybe (truncateStmt mark)
+
+truncateStmt :: Position -> Stmt -> Maybe Stmt
+truncateStmt mark stmt
+  | pos `endsBefore` mark = Just stmt
+  | pos `startsAfter` mark = Nothing
+  | otherwise =
+      case stmt of
+        StmtBind posn pat ty e -> StmtBind posn pat ty <$> truncateLExpr mark e
+        _ -> undefined
+  where
+    pos = getPos stmt
+
+truncateLExpr :: Position -> Expr -> Maybe Expr
+truncateLExpr mark expr =
+  case expr of
+    LExpr pos e
+      | pos `endsBefore` mark -> Just expr
+      | pos `startsAfter` mark -> Nothing
+      | otherwise -> LExpr pos <$> truncateOverlappingExpr mark e
+    _ -> Nothing
+
+truncateOverlappingExpr :: Position -> Expr -> Maybe Expr
+truncateOverlappingExpr mark expr =
+  case expr of
+    Bool _ -> Just expr
+    String _ -> Just expr
+    Int _ -> Just expr
+    Code _ -> Just expr
+    CType _ -> Just expr
+    Array es -> Just (Array (mapMaybe goExpr es)) -- TODO
+    Block ss -> Just (Block (mapMaybe goStmt ss))
+    Tuple es -> Just expr -- TODO
+    Record binds -> Just expr
+    Index a i -> Just expr
+    Lookup e n -> Just expr
+    TLookup e i -> Just expr
+    Var _ -> Just expr
+    Function pat e -> Just expr
+    Application e1 e2 -> Application <$> goExpr e1 <*> goExpr e2
+    Let d e -> Just expr
+    TSig e ty -> Just expr
+    IfThenElse c t f -> Just expr
+    LExpr p e -> truncateLExpr mark expr
+  where
+    goExpr = truncateOverlappingExpr mark
+    goStmt = truncateStmt mark
+
+startsAfter :: Pos -> Position -> Bool
+startsAfter reference Position {..} =
+  case reference of
+    Range _ refStartLine refStartCol _ _ ->
+      case refStartLine `compare` line of
+        LT -> False
+        EQ -> refStartCol >= character
+        GT -> True
+    _ -> False
+
+endsBefore :: Pos -> Position -> Bool
+endsBefore pos Position {..} =
+  case pos of
+    Range _ _ _ refEndLine refEndCol ->
+      case refEndLine `compare` line of
+        LT -> True
+        EQ -> refEndCol <= character
+        GT -> False
+    _ -> False
+
+fromParams :: Aeson.Value -> Either ResponseError InterpretParams
+fromParams v = first (internalError . Text.pack) (parseEither Aeson.parseJSON v)
 
 {-
-Verify that the user has clicked just past a semicolon
-- But merely trust that they did so inside a proof script
-
-- Parse the entire module
-  - *Assume we're in a `do` block
-- Slot a `Bind`/`Expr` (`LExpr`?) dispatching to proof_subshell in the
-  appropriate slot in the block list
-- *Print the whole thing
-- Send it to the repl wholesale (`include` via tempfile? line by line?)
-
--}
-
 mkShowDocumentParams ::
   -- | the document to show
   Uri ->
@@ -85,12 +159,4 @@ mkShowDocumentParams ::
   Maybe Range ->
   ShowDocumentParams
 mkShowDocumentParams = ShowDocumentParams
-
-mkDocRequest :: InterpretParams -> Either ResponseError ShowDocumentParams
-mkDocRequest = undefined
-
-fromParams :: Aeson.Value -> Either ResponseError InterpretParams
-fromParams v =
-  case parseEither parseJSON v of
-    Left e -> Left (ResponseError InternalError (Text.pack e) Nothing)
-    Right p -> Right p
+-}
