4a8757947282ea753c15ea3ba916357739565da1
Sam Cowger <sam@galois.com>
2023-07-12 10:03:27 -0700

    Move truncation machinery to a module and document it

--- saw-lsp/src/Handlers/Custom/InterpretToPoint.hs
+++ saw-lsp/src/Handlers/Custom/InterpretToPoint.hs
@@ -11,10 +11,10 @@ import Data.Aeson ((.:))
 import Data.Aeson qualified as Aeson
 import Data.Aeson.Types qualified as Aeson
 import Data.Bifunctor (first)
-import Data.Maybe (mapMaybe)
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Error (internalError)
+import Handlers.Custom.InterpretToPoint.Truncate
 import Language.LSP.Server
 import Language.LSP.Types
   ( MessageType (..),
@@ -24,7 +24,6 @@ import Language.LSP.Types
     ResponseError,
     SMethod (..),
     ShowDocumentParams (ShowDocumentParams),
-    ShowDocumentResult (ShowDocumentResult),
     ShowMessageParams (ShowMessageParams),
     Uri,
     filePathToUri,
@@ -33,10 +32,9 @@ import Language.LSP.Types
 import Language.LSP.Types.Lens qualified as LSP
 import Language.LSP.VFS (VirtualFile, virtualFileText)
 import Monad
-import SAWScript.AST (Expr (..), Located (..), Pattern (..), Stmt (..), prettyWholeModule)
+import SAWScript.AST (Stmt (..), prettyWholeModule)
 import SAWScript.Lexer (lexSAW)
 import SAWScript.Parser (parseModule)
-import SAWScript.Position (Pos (..), getPos)
 import SAWT (drainSAWEnv, flushOutput, runStmt, runStmtCheckpoint)
 import System.IO.Temp (writeSystemTempFile)
 
@@ -50,17 +48,6 @@ data InterpretParams = InterpretParams
 instance Aeson.FromJSON InterpretParams where
   parseJSON = Aeson.withObject "InterpretParams" \v ->
     InterpretParams <$> v .: "posn" <*> v .: "offset" <*> v .: "uri"
-
--- Include absolute offset?
-data Position = Position
-  { line :: Int,
-    character :: Int
-  }
-  deriving (Show)
-
-instance Aeson.FromJSON Position where
-  parseJSON = Aeson.withObject "Position" \v ->
-    Position <$> v .: "line" <*> v .: "character"
 
 handleInterpretToPoint :: Handlers ServerM
 handleInterpretToPoint = requestHandler (SCustomMethod "$/interpretToPoint") doInterp
@@ -76,12 +63,7 @@ doInterp request responder =
     -- debug' (show interpParams)
     fileText <- virtualFileText <$> resolveUri (toNormalizedUri (uri interpParams))
     fileStmts <- liftEither (first (internalError . Text.pack) (parseFile fileText))
-    let Position l c = posn interpParams
-        mark = Position (l + 1) (c + 1)
-        var v = Var (Located v "" Unknown)
-        printGoal = StmtBind Unknown (PWild Nothing) Nothing (var "print_goal")
-        admit = StmtBind Unknown (PWild Nothing) Nothing (Application (var "admit") (String "TODO"))
-        fileStmts' = truncateAfterPointWith [printGoal, admit] mark fileStmts
+    let fileStmts' = truncateScript (posn interpParams) fileStmts
     case fileStmts' of
       [] ->
         let msg = Text.pack $ "would truncate all " <> show (length fileStmts) <> " statements"
@@ -123,75 +105,6 @@ parseFile text =
   do
     let lexed = lexSAW "TODO" (Text.unpack text)
     first show (parseModule lexed)
-
-truncateAfterPointWith :: [Stmt] -> Position -> [Stmt] -> [Stmt]
-truncateAfterPointWith additions mark = mapMaybe (truncateStmtWith additions mark)
-
-truncateStmtWith :: [Stmt] -> Position -> Stmt -> Maybe Stmt
-truncateStmtWith additions mark stmt
-  | pos `endsBefore` mark = Just stmt
-  | pos `startsAfter` mark = Nothing
-  | otherwise =
-      case stmt of
-        StmtBind posn pat ty e -> StmtBind posn pat ty <$> truncateLExprWith additions mark e
-        _ -> undefined
-  where
-    pos = getPos stmt
-
-truncateLExprWith :: [Stmt] -> Position -> Expr -> Maybe Expr
-truncateLExprWith additions mark expr =
-  case expr of
-    LExpr pos e
-      | pos `endsBefore` mark -> Just expr
-      | pos `startsAfter` mark -> Nothing
-      | otherwise -> LExpr pos <$> truncateOverlappingExprWith additions mark e
-    _ -> Nothing
-
-truncateOverlappingExprWith :: [Stmt] -> Position -> Expr -> Maybe Expr
-truncateOverlappingExprWith additions mark expr =
-  case expr of
-    Bool _ -> Just expr
-    String _ -> Just expr
-    Int _ -> Just expr
-    Code _ -> Just expr
-    CType _ -> Just expr
-    Array es -> Just (Array (mapMaybe goExpr es)) -- TODO
-    Block ss -> Just (Block (mapMaybe goStmt ss <> additions))
-    Tuple es -> Just expr -- TODO
-    Record binds -> Just expr
-    Index a i -> Just expr
-    Lookup e n -> Just expr
-    TLookup e i -> Just expr
-    Var _ -> Just expr
-    Function pat e -> Just expr
-    Application e1 e2 -> Application <$> goExpr e1 <*> goExpr e2
-    Let d e -> Just expr
-    TSig e ty -> Just expr
-    IfThenElse c t f -> Just expr
-    LExpr p e -> truncateLExprWith additions mark expr
-  where
-    goExpr = truncateOverlappingExprWith additions mark
-    goStmt = truncateStmtWith additions mark
-
-startsAfter :: Pos -> Position -> Bool
-startsAfter reference Position {..} =
-  case reference of
-    Range _ refStartLine refStartCol _ _ ->
-      case refStartLine `compare` line of
-        LT -> False
-        EQ -> refStartCol >= character
-        GT -> True
-    _ -> False
-
-endsBefore :: Pos -> Position -> Bool
-endsBefore pos Position {..} =
-  case pos of
-    Range _ _ _ refEndLine refEndCol ->
-      case refEndLine `compare` line of
-        LT -> True
-        EQ -> refEndCol <= character
-        GT -> False
-    _ -> False
 
 fromParams :: Aeson.Value -> Either ResponseError InterpretParams
 fromParams v = first (internalError . Text.pack) (Aeson.parseEither Aeson.parseJSON v)
---
+++ saw-lsp/src/Handlers/Custom/InterpretToPoint/Truncate.hs
@@ -0,0 +1,117 @@
+{-# LANGUAGE BlockArguments #-}
+
+module Handlers.Custom.InterpretToPoint.Truncate (Position (..), truncateScript) where
+
+import Data.Aeson ((.:))
+import Data.Aeson qualified as Aeson
+import Data.Maybe (mapMaybe)
+import SAWScript.AST (Expr (..), Located (..), Pattern (..), Stmt (..))
+import SAWScript.Position (Pos (..), getPos)
+
+-- Include absolute offset?
+data Position = Position
+  { line :: Int,
+    character :: Int
+  }
+  deriving (Show)
+
+instance Aeson.FromJSON Position where
+  parseJSON = Aeson.withObject "Position" \v ->
+    Position <$> v .: "line" <*> v .: "character"
+
+-- | At the given `Position`, which we assume to be within an expression
+-- containing a list of statements, insert a call to `print_goal` and a call to
+-- `admit`, and truncate the rest of the statements in the block, as well as the
+-- rest of the statements provided.
+truncateScript :: Position -> [Stmt] -> [Stmt]
+truncateScript (Position l c) stmts =
+  let mark = Position (l + 1) (c + 1)
+      mkVar v = Var (Located v "" Unknown)
+      printGoal = StmtBind Unknown (PWild Nothing) Nothing (mkVar "print_goal")
+      admit = StmtBind Unknown (PWild Nothing) Nothing (Application (mkVar "admit") (String "TODO"))
+   in truncateScriptWith [printGoal, admit] mark stmts
+
+-- | At the given `Position`, which we assume to be within an expression
+-- containing a list of statements, insert `additions` and truncate the rest of
+-- the statements in the block, as well as the rest of the statements provided.
+truncateScriptWith :: [Stmt] -> Position -> [Stmt] -> [Stmt]
+truncateScriptWith additions mark = mapMaybe (truncateBindStmtWith additions mark)
+
+-- | If `mark` describes a point in the middle of `stmt`, try to truncate
+-- statements within the expression `stmt` contains.
+truncateBindStmtWith :: [Stmt] -> Position -> Stmt -> Maybe Stmt
+truncateBindStmtWith additions mark stmt
+  | pos `endsBefore` mark = Just stmt
+  | pos `startsAfter` mark = Nothing
+  | otherwise =
+      case stmt of
+        StmtBind posn pat ty e -> StmtBind posn pat ty <$> truncateLExprWith additions mark e
+        -- TODO: this could be triggered if a user defines a block statement
+        -- using an introduction form other than bind (i.e. `<-`), which is
+        -- possible
+        _ -> undefined
+  where
+    pos = getPos stmt
+
+-- | If `mark` describes a point in the middle of `expr`, try to truncate
+-- statements inside `expr`.
+truncateLExprWith :: [Stmt] -> Position -> Expr -> Maybe Expr
+truncateLExprWith additions mark expr =
+  case expr of
+    LExpr pos e
+      | pos `endsBefore` mark -> Just expr
+      | pos `startsAfter` mark -> Nothing
+      | otherwise -> LExpr pos <$> truncateOverlappingExprWith additions mark e
+    _ -> Nothing
+
+-- Assuming `mark` describes a point in the middle of `expr`, if `expr` is of a
+-- form that contains a set of statements (e.g. arrays and blocks), truncate the
+-- statements following `mark` within that set. If not, leave the expression
+-- alone.
+truncateOverlappingExprWith :: [Stmt] -> Position -> Expr -> Maybe Expr
+truncateOverlappingExprWith additions mark expr =
+  case expr of
+    Bool _ -> Just expr
+    String _ -> Just expr
+    Int _ -> Just expr
+    Code _ -> Just expr
+    CType _ -> Just expr
+    Array es -> Just (Array (mapMaybe goExpr es)) -- TODO
+    Block ss -> Just (Block (mapMaybe goStmt ss <> additions))
+    Tuple es -> Just expr -- TODO
+    Record binds -> Just expr
+    Index a i -> Just expr
+    Lookup e n -> Just expr
+    TLookup e i -> Just expr
+    Var _ -> Just expr
+    Function pat e -> Just expr
+    Application e1 e2 -> Application <$> goExpr e1 <*> goExpr e2
+    Let d e -> Just expr
+    TSig e ty -> Just expr
+    IfThenElse c t f -> Just expr
+    LExpr p e -> truncateLExprWith additions mark expr
+  where
+    goExpr = truncateOverlappingExprWith additions mark
+    goStmt = truncateBindStmtWith additions mark
+
+-- | Does the `Pos` start after the `Position`?
+startsAfter :: Pos -> Position -> Bool
+startsAfter reference Position {..} =
+  case reference of
+    Range _ refStartLine refStartCol _ _ ->
+      case refStartLine `compare` line of
+        LT -> False
+        EQ -> refStartCol >= character
+        GT -> True
+    _ -> False
+
+-- | Does the `Pos` end before the `Position`?
+endsBefore :: Pos -> Position -> Bool
+endsBefore pos Position {..} =
+  case pos of
+    Range _ _ _ refEndLine refEndCol ->
+      case refEndLine `compare` line of
+        LT -> True
+        EQ -> refEndCol <= character
+        GT -> False
+    _ -> False
