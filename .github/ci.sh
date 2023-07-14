1af0efa1904247f4bf45f80c5f80123638df369b
Brett Boston <boston@galois.com>
2023-07-14 16:35:00 -0700

    Compute coverage from HPC files

--- .github/ci.sh
+++ .github/ci.sh
@@ -154,6 +154,24 @@ files_since() {
   echo "$files"
 }
 
+compute_coverage() {
+  # Combine .tix files
+  local SUM_TIX="all.tix"
+  hpc sum --output=$SUM_TIX --union --exclude=Main --exclude=GitRev (find . -name "*.tix")
+  # Generate report
+  local HPC_ROOT=$(find dist-newstyle -name "hpc")
+  # TODO: Remove debug echos"
+  echo "HPC_ROOT: ${HPC_ROOT}"
+  local HPC_ARGS=""
+  for dir in ${HPC_ROOT}/vanilla/mix/*; do
+    echo "dir: ${dir}"
+    local HPC_ARGS="${HPC_ARGS} --hpcdir=${dir}"
+  done
+  echo "HPC_ARGS: ${HPC_ARGS}"
+  hpc report ${HPC_ARGS} ${SUM_TIX}
+  # TODO: Generate HTML
+}
+
 COMMAND="$1"
 shift
 
