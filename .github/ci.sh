f589a5502a18a1e8aab4995dc659cbc30a4e4975
Brett Boston <boston@galois.com>
2023-09-11 10:19:54 -0700

    Package up supporting test files

--- .github/ci.sh
+++ .github/ci.sh
@@ -82,7 +82,7 @@ collect_hpc_files() {
   local MIX_FILES=$(find dist-newstyle -name "*.mix")
   local GENERATED_HS_FILES=$(find dist-newstyle/build -name "*.hs")
   local BINS="dist/bin .github compute-coverage.sh"
-  local TESTS="intTests"
+  local TESTS="intTests doc examples deps/cryptol-specs"
   tar cvf hpc.tar.gz ${MIX_FILES} ${GENERATED_HS_FILES} ${BINS} ${TESTS}
 }
 
