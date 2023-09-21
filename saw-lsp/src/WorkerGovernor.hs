860d217b9086d43600fbfc788fad41049c3d08b5
Sam Cowger <sam@galois.com>
2023-09-21 13:39:44 -0700

    Make WorkerGovernor stateful

--- saw-lsp/src/WorkerGovernor.hs
+++ saw-lsp/src/WorkerGovernor.hs
@@ -1,32 +1,98 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module WorkerGovernor where
 
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Monad (forever, void)
-import Responder.Result (Result (..))
+import Control.Monad.IO.Class
+import Control.Monad.State (MonadState, StateT, evalStateT, gets, modify)
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Responder.Result (Result (..), ThreadHandle, threadHandle)
 
 data Action
   = Spawn
   | Kill ThreadId
 
+data WorkerGovernorState = WorkerGovernorState
+  { wgInput :: TChan Action,
+    wgFresh :: Int,
+    wgThreads :: Map ThreadHandle ThreadId,
+    wgOutput :: TChan Result
+  }
+
+mkWorkerGovernorState :: TChan Action -> TChan Result -> WorkerGovernorState
+mkWorkerGovernorState aChan rChan =
+  WorkerGovernorState
+    { wgInput = aChan,
+      wgFresh = 0,
+      wgThreads = mempty,
+      wgOutput = rChan
+    }
+
+newtype WorkerGovernor a = WorkerGovernor
+  { unWorkerGovernor :: StateT WorkerGovernorState IO a
+  }
+  deriving
+    ( Applicative,
+      Functor,
+      Monad,
+      MonadIO,
+      MonadState WorkerGovernorState
+    )
+
+runWorkerGovernor :: WorkerGovernor a -> WorkerGovernorState -> IO a
+runWorkerGovernor (WorkerGovernor action) = evalStateT action
+
+readAction :: WorkerGovernor Action
+readAction =
+  do
+    input <- gets wgInput
+    liftIO (atomically (readTChan input))
+
+writeResult :: Result -> WorkerGovernor ()
+writeResult res =
+  do
+    output <- gets wgOutput
+    liftIO (atomically (writeTChan output res))
+
+registerThread :: ThreadId -> WorkerGovernor ThreadHandle
+registerThread tid =
+  do
+    handle <- gets (threadHandle . wgFresh)
+    modify
+      \WorkerGovernorState {..} ->
+        WorkerGovernorState
+          { wgFresh = succ wgFresh,
+            wgThreads = Map.insert handle tid wgThreads,
+            ..
+          }
+    pure handle
+
 launchWorkerGovernor :: TChan Action -> TChan Result -> IO ()
 launchWorkerGovernor actionChannel resultChannel =
-  void (forkIO (forever (workerGovernor actionChannel resultChannel)))
+  let st = mkWorkerGovernorState actionChannel resultChannel
+   in void (forkIO (runWorkerGovernor (forever workerGovernor) st))
 
-workerGovernor :: TChan Action -> TChan Result -> IO ()
-workerGovernor actionChannel resultChannel =
+workerGovernor :: WorkerGovernor ()
+workerGovernor =
   do
-    action <- atomically (readTChan actionChannel)
+    action <- readAction
     case action of
       Spawn -> spawn
       Kill tid -> kill tid
-  where
-    alertResponder = atomically . writeTChan resultChannel
-    spawn =
-      do
-        tid <- forkIO (forever (pure ()))
-        alertResponder (Pending tid)
-    kill tid =
-      do
-        killThread tid
-        alertResponder Success
+
+spawn :: WorkerGovernor ()
+spawn =
+  do
+    tID <- liftIO (forkIO (forever (pure ())))
+    tHandle <- registerThread tID
+    writeResult (Pending tHandle)
+
+kill :: ThreadId -> WorkerGovernor ()
+kill tid =
+  do
+    liftIO (killThread tid)
+    writeResult Success
