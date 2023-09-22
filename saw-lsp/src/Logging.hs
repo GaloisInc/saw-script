9febd8512e5785b9d2d1ed6c8887d59a11e65f1a
Sam Cowger <sam@galois.com>
2023-09-22 16:49:11 -0700

    Reformat log messages

--- saw-lsp/src/Logging.hs
+++ saw-lsp/src/Logging.hs
@@ -2,6 +2,8 @@ module Logging where
 
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import System.Log (Priority (..))
+import System.Log.Formatter (simpleLogFormatter)
+import System.Log.Handler (LogHandler (setFormatter))
 import System.Log.Handler.Simple (fileHandler)
 import System.Log.Logger (addHandler, debugM, infoM, setLevel, updateGlobalLogger, warningM)
 
@@ -9,10 +11,11 @@ initializeLogging :: String -> FilePath -> IO ()
 initializeLogging logName logFile =
   do
     updateGlobalLogger logName (setLevel logLevel)
-    h <- fileHandler logFile logLevel
+    h <- formatted <$> fileHandler logFile logLevel
     updateGlobalLogger logName (addHandler h)
   where
     logLevel = DEBUG
+    formatted h = setFormatter h (simpleLogFormatter "[$time : $prio] $msg")
 
 info :: MonadIO m => String -> String -> m ()
 info logName msg = liftIO (infoM logName msg)
