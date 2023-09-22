d3d8f876a34e03f1461de72d94f1508d697c920f
Sam Cowger <sam@galois.com>
2023-09-22 15:43:26 -0700

    Make interpretToPoint request send the appropriate message

--- saw-lsp/src/Handlers/Custom/InterpretToPoint.hs
+++ saw-lsp/src/Handlers/Custom/InterpretToPoint.hs
@@ -6,14 +6,12 @@ module Handlers.Custom.InterpretToPoint where
 
 import Control.Concurrent.STM (atomically, writeTChan)
 import Control.Lens ((^.))
-import Control.Monad.Catch (throwM)
 import Control.Monad.Cont (MonadIO (liftIO))
 import Control.Monad.Reader (ask)
 import Data.Aeson ((.:))
 import Data.Aeson qualified as Aeson
 import Data.Aeson.Types qualified as Aeson
 import Data.Bifunctor (first)
-import Data.List.NonEmpty (NonEmpty ((:|)))
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.Text qualified as Text
@@ -33,6 +31,7 @@ import Language.LSP.Types
   )
 import Language.LSP.Types.Lens qualified as LSP
 import Language.LSP.VFS (virtualFileText)
+import Message (Action (..))
 import SAWScript.AST (Stmt (..))
 import SAWScript.Lexer (lexSAW)
 import SAWScript.Parser (parseModule)
@@ -40,7 +39,6 @@ import Server.Error (internalError)
 import Server.Monad
 import System.IO.Temp (writeSystemTempFile)
 import Text.Printf (printf)
-import Message (Action (..))
 
 handleInterpretToPoint :: Handlers ServerM
 handleInterpretToPoint = requestHandler (SCustomMethod "$/interpretToPoint") doInterp
@@ -51,24 +49,14 @@ doInterp ::
   ServerM ()
 doInterp request responder =
   do
-    warning "Interpreting script"
+    debug "Interpreting script"
 
     ServerEnv {..} <- ask
+    interpParams <- liftEither (fromParams (request ^. LSP.params))
+    fileStmts <- resolve (uri interpParams)
+    truncatedStmts <- truncateStmts (posn interpParams) fileStmts
 
-    liftIO (atomically (writeTChan seWorkerGovernorChannel Spawn))
-
-    -- undefined
-
--- interpParams <- liftEither (fromParams (request ^. LSP.params))
-
--- fileStmts <- resolve (uri interpParams)
--- truncatedStmts <- truncateStmts (posn interpParams) fileStmts
-
--- (matchedPrefix, _val, outM) <- undefined -- liftSAW (interpretSAWScript True truncatedStmts)
--- -- inform' $ printf "Reusing prior execution of %i statements" matchedPrefix
-
--- let goal = fromMaybe "<no goal>" outM
--- displayGoal goal
+    liftIO (atomically (writeTChan seWorkerGovernorChannel (Interpret truncatedStmts)))
 
 resolve :: Uri -> ServerM [Stmt]
 resolve uri =
@@ -79,7 +67,7 @@ resolve uri =
         filePath = fromMaybe "<no path>" (uriToFilePath uri)
     liftEither (first (internalError . Text.pack) (parseSAWFile filePath fileText))
 
-truncateStmts :: Position -> [Stmt] -> ServerM (NonEmpty Stmt)
+truncateStmts :: Position -> [Stmt] -> ServerM [Stmt]
 truncateStmts position fileStmts =
   do
     uuid <- liftIO UUID.nextRandom
@@ -89,12 +77,7 @@ truncateStmts position fileStmts =
         "Truncating %i statements to %i statements"
         (length fileStmts)
         (length truncatedStmts)
-    case truncatedStmts of
-      [] ->
-        let msg = "Cannot interpret empty script"
-         in warnText msg >> throwM (internalError msg)
-      (stmt : stmts) ->
-        pure (stmt :| stmts)
+    pure truncatedStmts
 
 displayGoal :: String -> ServerM ()
 displayGoal goal =
