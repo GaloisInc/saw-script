29f9c9e5849cd48c210fdeb050aa297f6055e434
Ryan <mccleeary@galois.com>
2023-09-28 10:46:01 -0600

    Change check in unit test for type check error.

--- intTests/test_llvm_bound_check/test.sh
+++ intTests/test_llvm_bound_check/test.sh
@@ -3,8 +3,8 @@
 set -e
 
 # We want to ensure there isn't a memory store failure and this is caught during type checking of Setup Value
-if $SAW bound.saw | grep -q "Memory store failed"; then
-    exit 1
-else
+if $SAW bound.saw | grep -q "array type index out of bounds"; then
     exit 0
+else
+    exit 1
 fi
