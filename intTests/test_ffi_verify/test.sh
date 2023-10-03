320bc33de85faea6904142c62c71074dd00e0a76
Bretton <qsctr@users.noreply.github.com>
2023-10-03 10:21:34 -0700

    llvm_ffi_setup: Check in .bc files for tests

--- intTests/test_ffi_verify/test.sh
+++ intTests/test_ffi_verify/test.sh
@@ -1,5 +1,3 @@
 set -e
 
-clang -Wall -Werror -g -c -emit-llvm -o test.bc test.c
-
 $SAW test.saw
--- intTests/test_ffi_verify_salsa/test.sh
+++ intTests/test_ffi_verify_salsa/test.sh
@@ -1,5 +1,3 @@
 set -e
 
-clang -Wall -Werror -g -c -emit-llvm -o salsa.bc salsa.c
-
 CRYPTOLPATH=../../deps/cryptol-specs $SAW salsa.saw
