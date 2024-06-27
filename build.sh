1adca58e09f0b2a85b64182b3753cffa41dd3bad
David Holland <dholland@galois.com>
2024-06-27 14:00:14 -0400

    Build crux-mir-comp by default in build.sh.

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@ function install() {
   cp $(find dist-newstyle -type f -name $1 | sort -g | tail -1) bin/
 }
 
-cabal build exe:cryptol exe:saw exe:saw-remote-api
+cabal build exe:cryptol exe:saw exe:saw-remote-api exe:crux-mir-comp
 
 rm -rf bin && mkdir bin
 install cryptol
 install saw
 install saw-remote-api
+install crux-mir-comp
 
 echo
 echo "COPIED EXECUTABLES TO `pwd`/bin."
