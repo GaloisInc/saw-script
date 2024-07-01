aa0ed80e2942be40e18121181fb6c0b852b87ca2
David Holland <dholland@galois.com>
2024-07-01 10:51:15 -0400

    Build extcore-info and verif-viewer as well.

--- build.sh
+++ build.sh
@@ -5,13 +5,16 @@ function install() {
   cp $(find dist-newstyle -type f -name $1 | sort -g | tail -1) bin/
 }
 
-cabal build exe:cryptol exe:saw exe:saw-remote-api exe:crux-mir-comp
+cabal build exe:cryptol exe:saw exe:saw-remote-api \
+            exe:crux-mir-comp exe:extcore-info exe:verif-viewer
 
 rm -rf bin && mkdir bin
 install cryptol
 install saw
 install saw-remote-api
 install crux-mir-comp
+install extcore-info
+install verif-viewer
 
 echo
 echo "COPIED EXECUTABLES TO `pwd`/bin."
