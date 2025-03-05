GaloisInc/saw-script

commit 19ba85a8b6e0c853c7e79045a912099df0da1038

interpreter: Move the typechecker call out of processStmtBind.
Now it's in interpretStmt and that's the first step to having only
one typechecker call in interpretStmt. Which is in turn the first
step on typechecking _before_ we start interpreting.

src/SAWScript/Interpreter.hs
@@ -323,27 +323,16 @@ stmtInterpreter ro rw stmts =
 processStmtBind ::
   InteractiveMonad m =>
   Bool ->
-  SS.Pos ->
   SS.Pattern ->
   SS.Expr ->
   m ()
-processStmtBind printBinds pos pat expr = do -- mx mt
-  ctx <- getMonadContext
+processStmtBind printBinds pat expr = do -- mx mt
   rw <- liftTopLevel getMergedEnv
 
-  let stmt = SS.StmtBind pos pat expr
-  stmt'' <- liftTopLevel $
-    either failTypecheck return $ checkStmt (rwValueTypes rw) (rwNamedTypes rw) pos ctx stmt
-  let ~(SS.StmtBind _ pat'' expr'') = stmt''
+  val <- liftTopLevel $ interpret expr
 
-  val <- liftTopLevel $ interpret expr''
-
-  -- While checkStmt could return the type of the _statement_, that'll
-  -- be unit. (Actually for the time being it is not, but that's a
-  -- side consequence of the way return values of do-blocks are
-  -- currently handled and we shouldn't rely on it.) We want the type
-  -- of the _expression_. Extract it from the updated pattern, since
-  -- the typechecker will have filled it in there.
+  -- Fetch the type from updated pattern, since the typechecker will
+  -- have filled it in there.
   --
   -- Note that this type won't include the current monad type, because
   -- it's the type of the value that the pattern on the left of <- is
@@ -356,7 +345,7 @@ processStmtBind printBinds pos pat expr = do -- mx mt
               SS.PTuple tuplepos pats ->
                   SS.TyCon tuplepos (SS.TupleCon (genericLength pats)) (map extract pats)
         in
-        extract pat''
+        extract pat
 
   -- Reject polymorphic values. XXX: as noted above this should either
   -- be inside the typechecker or restricted to the repl.
@@ -437,8 +426,14 @@ interpretStmt printBinds stmt =
 
   case stmt of
 
-    SS.StmtBind pos pat expr ->
-      withTopLevel (withPosition pos) (processStmtBind printBinds pos pat expr)
+    SS.StmtBind pos _pat _expr -> do
+      ctx <- getMonadContext
+      stmt' <- liftTopLevel $ do
+        rw <- getTopLevelRW
+        either failTypecheck return $ checkStmt (rwValueTypes rw) (rwNamedTypes rw) pos ctx stmt
+      let ~(SS.StmtBind _ pat' expr') = stmt'
+
+      withTopLevel (withPosition pos) (processStmtBind printBinds pat' expr')
 
     SS.StmtLet _ dg           ->
       liftTopLevel $

commit 3cfe898e9d6284f6b994ca75324b087d594436fb

Interpreter: getTopLevelRW -> getMergedEnv in interpretStmt
This fixes a problem where syntactic top-level bind statements don't
see local definitions within the current block.

How can a syntactic top-level bind statement also be within a block?
When the block calls proof_subshell to open an interactive proof repl;
that is its own syntactic top level.

Fixes #2242, a regression introduced in commit 19ba85a.

This also fixes an analogous misbehavior with local typedefs, except
that that _isn't_ a regression; it's ~always been wrong. The change
also extends to import and let {{ }} statements someone types into an
interactive proof subshell, and it's probably possible to concoct
broken examples of those that this change fixes but I haven't tried.

Note: Ryan Scott did all the real work tracking down what I broke.

src/SAWScript/Interpreter.hs
@@ -441,7 +441,7 @@ interpretStmt printBinds stmt = do
   let ?fileReader = BS.readFile
 
   ctx <- getMonadContext
-  rw <- liftTopLevel $ getTopLevelRW
+  rw <- liftTopLevel getMergedEnv
   stmt' <- processTypeCheck $ checkStmt (rwValueTypes rw) (rwNamedTypes rw) ctx stmt
 
   case stmt' of