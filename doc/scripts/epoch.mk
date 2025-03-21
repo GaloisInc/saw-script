33e5bd18651316aa0da61566ae1fee108b97f399
David Holland <dholland@galois.com>
2025-03-20 22:09:05 -0400

    Set SOURCE_DATE_EPOCH for the release.
    It was April 1, which is still in the future, so set it to April 2 so
    it doesn't go backwards.

--- doc/scripts/epoch.mk
+++ doc/scripts/epoch.mk
@@ -1,3 +1,3 @@
-# BSD/Linux: date +%s -d "04/01/2025 00:00:00 GMT"
-# OSX: date -j -f "%m/%d/%Y %H:%M:%S %Z" "4/1/2025 00:00:00 GMT" +%s
-SOURCE_DATE_EPOCH=1743465600
+# BSD/Linux: date +%s -d "04/02/2025 00:00:00 GMT"
+# OSX: date -j -f "%m/%d/%Y %H:%M:%S %Z" "4/2/2025 00:00:00 GMT" +%s
+SOURCE_DATE_EPOCH=1743552000
