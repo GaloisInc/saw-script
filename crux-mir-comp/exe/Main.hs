c5244aa8e2ab4427a4d9fefb6e4f409b54dfd14b
Iavor S. Diatchki <diatchki@galois.com>
2025-07-18 09:24:06 -0700

    Updates to match changes in `crucible`

--- crux-mir-comp/exe/Main.hs
+++ crux-mir-comp/exe/Main.hs
@@ -12,5 +12,6 @@ import Mir.Cryptol (cryptolOverrides)
 main :: IO ()
 main = do
     setLocaleEncoding utf8
-    Mir.mainWithExtraOverrides newMirState $
+    let initUser = Mir.InitUserState newMirState
+    Mir.mainWithExtraOverrides initUser $
       compositionalOverrides `Mir.orOverride` cryptolOverrides
