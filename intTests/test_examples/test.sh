de75dafd6aca5741b465b5327e0a2cd3b42fc5ac
Marcella Hastings <marcella@galois.com>
2025-07-25 12:51:05 -0400

    Set CRYPTOLPATH for examples test #2499

--- intTests/test_examples/test.sh
+++ intTests/test_examples/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-
+export CRYPTOLPATH="../../deps/cryptol-specs"
 (cd ../../examples &&
 for f in `find aes fresh-post ghost java llvm multi-override partial-spec -name "*.saw"` ; do
     (cd `dirname $f` && $SAW `basename $f`)
