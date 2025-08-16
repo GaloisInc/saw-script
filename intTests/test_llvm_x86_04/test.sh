733485c7f7d0cd9d917bb562722278851b813bc8
David Holland <dholland@galois.com>
2025-08-15 21:10:10 -0400

    tests: Check in LLVM bitcode files for test_llvm_x86_*.
    Add makefiles in test_llvm_x86_* to regen them (and the other bits)
    per current practices.
    
    This moves the artefact build bits out of test.sh. Once upon a time
    they were all built on the fly. That then didn't work on non-x86
    targets, at which point some of the artefacts (but not all) got
    checked in and their build actions commented out in test.sh.
    
    However, the bitcode files were still being generated on the fly.
    This used whatever clang you happened to have, for whatever target it
    happened to default to, and was thus only working on non-x86 targets
    by accident.
    
    The provenance for these bitcode files has not been forgotten and will
    be added in an upcoming commit.

--- intTests/test_llvm_x86_04/test.sh
+++ intTests/test_llvm_x86_04/test.sh
@@ -1,6 +1,3 @@
 set -e
 
-#yasm -felf64 addvar.S
-#ld addvar.o -o addvar
-clang -c -emit-llvm test.c
 $SAW test.saw
--- intTests/test_llvm_x86_05/test.sh
+++ intTests/test_llvm_x86_05/test.sh
@@ -1,6 +1,3 @@
 set -e
 
-#yasm -felf64 returntest.S
-#ld returntest.o -o returntest
-clang -c -emit-llvm test.c
 $SAW test.saw
