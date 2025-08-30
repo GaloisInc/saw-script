1f998127218782a8d227e3789fe875a5a3ad4b1a
Mark Tullsen <tullsen@galois.com>
2025-08-30 15:39:24 -0700

    revamping submodule tests.  test_saw_submodule_access1 should pass.

--- intTests/test_saw_submodule_access/test.sh
+++
@@ -1,6 +0,0 @@
-# set -e
-
-$SAW testD1.saw
-$SAW testD2.saw
-$SAW testD3.saw
-$SAW testD4.saw
---
+++ intTests/test_saw_submodule_access1/test.sh
@@ -0,0 +1,5 @@
+set -e
+  # all must pass.
+$SAW test_import_errors.saw
+$SAW test_import_D.saw
+# $SAW test_load_D.saw    # currently not working!
