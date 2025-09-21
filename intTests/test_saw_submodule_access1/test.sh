2f1be105cda7073ab0e3fb2021a85c82777f162d
Mark Tullsen <tullsen@galois.com>
2025-09-20 20:49:52 -0700

    generalize the internals of "CryptolModule" (as seen on command-line) and updating code to reflect this
    - CryptolModule (as known at the command line) is generalized to ExtCryptolModule,
      - this allows for a module to be represented as EITHER
        - CryptolModule (2 namespaces), or
        - the name of a loaded module
    
    - This allows for simpler and newly shared code
    - This now should support submodules when we `cryptol_load`.

--- intTests/test_saw_submodule_access1/test.sh
+++ intTests/test_saw_submodule_access1/test.sh
@@ -5,10 +5,6 @@ $SAW test_import_errors.saw
 
 $SAW test_import_D.saw
 
-! $SAW test_load_D.saw
-
-  # finishing https://github.com/GaloisInc/saw-script/pull/2593
-  #  should allow test_load_D.saw to succeed. (as it should)
-  # TODO: remove the ! above when that PR is done.
+$SAW test_load_D.saw
 
 $SAW test_UseFunctors.saw
