2780ebc3ba97e93c28de9b47803805c2c3ebf324
David Holland <dholland@galois.com>
2025-10-23 18:28:39 -0400

    Trie: tidy, simplify, improve naming, add comments.
    NFCI

--- saw-script/src/SAWScript/REPL/Trie.hs
+++ saw-script/src/SAWScript/REPL/Trie.hs
@@ -23,52 +23,70 @@ import qualified Data.Text as Text
 import           Data.Text (Text)
 import qualified Data.Map as Map
 import           Data.Map (Map)
-import           Data.Maybe (fromMaybe,maybeToList)
+import           Data.Maybe (maybeToList)
 
 import           SAWScript.Panic (panic)
 
 
--- | Maps string names to values, allowing for partial key matches and querying.
+-- | Maps string names to values, allowing for partial key matches and
+--   querying.
+--
+--   Each node contains a map from characters to subnodes, and
+--   possibly also a value corresponding to the characters seen so
+--   far.
 data Trie a = Node (Map Char (Trie a)) (Maybe a)
     deriving (Show)
 
+-- | The empty `Trie`.
 empty :: Trie a
 empty  = Node Map.empty Nothing
 
--- | Insert a value into the Trie.  Will call `panic` if a value already exists
--- with that key.
+-- | Insert a value into a `Trie`. Will panic if a value already exists
+--   with the same key.
 insert :: Text -> a -> Trie a -> Trie a
-insert k a = loop k
+insert k v node0 = insert' k node0
   where
-  loop key (Node m mb) = case Text.uncons key of
-    Just (c, key') -> Node (Map.alter (Just . loop key' . fromMaybe empty) c m) mb
-    Nothing -> case mb of
-      Nothing -> Node m (Just a)
-      Just _  -> panic "insert" ["Key already exists: " <> k]
+  -- the insertion value is invariant so recurse without it
+  insert' key (Node tbl mbValue) = case Text.uncons key of
+      Nothing -> case mbValue of
+          Nothing ->
+              -- Reached the end of the key and there's nothing here yet;
+              -- insert the value.
+              Node tbl (Just v)
+              -- There's already something here; croak
+          Just _  ->
+              panic "insert" ["Key already exists: " <> k]
+      Just (c, key') ->
+          let subnode = case Map.lookup c tbl of
+                Nothing -> empty
+                Just n -> n
+              subnode' = insert' key' subnode
+          in
+          Node (Map.insert c subnode' tbl) mbValue
 
--- | Return all matches with the given prefix.
+-- | Return all values in the `Trie` whose keys match the given
+--   prefix.
 lookup :: Text -> Trie a -> [a]
-lookup key t@(Node mp _) = case Text.uncons key of
+lookup key node@(Node tbl _) = case Text.uncons key of
+    Nothing ->
+        leaves node
+    Just (c, key') -> case Map.lookup c tbl of
+        Nothing -> []
+        Just node' -> lookup key' node'
 
-  Just (c, key') -> case Map.lookup c mp of
-    Just m' -> lookup key' m'
-    Nothing -> []
-
-  Nothing -> leaves t
-
--- | Return all matches with the given prefix. However, if an exact match
--- exists, return just that match.
+-- | Return all values in the `Trie` whose keys match the given
+--   prefix. However, if an exact match to the key exists, return just
+--   that match.
 lookupWithExact :: Text -> Trie a -> [a]
-lookupWithExact key t@(Node mp mb) = case Text.uncons key of
-
-  Just (c, key') -> case Map.lookup c mp of
-    Just m' -> lookupWithExact key' m'
-    Nothing -> []
-
-  Nothing -> case mb of
-    Just b -> [b]
-    Nothing -> leaves t
+lookupWithExact key node@(Node tbl mbValue) = case Text.uncons key of
+    Nothing -> case mbValue of
+        Nothing -> leaves node
+        Just val -> [val]
+    Just (c, key') -> case Map.lookup c tbl of
+        Nothing -> []
+        Just node' -> lookupWithExact key' node'
 
 -- | Return all of the values from a Trie.
 leaves :: Trie a -> [a]
-leaves (Node mp mb) = maybeToList mb ++ concatMap leaves (Map.elems mp)
+leaves (Node tbl mbValue) =
+    maybeToList mbValue ++ concatMap leaves (Map.elems tbl)
