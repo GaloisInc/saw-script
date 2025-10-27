be8d3969a005780e112ad5dc5c90f54e2f75b29d
David Holland <dholland@galois.com>
2025-10-27 19:19:06 -0400

    intTests: Extend test-and-diff.sh to handle providing stdin.
    Switch test2242_2243 to use test-and-diff.sh.
    
    While working on the previous changes I became concerned about whether
    failures would actually be detected; the answer proves to be no.
    Errors that occur in a nested interactive subshell get trapped and
    discarded (because that's what the REPL does with errors) and at least
    present01 and present03 do not fail overall if the actions in the
    nested subshell, which are the point of those tests, fail.
    
    This requires adding some mangy stuff to the output postprocessing so
    that the timestamps in the output actually go away; however, we can
    get rid of all that once we fix #2090.

--- intTests/test2242_2243/test.sh
+++ intTests/test2242_2243/test.sh
@@ -1,10 +1,1 @@
-set -e
-
-$SAW present01.saw < present01.stdin
-$SAW present02.saw < present02.stdin
-
-$SAW leak01.saw < leak01.stdin
-$SAW leak02.saw < leak02.stdin
-$SAW leak03.saw < leak03.stdin
-$SAW leak04.saw < leak04.stdin
-$SAW leak05.saw < leak05.stdin
+exec ${TEST_SHELL:-bash} ../support/test-and-diff.sh "$@"
