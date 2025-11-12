44761758e375a82ad8e12c196b8a13fb1d7fc110
David Holland <dholland@galois.com>
2025-11-12 00:06:26 -0500

    prettyprinting: G/C scPrettyNameInCtx.
    There were two uses, both of which passed an empty context and
    can therefore use scPrettyName instead.

--- saw-core/src/SAWCore/OpenTerm.hs
+++ saw-core/src/SAWCore/OpenTerm.hs
@@ -288,8 +288,7 @@ piArg (OpenTerm m) =
   OpenTerm $ m >>= \case
   (unwrapTermF -> Pi _ tp _) -> pure tp
   t ->
-    fail ("piArg: not a pi type: " ++
-          scPrettyTermInCtx PPS.defaultOpts [] t)
+    fail ("piArg: not a pi type: " ++ scPrettyTerm PPS.defaultOpts t)
 
 -- | Build a lambda abstraction as an 'OpenTerm'
 lambda :: LocalName -> OpenTerm -> (OpenTerm -> OpenTerm) -> OpenTerm
