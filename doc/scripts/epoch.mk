319a6bce189485f2ef87a80ab64795a8fc08dee6
August Schwerdfeger <august.schwerdfeger@galois.com>
2025-11-18 13:55:09 -0600

    Update the text release date in 'epoch.mk'.

--- doc/scripts/epoch.mk
+++ doc/scripts/epoch.mk
@@ -24,6 +24,6 @@
 #    - the devel version can then be bumped to the current day if needed
 #
 
-# BSD/Linux: date +%s -d "08/22/2025 00:00:00 GMT"
-# OSX: date -j -f "%m/%d/%Y %H:%M:%S %Z" "8/22/2025 00:00:00 GMT" +%s
+# BSD/Linux: date +%s -d "11/18/2025 00:00:00 GMT"
+# OSX: date -j -f "%m/%d/%Y %H:%M:%S %Z" "11/18/2025 00:00:00 GMT" +%s
 SOURCE_DATE_EPOCH=1763424000
