212d6ab7a2b59f659e4cdbccebaea5dbd338478e
Brian Huffman <huffman@galois.com>
2025-12-17 15:10:06 -0800

    saw-core: Add haddocks to functions in SAWCore.Cache.

--- saw-core/src/SAWCore/Cache.hs
+++ saw-core/src/SAWCore/Cache.hs
@@ -25,11 +25,19 @@ import qualified Data.Map as Map
 import           Data.Map (Map)
 import           Data.Ref
 
+-- | A @MapCache m k a@ is a mutable reference to a 'Map' with keys of
+-- type @k@ and values of type @a@, for use in monad @m@.
 newtype MapCache m k a = MapCache (T m (Map k a))
 
+-- | Create a new empty 'MapCache'.
 newMapCache :: (C m, Ord k) => m (MapCache m k a)
 newMapCache = MapCache <$> new Map.empty
 
+-- | Memoize a computation of type @k -> m a@ using a 'MapCache'.
+-- If the cache already contains an entry for the given key, return
+-- the value immediately without running the given monadic action.
+-- Otherwise run the monadic action, store the result in the cache,
+-- and also return the newly-computed value.
 useMapCache :: (C m, Ord k) => MapCache m k a -> k -> m a -> m a
 useMapCache (MapCache ref) k action =
   do m <- Data.Ref.read ref
@@ -40,11 +48,21 @@ useMapCache (MapCache ref) k action =
             modify ref (Map.insert k x)
             pure x
 
+-- | An @IntCache m k a@ is a mutable reference to an 'IntMap' with
+-- keys of type 'Int' and values of type @a@, for use in monad @m@.
+-- @IntCache m a@ is a more efficient alternative to @MapCache m Int
+-- a@.
 newtype IntCache m a = IntCache (T m (IntMap a))
 
+-- | Create a new empty 'IntCache'.
 newIntCache :: (C m) => m (IntCache m a)
 newIntCache = IntCache <$> new IntMap.empty
 
+-- | Memoize a computation of type @Int -> m a@ using an 'IntCache'.
+-- If the cache already contains an entry for the given key, return
+-- the value immediately without running the given monadic action.
+-- Otherwise run the monadic action, store the result in the cache,
+-- and also return the newly-computed value.
 useIntCache :: C m => IntCache m a -> Int -> m a -> m a
 useIntCache (IntCache ref) k action =
   do m <- Data.Ref.read ref
