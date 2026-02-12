2b8d0713c9c4209a4b1d2d59df27acc6f89d1aeb
Daniel Matichuk <dmatichuk@galois.com>
2026-02-12 11:52:06 -0800

    WIP: QualName

--- saw-core/src/SAWCore/URI.hs
+++ saw-core/src/SAWCore/URI.hs
@@ -9,18 +9,18 @@ Maintainer  : dmatichuk@galois.com
 Stability   : experimental
 Portability : non-portable (language extensions)
 
-Simple URI implementation with namespaces and paths.
+Qualified names with namespaces,paths and subpaths.
 -}
 
-module SAWCore.URI 
+module SAWCore.QualName 
   ( Namespace(..)
-  , URI
-  , mkURI
-  , parseURI
-  , renderURI
+  , readNamespace
+  , QualName
+  , mkQualName
+  , parse
+  , render
   ) where
 
-import qualified Data.Foldable as Foldable
 import           Data.Hashable
 import qualified Data.List as List
 import           Data.Map (Map)
@@ -56,59 +56,11 @@ readNamespace txt = case Map.lookup txt namespaceMap of
   Just ns -> pure ns
   Nothing -> fail $ "readNamespace: namespace not found: " ++ Text.unpack txt
 
-parsePath :: MonadFail m => Text -> m ([Text], Text)
-parsePath txt
-  | not (Text.null txt)
-  , ps <- map percentDecode $ Text.splitOn "/" txt
-  = case ps of
-      -- allow for a leading '/'
-      (p:ps') | Text.null p -> mkPath ps'
-      _ -> mkPath ps
-parsePath txt =
-  fail $ "parsePath: invalid path: " ++ (Text.unpack txt)
-
-data URI = URI 
-  { uriNamespace :: Namespace
-  , uriBaseName :: Text
-  , uriPath :: [Text]
-  , uriIndex :: Maybe Int
-  }
-  deriving (Eq, Ord)
-
-instance Hashable URI where
-  hashWithSalt s (URI a b c d) = s
-    `hashWithSalt` a 
-    `hashWithSalt` b
-    `hashWithSalt` c
-    `hashWithSalt` d
-
-percentEncode :: Text -> Text
-percentEncode txt = Text.replace "/" "%2F" $ Text.replace "%" "%25" txt
-
-percentDecode :: Text -> Text
-percentDecode txt = Text.replace "%25" "%" $ Text.replace "%2F" "/" txt
-
-mkPath :: (MonadFail m) => [Text] -> m ([Text], Text)
-mkPath ps = case (List.any Text.null ps,ps) of
-  (False, _:_) -> return (List.init ps,List.last ps)
-  _ -> fail $ "mkPath: invalid path: " ++ show ps
-
-mkURI :: (MonadFail m, Foldable t) => Namespace -> t Text -> Maybe Int -> m URI
-mkURI ns ps idx = do
-  (ps', nm) <- mkPath (Foldable.toList ps)
-  return $ URI ns nm ps' idx
-
-nonEmpty :: MonadFail m => String -> Text -> m ()
-nonEmpty msg txt = case Text.null txt of
-  True -> fail msg
-  False -> return ()
-
 splitM :: MonadFail m => Bool -> Char -> Text -> m (Text,Text)
 splitM fwd c txt = do
   nonEmpty err txt
   let txt' = if fwd then txt else Text.reverse txt
   let (lhs,rhs) = Text.break (==c) txt'
-  nonEmpty err lhs
   case Text.uncons rhs of
     Nothing -> fail err
     Just(_,rhs') -> case fwd of
@@ -118,22 +70,93 @@ splitM fwd c txt = do
     err = "splitM: failed to split on char '" ++ [c] ++ "' in '"
       ++ Text.unpack txt
 
-parseURI :: MonadFail m => Text -> m URI
-parseURI txt0 = do
-  (ns, txt1) <- splitM True ':' txt0
-  ns' <- readNamespace ns
-  let (mi, txt3) = case splitM False '#' txt1 of
+splitOnM :: MonadFail m => Text -> Text -> m [Text]
+splitOnM _ txt | Text.null txt = fail "splitOnM: empty argument"
+splitOnM sep txt = return $ Text.splitOn sep txt
+
+unsnocM :: MonadFail m => [a] -> m ([a], a)
+unsnocM [] = fail "unsnocM: empty list"
+unsnocM xs = return $ (List.init xs, List.last xs)
+
+parsePath :: MonadFail m => Text -> m ([Text], [Text], Text)
+parsePath txt0 = do
+  case splitM True '[' txt0 of
+    Just (path_txt, subpath_txt_) -> do
+      path <- splitOnM path_txt
+      (subpath_txt, s) <- splitM False ']' subpath_txt_
+      True <- return $ Text.null s
+      subpath_nm <- splitOnM subpath_txt
+      (subpath,nm) <- unsnocM subpath_nm
+      return (path, subpath, nm)
+    Nothing -> do
+      path_nm <- splitOnM txt0
+      (path,nm) <- unsnocM path_nm
+      return (path, [], nm)
+
+data QualName = QualName
+  { qnNameSpace :: Namespace
+  , qnBaseName :: Text
+  , qnPath :: [Text]
+  , qnSubPath :: [Text]
+  , qnIndex :: Maybe Int
+  }
+  deriving (Eq, Ord)
+
+
+instance Hashable QualName where
+  hashWithSalt s (QualName a b c d e) = s
+    `hashWithSalt` a 
+    `hashWithSalt` b
+    `hashWithSalt` c
+    `hashWithSalt` d
+    `hashWithSalt` e
+
+
+invalidPathElem :: Text -> Bool
+invalidPathElem txt = 
+     Text.null txt 
+  || Text.any (\c -> c == '[' || c == ']') txt
+  || Text.isInfixOf "::" txt
+
+pathToQualName :: (MonadFail m, Foldable t) => Namespace -> t Text -> m QualName
+pathToQualName _ [] = fail "pathToQualName: empty path"
+pathToQualName ns ps = mkQualName ns (List.init ps) [] (List.last ps) (-1)
+
+indexedQualName :: Namespace -> Text -> Int -> m QualName
+indexedQualName ns nm i = mkQualName ns [] [] nm i
+
+mkQualName :: (MonadFail m) => Namespace -> [Text] -> [Text] -> Text -> Maybe Int -> m QualName
+mkQualName ns ps sps nm idx = do
+  mapM_ go (nm : ps ++ sps)
+  return $ QualName ns nm ps sps idx
+  where
+    go txt = case invalidPathElem txt of
+      True -> fail $ "mkQualName: invalid path element: " ++ Text.unpack txt
+      False -> return ()
+
+nonEmpty :: MonadFail m => String -> Text -> m ()
+nonEmpty msg txt = case Text.null txt of
+  True -> fail msg
+  False -> return ()
+
+
+
+parse :: MonadFail m => Text -> m QualName
+parse txt0 = do
+  (txt1, ns_txt) <- splitM False '@' txt0
+  ns <- readNameSpace ns_txt
+  let (i', txt3) = case splitM False '#' txt1 of
         Just (txt2,si)
           | Right (i,s) <- Text.decimal si
           , Text.null s
-         -> (Just i, txt2)
-        _ -> (Nothing, txt1)
-  (ps,nm) <- parsePath txt3
-  return $ URI ns' nm ps mi
+         -> (i, txt2)
+        _ -> (-1, txt1)
+  (path,subpath,nm) <- parsePath txt3
+  mkQualName path subpath nm i'
 
-renderURI :: URI -> Text
-renderURI uri =
-     renderNamespace (uriNamespace uri)
+render :: QualName -> Text
+render uri =
+     renderNameSpace (uriNameSpace uri)
   <> ":" <> pathBody <> suffix
   where
     suffix :: Text
@@ -146,5 +169,5 @@ renderURI uri =
       [] -> percentEncode $ uriBaseName uri
       ps -> "/" <> Text.intercalate "/" (map percentEncode (ps ++ [uriBaseName uri]))
 
-instance Show URI where
+instance Show QualName where
   show uri = Text.unpack (renderURI uri)
