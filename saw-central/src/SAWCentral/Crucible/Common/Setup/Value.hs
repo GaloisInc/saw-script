f5e783a04400711e2278c7f47562c26777ab780d
David Holland <dholland@galois.com>
2026-05-28 15:42:20 -0400

    crucible: Tidy ConditionMetadata type.
    Add haddocks, remove unused Ord instance. NFCI

--- saw-central/src/SAWCentral/Crucible/Common/Setup/Value.hs
+++ saw-central/src/SAWCentral/Crucible/Common/Setup/Value.hs
@@ -211,21 +211,31 @@ deriving instance (SetupValueHas Show ext) => Show (SetupValue ext)
 -- ** Pre- and post-conditions
 
 -- FUTURE: conditionType should be an enumeration, not an arbitrary
--- string.
+-- string. The complication is that some of the things it can range
+-- over are ext-specific (and some aren't) so it would require adding
+-- a bunch of machinery.
 --
 -- (and perhaps conditionContext should be more structured as well;
 -- certainly since it's mostly "", it could stand to be Maybe Text; but
 -- in the couple places where it's something else it could be something
 -- machine-readable instead of a printed description.)
 
-data ConditionMetadata =
-  ConditionMetadata
-  { conditionLoc  :: ProgramLoc
-  , conditionTags :: Set String
-  , conditionType :: String
-  , conditionContext :: String
+-- | Information about a verification condition. This is created as
+--   conditions are asserted/generated and then carried around with
+--   them.
+data ConditionMetadata = ConditionMetadata {
+    -- | The program location where the condition was asserted.
+    conditionLoc  :: ProgramLoc,
+    -- | The goal tags for goals this condition generates.
+    conditionTags :: Set String,
+    -- | An arbitrary descriptive string for this condition.
+    conditionType :: String,
+    -- | The caller when working in an override spec.
+    --   Note: because we don't apply overrides when _in_ overrides,
+    --   this should never need to have more than one thing in it.
+    conditionContext :: String
   }
- deriving (Show, Eq, Ord)
+ deriving (Show, Eq)
 
 --------------------------------------------------------------------------------
 -- *** Method specs
