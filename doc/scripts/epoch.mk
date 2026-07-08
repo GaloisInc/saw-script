9357c15b914b34089f335bc066fc8a8c13cc9a98
Ryan Scott <rscott@galois.com>
2026-07-08 14:02:49 -0400

    manual: bump date

--- doc/scripts/epoch.mk
+++ doc/scripts/epoch.mk
@@ -24,6 +24,6 @@
 #    - the devel version can then be bumped to the current day if needed
 #
 
-# BSD/Linux: date +%s -d "07/03/2026 00:00:00 GMT"
-# OSX: date -j -f "%m/%d/%Y %H:%M:%S %Z" "07/03/2026 00:00:00 GMT" +%s
-SOURCE_DATE_EPOCH=1783036800
+# BSD/Linux: date +%s -d "07/08/2026 00:00:00 GMT"
+# OSX: date -j -f "%m/%d/%Y %H:%M:%S %Z" "07/08/2026 00:00:00 GMT" +%s
+SOURCE_DATE_EPOCH=1783468800
