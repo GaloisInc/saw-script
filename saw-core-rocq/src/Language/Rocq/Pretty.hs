0550306a6c86c41018b457b852ddb04e0faa4b28
David Holland <dholland@galois.com>
2026-08-03 17:58:38 -0400

    Language.Rocq.Pretty: import Prettyprinter as PP.
    This is not necessary here for namespace control, since this file
    really only contains printing logic. But it's the way things are
    everywhere else in SAW and there's some value to that uniformity; also
    it's generally helpful to know at a glance which calls are actually
    prettyprinter operations.
    
    NFCI

--- saw-core-rocq/src/Language/Rocq/Pretty.hs
+++ saw-core-rocq/src/Language/Rocq/Pretty.hs
@@ -12,12 +12,12 @@ Converts Rocq AST to prettyprinter documents.
 
 module Language.Rocq.Pretty (prettyDecl) where
 
-import Prettyprinter
+import qualified Prettyprinter as PP
+import Prettyprinter ((<+>))
 
 import Language.Rocq.AST
 import Data.Word
 import Numeric (showHex)
-import Prelude hiding ((<$>), (<>))
 
 -- | Replace all occurrences of the double quote character @"@ with
 --   the string @""@, i.e., two copies of it, as this is how Rocq
@@ -26,12 +26,12 @@ escapeStringLit :: String -> String
 escapeStringLit = concatMap (\c -> if c == '"' then "\"\"" else [c])
 
 -- | Wrapper for printing arbitrary text
-text :: String -> Doc ann
-text = pretty
+text :: String -> PP.Doc ann
+text s = PP.pretty s
 
 -- | Wrapper for printing integers
-integer :: Integer -> Doc ann
-integer = pretty
+integer :: Integer -> PP.Doc ann
+integer n = PP.pretty n
 
 -- FUTURE: Move these to SAWSupport.Pretty
 
@@ -48,67 +48,67 @@ integer = pretty
 --   empty docs, which seems against the concept of prettyprinting
 --   layout anyway, there can be another way to do that, like the
 --   latex "phantom" object.
-hsep' :: [Doc ann] -> Doc ann
+hsep' :: [PP.Doc ann] -> PP.Doc ann
 hsep' docs = case docs of
-    [] -> emptyDoc
-    _ : _ -> emptyDoc <+> hsep docs
+    [] -> PP.emptyDoc
+    _ : _ -> PP.emptyDoc <+> PP.hsep docs
 
 
 -- | Print a list separated by @sepr@.
 --   Glues the separator to the end of each element.
-tightSepList :: Doc ann -> [Doc ann] -> Doc ann
+tightSepList :: PP.Doc ann -> [PP.Doc ann] -> PP.Doc ann
 tightSepList _ [] = mempty
 tightSepList _ [d] = d
 tightSepList sepr (d:ds) = d <> sepr <+> tightSepList sepr ds
 
 -- | Print an `Ident`
-prettyIdent :: Ident -> Doc ann
+prettyIdent :: Ident -> PP.Doc ann
 prettyIdent (Ident s) = text s
 
 -- | Common code to print a name with a type
-prettyNameType :: Ident -> Type -> Doc ann
-prettyNameType x ty = prettyIdent x <+> colon <+> prettyTerm PrecNone ty
+prettyNameType :: Ident -> Type -> PP.Doc ann
+prettyNameType x ty = prettyIdent x <+> ":" <+> prettyTerm PrecNone ty
 
 -- | Print an ordinary (lambda/let) binder
-prettyBinder :: Binder -> Doc ann
+prettyBinder :: Binder -> PP.Doc ann
 prettyBinder b = case b of
     Binder Explicit x Nothing   -> prettyIdent x
-    Binder Explicit x (Just ty) -> parens $ prettyNameType x ty
-    Binder Implicit x Nothing    -> braces $ prettyIdent x
-    Binder Implicit x (Just ty)  -> braces $ prettyNameType x ty
+    Binder Explicit x (Just ty) -> PP.parens $ prettyNameType x ty
+    Binder Implicit x Nothing    -> PP.braces $ prettyIdent x
+    Binder Implicit x (Just ty)  -> PP.braces $ prettyNameType x ty
 
 -- | Print a pi (forall) binder
 --   (we don't seem to be able to represent @exists@)
-prettyPiBinder :: PiBinder -> Doc ann
+prettyPiBinder :: PiBinder -> PP.Doc ann
 prettyPiBinder b = case b of
     PiBinder Explicit Nothing ty ->
         prettyTerm PrecApp ty <+> "->"
     PiBinder Explicit (Just x) ty ->
-        "forall" <+> parens (prettyNameType x ty) <> comma
+        "forall" <+> PP.parens (prettyNameType x ty) <> ","
     PiBinder Implicit Nothing ty ->
-        braces (prettyTerm PrecApp ty) <+> "->"
+        PP.braces (prettyTerm PrecApp ty) <+> "->"
     PiBinder Implicit (Just x) ty ->
-        "forall" <+> braces (prettyNameType x ty) <> comma
+        "forall" <+> PP.braces (prettyNameType x ty) <> ","
 
 -- | Print a list of binders
-prettyBinders :: [Binder] -> Doc ann
-prettyBinders bs = hsep $ map prettyBinder bs
+prettyBinders :: [Binder] -> PP.Doc ann
+prettyBinders bs = PP.hsep $ map prettyBinder bs
 
 -- | Print an optional type annotation
-prettyMaybeTy :: Maybe Type -> Doc ann
-prettyMaybeTy Nothing = mempty
-prettyMaybeTy (Just ty) = colon <+> prettyTerm PrecNone ty
+prettyMaybeTy :: Maybe Type -> PP.Doc ann
+prettyMaybeTy Nothing = PP.emptyDoc
+prettyMaybeTy (Just ty) = ":" <+> prettyTerm PrecNone ty
 
 -- | Print a `sort`
-prettySort :: Sort -> Doc ann
+prettySort :: Sort -> PP.Doc ann
 prettySort s = case s of
     Prop -> "Prop"
     Set -> "Set"
     Type -> "Type"
 
 -- | Print a list of pi-binders
-prettyPiBinders :: [PiBinder] -> Doc ann
-prettyPiBinders bs = hsep $ map prettyPiBinder bs
+prettyPiBinders :: [PiBinder] -> PP.Doc ann
+prettyPiBinders bs = PP.hsep $ map prettyPiBinder bs
 
 -- | Type to hold the current expression precedence while printing
 data Prec
@@ -119,11 +119,11 @@ data Prec
   deriving (Eq, Ord)
 
 -- | Insert parens based on the first argument
-parensIf :: Bool -> Doc ann -> Doc ann
-parensIf p d = if p then parens d else d
+parensIf :: Bool -> PP.Doc ann -> PP.Doc ann
+parensIf p d = if p then PP.parens d else d
 
 -- | Print a term
-prettyTerm :: Prec -> Term -> Doc ann
+prettyTerm :: Prec -> Term -> PP.Doc ann
 prettyTerm p e =
   case e of
     Lambda bs t ->
@@ -151,7 +151,7 @@ prettyTerm p e =
           t' = prettyTerm PrecNone t
           body' = prettyTerm PrecLambda body
       in
-      parensIf (p > PrecLambda) $ fillSep [
+      parensIf (p > PrecLambda) $ PP.fillSep [
           "let" <+> x' <+> bs' <+> mty' <+> ":=" <+> t' <+> "in",
           body'
       ]
@@ -168,7 +168,7 @@ prettyTerm p e =
       let f' = prettyTerm PrecApp f
           args' = map (prettyTerm PrecAtom) args
       in
-      parensIf (p > PrecApp) $ hsep (f' : args')
+      parensIf (p > PrecApp) $ PP.hsep (f' : args')
     Sort s ->
       prettySort s
     Var x ->
@@ -201,71 +201,71 @@ prettyTerm p e =
           text (show i ++ "%Z")
     List ts ->
       let ts' = map (prettyTerm PrecNone) ts in
-      brackets $ tightSepList ";" ts'
+      PP.brackets $ tightSepList ";" ts'
     StringLit s ->
-      dquotes (text $ escapeStringLit s)
+      PP.dquotes (text $ escapeStringLit s)
     Scope term scope ->
       let term' = prettyTerm PrecAtom term
           scope' = text scope
       in
       term' <> "%" <> scope'
     Ltac s ->
-      "ltac:" <> parens (text s)
+      "ltac:" <> PP.parens (text s)
 
 -- | Common code for the simple declarations
-prettyBasicDecl :: Doc ann -> Ident -> Type -> Doc ann
+prettyBasicDecl :: PP.Doc ann -> Ident -> Type -> PP.Doc ann
 prettyBasicDecl what nm ty =
   let nm' = prettyIdent nm
       ty' = prettyTerm PrecNone ty
   in
-  nest 2 (what <+> nm' <+> ":" <+> ty' <+> ".") <> hardline
+  PP.nest 2 (what <+> nm' <+> ":" <+> ty' <+> ".") <> PP.hardline
 
 -- | Print a top-level declaration
-prettyDecl :: Decl -> Doc ann
+prettyDecl :: Decl -> PP.Doc ann
 prettyDecl decl = case decl of
   Axiom nm ty -> prettyBasicDecl "Axiom" nm ty
   Parameter nm ty -> prettyBasicDecl "Parameter" nm ty
   Variable nm ty -> prettyBasicDecl "Variable" nm ty
   Comment s ->
-    "(*" <+> text s <+> "*)" <> hardline
+    "(*" <+> text s <+> "*)" <> PP.hardline
   Definition nm bs mty body ->
     let nm' = prettyIdent nm
         bs' = hsep' $ map prettyBinder bs
         mty' = prettyMaybeTy mty
         body' = prettyTerm PrecNone body
     in
-    nest 2 (
-      vsep [
+    PP.nest 2 (
+      PP.vsep [
           "Definition" <+> nm' <> bs' <+> mty' <+> ":=",
           body' <> "."
       ]
-    ) <> hardline
+    ) <> PP.hardline
   InductiveDecl ind ->
     prettyInductive ind
   Section nm ds ->
     let nm' = prettyIdent nm
-        ds' = map (indent 2 . prettyDecl) ds
+        ds' = map (PP.indent 2 . prettyDecl) ds
         header = "Section" <+> nm' <+> "."
         footer = "End" <+> nm' <+> "."
     in
     -- XXX vsep issues soft newlines and there should be a hard newline
     -- after the head and after the foot. (Note that every other Decl
     -- always ends in a hard newline, so ds' is ok.)
-    -- (XXX: Does `vsep` on top of `hardline` generate two lines?)
-    vsep $ [header] ++ ds' ++ [footer]
+    -- (XXX: Does `PP.vsep` on top of `PP.hardline` generate two lines?)
+    PP.vsep $ [header] ++ ds' ++ [footer]
   Snippet s ->
     text s
 
 -- | Print a single constructor
-prettyConstructor :: Constructor -> Doc ann
+prettyConstructor :: Constructor -> PP.Doc ann
 prettyConstructor (Constructor {..}) =
   let name' = prettyIdent constructorName
       ty' = prettyTerm PrecNone constructorType
   in
-  nest 2 $ "|" <+> name' <+> ":" <+> ty'
+  PP.nest 2 $ "|" <+> name' <+> ":" <+> ty'
 
 -- | Print an inductive type declaration
-prettyInductive :: Inductive -> Doc ann
+prettyInductive :: Inductive -> PP.Doc ann
 prettyInductive (Inductive {..}) =
   let name' = prettyIdent inductiveName
       params' = hsep' $ map prettyBinder inductiveParameters
@@ -274,5 +274,5 @@ prettyInductive (Inductive {..}) =
       ctors' = map prettyConstructor inductiveConstructors
       header = "Inductive" <+> name' <> params' <+> ":" <> indices' <+> sort' <+> ":="
   in
-  vsep ([nest 2 header] ++ ctors' ++ ["."]) <> hardline
+  PP.vsep ([PP.nest 2 header] ++ ctors' ++ ["."]) <> PP.hardline
 
