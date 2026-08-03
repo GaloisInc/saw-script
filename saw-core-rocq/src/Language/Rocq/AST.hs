2a9d909d905a94eca0af7f25ebbd0a5f04d35723
David Holland <dholland@galois.com>
2026-08-03 17:30:25 -0400

    Language.Rocq.AST: tidy
    (mostly: add comments, mark terms that are types as types)
    
    NFCI.

--- saw-core-rocq/src/Language/Rocq/AST.hs
+++ saw-core-rocq/src/Language/Rocq/AST.hs
@@ -1,11 +1,12 @@
 
 {- |
 Module      : Language.Rocq.AST
-Copyright   : Galois, Inc. 2018
+Description : AST definition for Rocq exporter
 License     : BSD3
-Maintainer  : atomb@galois.com
-Stability   : experimental
-Portability : portable
+Maintainer  : saw@galois.com
+
+This module declares a (simplified) Rocq/Gallina AST for the back end
+of the Rocq exporter.
 -}
 
 module Language.Rocq.AST (
@@ -23,12 +24,18 @@ module Language.Rocq.AST (
 
 import Data.String (IsString(..))
 
--- | An 'Ident' is a Rocq qualified identifier represented as a string,
--- with the invariant that it is lexically valid.
--- A valid Rocq identifier is a sequence of letters, digits,
--- underscores and primes that starts with a letter or underscore.
--- A /qualified/ identifier is a sequence of one or more identifiers
--- separated by periods.
+-- | An 'Ident' is a Rocq qualified identifier represented as a
+--   string, with the invariant that it is lexically valid.
+--
+--   A valid Rocq identifier is a sequence of letters, digits,
+--   underscores and primes that starts with a letter or underscore.
+--
+--   A /qualified/ identifier is a sequence of one or more identifiers
+--   separated by periods.
+--
+--   We don't enforce the distinction between qualified and unqualified
+--   identifiers in this representation.
+--
 newtype Ident = Ident String
   deriving (Eq, Ord)
 
@@ -38,32 +45,67 @@ instance Show Ident where
 instance IsString Ident where
   fromString s = Ident s
 
+-- | Type to hold universes
+--
 data Sort
   = Prop
   | Set
   | Type
   deriving (Show)
 
+-- | Type to hold Gallina expressions
+--
 data Term
+
+    -- | fun x => e
   = Lambda [Binder] Term
-  | Fix Ident [Binder] Term Term
+
+    -- | Fix f x : ty := e
+  | Fix Ident [Binder] Type Term
+
+    -- | forall x, e
   | Pi [PiBinder] Term
+
+    -- | let f arg [: ty] := e in e
   | Let Ident [Binder] (Maybe Type) Term Term
+
+    -- | if e then e else e
   | If Term Term Term
+
+    -- | f args
   | App Term [Term]
+
+    -- | Prop or Type
   | Sort Sort
+
+   -- | x
   | Var Ident
-    -- | A variable that needs to be printed with a leading at sign in order to
-    -- make all arguments explicit
+  
+    -- | @x, that is, Var with @ in order to explicitly apply implicit
+    --   arguments.
   | ExplVar Ident
-    -- | A ascription @tm : tp@ of a type to a term
-  | Ascription Term Term
+
+    -- | Type annotation e: ty
+  | Ascription Term Type
+
+    -- | integer constant in nat
   | NatLit Integer
+  
+    -- | integer constant in Z
   | ZLit Integer
+
+    -- | [e1; e2; ...]
   | List [Term]
+
+    -- | "foo"
   | StringLit String
+
+    -- | e%scope
   | Scope Term String
+
+    -- | Expression-level ltac invocation ltac:(text)
   | Ltac String
+
   deriving (Show)
 
 -- | Type synonym useful for indicating when a term is used as a type.
@@ -75,28 +117,32 @@ data BinderImplicity
   | Explicit
     deriving (Show)
 
--- | An 'Ident' with an optional 'Type', which may be explicit or implicit.
--- For use representing the bound variables in 'Lambda's, 'Let's, etc.
+-- | Bound variable in a `Lambda` or `Let`, optionally with type
 data Binder
   = Binder BinderImplicity Ident (Maybe Type)
     deriving (Show)
 
--- | An 'Type' with an optional 'Ident', which may be explicit or implicit.
--- For use representing arguments in 'Pi' types.
+-- | Bound variable in a `Pi` (forall), optionally with a name
 data PiBinder
   = PiBinder BinderImplicity (Maybe Ident) Type
     deriving (Show)
 
--- Because saw-core does not give very helpful access to the parameters and
--- indices, we just follow their style and define the constructor by its fully
--- applied return type.
+-- | Single constructor declaration in an inductive type declaration.
+--
+--   Because saw-core does not give very helpful access to the parameters and
+--   indices, we just follow that style and define the constructor by its fully
+--   applied return type.
+--
+--   NOTE: constructor names must be unqualified.
+--
 data Constructor = Constructor
   { constructorName    :: Ident
-  -- ^ NOTE: The constructor name must be an /unqualified/ identifier.
-  , constructorType    :: Term
+  , constructorType    :: Type
   }
   deriving (Show)
 
+-- | Inductive type declaration
+--
 data Inductive = Inductive
   { inductiveName         :: Ident
   , inductiveParameters   :: [Binder]
@@ -106,6 +152,13 @@ data Inductive = Inductive
   }
   deriving (Show)
 
+-- | Arbitrary top-level declaration.
+--
+--   Does not support modules or functors.
+--
+--   `Snippet` inserts raw text, presumably for things this AST can't
+--   represent. XXX: probably shouldn't need that
+--
 data Decl
   = Axiom Ident Type
   | Comment String
