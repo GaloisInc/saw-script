0a978c0506352ad2033c4b8b11441edd95d4088d
Daniel Matichuk <dmatichuk@galois.com>
2026-08-27 13:07:12 -0700

    add missing tests to Tests.thy

--- otherTests/saw-core-isabelle/Tests.thy
+++ otherTests/saw-core-isabelle/Tests.thy
@@ -2,7 +2,7 @@ theory Tests
   imports Negate_Proofs And_Proofs Or_Proofs XOR_Proofs Bit_Proofs Comparisons_Proofs
           Sequences_Proofs Comprehension_Proofs RecordDecls_Proofs Words_Proofs OOB_Proofs
           Recursion_Proofs TypeArith_Proofs Quickcheck_Tests "isabelle/tac_prove0" "isabelle/Enums"
-          "isabelle/Nested" "isabelle/Names"
+          "isabelle/Nested" "isabelle/Names" "isabelle/Enums" "isabelle/Foreign" "isabelle/Inf"
 begin
 
 lemma "\<And>x y z. tac_prove0.goal x y z"
